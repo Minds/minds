269b0ae35b5096b30d74942ac88a864b51969fd5
Mark Harding <mark@marksmusiconline.co.uk>
2012-07-26 22:59:26 +0100

    [MOBILE] taken out the ads rotator - too big.

---
+++ mod/mobile/views/mobile/Ads/css.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Satheesh PM, BARC Mumbai
+ * www.satheesh.anushaktinagar.net
+ * 
+ */
+?>
+
+/* ***************************************
+	Ads
+*****************************************/
+
+#ads-footer{
+display:none;
+margin-top: 75px;
+width : auto;
+padding : 3px ;
+overflow: hidden;
+text-align : center;
+}
+
+#ads-sidebar {
+display:none;
+padding : 3px ;
+text-align : center;
+width : auto;
+overflow: hidden;
+}
+
+#ads-header {
+display:none;
+width : auto;
+height : 100px;
+padding : 3px ;
+border-bottom : 1px solid #EEE;
+overflow: hidden;
+position: relative;
+	}
