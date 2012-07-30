28090b2ecdf8388ebeb421bcf6416880f27fb5b0
Mark Harding <mark@marksmusiconline.co.uk>
2012-07-30 11:12:46 +0100

    [SPRINT1] Login box drops down when moused over.

--- mod/minds_theme/views/default/minds/meta.php
+++ mod/minds_theme/views/default/minds/meta.php
@@ -1,5 +1,8 @@
 <?php
-
+	/* MINDS META
+	 *
+	 * SEO ENHANCE
+	*/
 	$description = get_input('description');
 	$keywords = get_input('keywords');
 	//are we a blog, wire post, event??
@@ -16,5 +19,23 @@
 		echo "<meta name=\"keywords\" content=\"" . elgg_get_plugin_setting('default_keywords', 'minds_theme') . "\" />";
 	}
 
-	
+	/* JS Extend */
 ?>
+<script>
+	//$('.elgg-button.elgg-button-dropdown').live('hover', elgg.ui.popupOpen);
+	//$('#login-dropdown-box').live('hover', {);
+	////$("#login-dropdown-box").hover(
+	
+	$(function()
+{    
+    $(".elgg-button.elgg-button-dropdown").mouseenter(function(){ 
+        $("#login-dropdown-box").slideToggle("fast"); 
+        $(this).toggleClass("elgg-state-active");
+    });
+	
+    $("#login-dropdown").mouseleave(function(){
+      $(".elgg-button.elgg-button-dropdown").toggleClass("elgg-state-active");
+      $("#login-dropdown-box").slideToggle("fast"); 
+    });
+});
+</script>
