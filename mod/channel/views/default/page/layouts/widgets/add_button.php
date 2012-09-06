54b85434ed059ce7120938a7c281409992864f7e
Mark Harding <mark@marksmusiconline.co.uk>
2012-09-06 15:55:33 +0100

    Added subscribe and unsubscribe links to channels

--- mod/channel/views/default/page/layouts/widgets/add_button.php
+++ mod/channel/views/default/page/layouts/widgets/add_button.php
@@ -27,6 +27,30 @@ elgg_load_js('elgg.wall');
 										'class' => 'elgg-button elgg-button-action channel'
 									));
 	}
+	
+	/**
+	 * Subscribe/Un-Subscribe button
+	 */
+	
+	if (elgg_is_logged_in()) {
+		if (elgg_get_logged_in_user_guid() != $user->guid) {
+			if ($user->isFriend()) {
+				echo elgg_view('output/url', array(
+										'text' => elgg_echo('friend:remove'),
+										'href' => "action/friends/remove?friend={$user->guid}",
+										'class' => 'elgg-button elgg-button-action channel',
+										'is_action' => true
+									));
+			} else {
+				echo elgg_view('output/url', array(	
+										'text' => elgg_echo('friend:add'),
+										'href' => "action/friends/add?friend={$user->guid}",
+										'class' => 'elgg-button elgg-button-action channel',
+										'is_action' => true
+									));
+			}
+		}
+	}
 									
 	echo elgg_view('output/url', array(
 		'href' => '#widgets-add-panel',
