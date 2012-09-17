b6a1e586aae849463173c56efd5afd30e86286ab
Mark Harding <mark@marksmusiconline.co.uk>
2012-09-17 15:43:36 +0100

    [WS] Gets a list of chats/messages and show messages inside a thread.

--- mod/Minds_WebServices/lib/chat.php
+++ mod/Minds_WebServices/lib/chat.php
@@ -7,8 +7,8 @@
  * @author Mark Harding
  *
  */
- 
- /**
+
+  /**
  * Web service to get the count of unread message
  *
  * @return int
@@ -21,3 +21,98 @@ expose_function('chats.count',
 				'GET',
 				false,
 				false);
+
+ /**
+  * Web service to get a list of chats the user in involved with
+  * 
+  * @return params  
+  */
+ function chats_get(){
+ 	$user_guid = elgg_get_logged_in_user_guid();
+	
+	
+	$chats = elgg_get_entities_from_relationship(array(
+				'type' => 'object',
+				'subtype' => 'chat',
+				'relationship' => 'member',
+				'relationship_guid' => $user_guid,
+				'inverse_relationship' => false,
+				'limit' => 10,
+				'pagination' => true,
+				'full_view' => false,
+		));
+	
+		
+	if($chats){
+			foreach($chats as $single) {
+				$chat['guid'] = $single->guid;
+				
+				$chat['title'] = $single->title;
+				
+				//members
+				$members = $single->getMemberEntities();
+				foreach ($members as $member) {
+					$chat['members'][] = $member->name;
+				}
+				
+				$chat['time_created'] = (int)$single->time_created;
+				
+				$return[] = $chat;
+			}
+	} else {
+			$return = elgg_echo('chat:none');
+	}
+	
+	return $return;
+ }
+expose_function('chats.get',
+				"chats_get",
+				array(
+					),
+				"Get  a list of message",
+				'GET',
+				true,
+				true);
+ /**
+  * Web service to get a chat thread
+  * 
+  * @return return
+  */
+function chat_get($guid){
+	$messages = elgg_get_entities(array(
+										'type' => 'object',
+										'subtype' => 'chat_message', 
+										//'container_guid' => $guid,
+										'limit' => 10,
+										'order_by' => 'e.time_created desc',
+										'pagination' => false,
+									));
+	$messages = array_reverse($messages);
+		
+	if($messages){
+			foreach($messages as $single) {
+				$message['guid'] = $single->guid;
+				
+				//owner
+				$owner = get_entity($single->owner_guid);
+				$message['owner']['guid'] = $owner->guid;
+				$message['owner']['name'] = $owner->name;
+				$message['owner']['avatar_url'] = $owner->getIconURL('small');
+				
+				$message['description'] = strip_tags($single->description);
+				
+				$return[] = $message;
+			}
+	}
+	
+	return $return;
+	
+}
+expose_function('chat.get',
+				"chat_get",
+				array(	'guid' => array ('type' => 'int'),
+					),
+				"Get  a list of message",
+				'GET',
+				true,
+				true);
