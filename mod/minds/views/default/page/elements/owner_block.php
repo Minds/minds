e4ba4bd6098d784bab9f49b90c28fb81d53143ef
Mark Harding <mark@marksmusiconline.co.uk>
2012-09-25 18:26:50 +0100

    Owner box for groups is now better

--- mod/minds/views/default/page/elements/owner_block.php
+++ mod/minds/views/default/page/elements/owner_block.php
@@ -12,8 +12,19 @@ elgg_push_context('owner_block');
 
 // groups and other users get owner block
 $owner = elgg_get_page_owner_entity();
-if ($owner instanceof ElggGroup ||
-	($owner instanceof ElggUser && $owner->getGUID() != elgg_get_logged_in_user_guid())) {
+if ($owner instanceof ElggGroup){
+	
+	$header = elgg_view_entity_icon($owner, 'large', array('link_class'=>'groups'));
+	
+	$body = elgg_view_menu('owner_block', array('entity' => $owner));
+	
+	echo elgg_view('page/components/module', array(
+		'header' => $header,
+		'body' => $body,
+		'class' => 'elgg-owner-block',
+	));
+	
+}elseif(($owner instanceof ElggUser && $owner->getGUID() != elgg_get_logged_in_user_guid())) {
 
 	$header = elgg_view_entity($owner, array('full_view' => false));
 
