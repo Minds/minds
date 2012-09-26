7f4696fbb2a42b6374b2c15ddfa0d9d5331e7b6e
Mark Harding <mark@marksmusiconline.co.uk>
2012-09-26 17:09:28 +0100

    User BG now caches

--- mod/channel/background.php
+++ mod/channel/background.php
@@ -3,6 +3,9 @@
 require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 
 header('Content-Type: image/jpeg');
+header('Expires: ' . date('r', time() + 864000));
+header("Pragma: public");
+header("Cache-Control: public");
 
 $owner_guid = get_input('guid');
 $thumb = get_input('thumb');
