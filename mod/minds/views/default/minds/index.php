d1ddd354d7be9474c20f3f9e97178962f44722e4
Mark Harding <mark@marksmusiconline.co.uk>
2012-09-30 17:24:56 +0100

    New logo for .COM

--- mod/minds/views/default/minds/index.php
+++ mod/minds/views/default/minds/index.php
@@ -2,10 +2,11 @@
 /**
  * Index View
  */
+$img_src = elgg_get_site_url() == 'http://www.minds.com' ? elgg_get_site_url().'mod/minds/graphics/minds_logo.png' : elgg_get_site_url().'mod/minds/graphics/minds_logo.gif';
 ?>
 <div class='minds_index'>
     <div class="logo">
-        <img src="<?php echo elgg_get_site_url(); ?>mod/minds/graphics/minds_logo.gif" width="200" height="90" />
+        <img src="<?php echo $img_src;?>" width="200" height="90" />
     </div>
     
     <?php if(!elgg_is_logged_in()) {
