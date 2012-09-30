0ea2abf738275f1d2a13502699a5b48962d82163
Mark Harding <mark@marksmusiconline.co.uk>
2012-09-30 17:41:12 +0100

    [FIX] Asks for username and email, not display and username!

--- mod/minds/views/default/forms/minds/front_register.php
+++ mod/minds/views/default/forms/minds/front_register.php
@@ -1,7 +1,7 @@
 <?php
 
-echo elgg_view('input/text', array(	'name'=> 'n', 'placeholder'=>elgg_echo('name')));
-
 echo elgg_view('input/text', array(	'name'=> 'u', 'placeholder'=>elgg_echo('username')));
+
+echo elgg_view('input/text', array(	'name'=> 'e', 'placeholder'=>elgg_echo('email')));
  
 echo elgg_view('input/submit', array('value' => elgg_echo('register:early')));
