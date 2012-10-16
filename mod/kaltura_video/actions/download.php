55810255df32f52ab586541be93b205fbef6ea1f
Mark Harding <mark@marksmusiconline.co.uk>
2012-10-16 22:15:52 +0100

    [FIX] Kaltura downloads

--- mod/kaltura_video/actions/download.php
+++ mod/kaltura_video/actions/download.php
@@ -9,8 +9,11 @@ $guid = get_input('guid');
 
 $ob = get_entity($guid);
 
+$kaltura_server = elgg_get_plugin_setting('kaltura_server_url',  'kaltura_video');
+$partnerId = elgg_get_plugin_setting('partner_id', 'kaltura_video');
+
 $filename = $ob->title . '.mp4';
-$fileurl = 'http://www.minds.tv/p/100/sp/0/playManifest/entryId/' . $ob->kaltura_video_id . '/format/url/flavorParamId/10/' . $filename;
+$fileurl = $kaltura_server . '/p/'. $partnerId . '/sp/0/playManifest/entryId/' . $ob->kaltura_video_id . '/format/url/flavorParamId/10/' . $filename;
 
 header("Pragma: public");
 
