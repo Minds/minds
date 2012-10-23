edde3005a7c8e8c5da09b630897825ed4bfd512e
Mark Harding <mark@marksmusiconline.co.uk>
2012-10-23 22:15:56 +0100

    Backgrounds don't work on .COM yet

--- mod/minds/views/default/minds/meta.php
+++ mod/minds/views/default/minds/meta.php
@@ -69,7 +69,7 @@ if(elgg_get_context() == 'channel' || elgg_get_context() == 'main' || elgg_get_c
 		
 	
 BODY;
-	}elseif(elgg_get_context() != 'channel') {
+	}elseif(elgg_get_context() != 'channel' && elgg_get_site_url() != 'http://www.minds.com/') {
 	$bgs = array('city.jpg', 'crowd-1.jpg', 'sunrise-1.jpg', 'space-1.jpg', 'space-2.jpg', 'space-3.jpg');
 	$rand_bg = $bgs[rand(0, 5)];
 	echo "body{
