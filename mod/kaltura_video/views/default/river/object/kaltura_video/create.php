f75e514e6841353eb82455d4a82bd0939fa5d3d3
Mark Harding <mark@marksmusiconline.co.uk>
2012-11-17 17:21:19 +0000

    Changes size of video thumbnails

--- mod/kaltura_video/views/default/river/object/kaltura_video/create.php
+++ mod/kaltura_video/views/default/river/object/kaltura_video/create.php
@@ -25,7 +25,7 @@
 		'href' => false,
 		'class' => 'uiVideoInline archive',
 		'video_id'=> $object->kaltura_video_id,
-		'text' =>  '<span></span><img src=\'' . kaltura_get_thumnail($object->kaltura_video_id, 525, 0, 100) . '\' width="525px"/>',
+		'text' =>  '<span></span><img src=\'' . kaltura_get_thumnail($object->kaltura_video_id, 525, 295, 100) . '\' width="525px"/>',
 		'title' => $object->title,
 	));
 //$image = kaltura_create_generic_widget_html ( $object->kaltura_video_id , 'news' );
