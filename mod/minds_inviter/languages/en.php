e8b82b452e51115ed8687487fd33c362ff395b8f
Mark Harding <mark@marksmusiconline.co.uk>
2012-11-25 14:25:51 +0000

    Language fix

--- mod/minds_inviter/languages/en.php
+++ mod/minds_inviter/languages/en.php
@@ -12,7 +12,7 @@ $english = array(
 	
 	'minds_inviter:success' => 'Done!',
 	
-	'minds_inviter:blurb' => 'Please select a service below to invite your friends to Minds. We will never ask you for, or store, your passwords for any other application.',
+	'minds_inviter:blurb' => 'Please select a service below to invite your friends to Minds. We will never ask you for, or store, your passwords for any application.',
 	
 	'minds_inviter:invite' => 'Select your contacts from the list below:',
 	'minds_inviter:invite:submit' => 'Invite',
@@ -20,7 +20,7 @@ $english = array(
 	'minds_inviter:subject' => '%s has invited you to Minds.',
 	'minds_inviter:body' => 'Hello.
 	
-	%s (%s) has invited to join Minds. Please go to http://www.minds.com to sign up. 
+	%s (%s) has invited you to Minds. Please click %s to sign up. 
 	
 	Thanks. 
 	
