e3605a7efd156c87c1ca65be2a0fa2e915f69916
Mark Harding <mark@marksmusiconline.co.uk>
2012-12-15 14:08:46 +0000

    Removes suggest and member pages
    These have been replaced by the channels plugin

--- mod/suggested_friends/views/default/suggested_friends/list.php
+++
@@ -1,4 +0,0 @@
-<div class="pftn_list_title">
-	<h2><?php echo elgg_echo('suggested_friends'); ?></h2>
-</div>
-<?php echo elgg_view('suggested_friends/people', array('people' => $vars['people'])); ?>
