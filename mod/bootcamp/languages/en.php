4d90e100faab537c71117b4e6e655118e47730ff
Mark Harding <mark@marksmusiconline.co.uk>
2012-12-18 18:17:32 +0000

    Language changes

--- mod/bootcamp/languages/en.php
+++ mod/bootcamp/languages/en.php
@@ -5,18 +5,18 @@
 
 $english = array(
 	'bootcamp' => 'Bootcamp',
-	'bootcamp:title' => 'Bootcamp',
-	'bootcamp:blurb' => 'Welcome to Bootcamp. We need a short description here to greet users and encourage them to sign up!',
+	'bootcamp:title' => 'Getting started...',
+	'bootcamp:blurb' => 'Time to customize your Minds channel!',
 	'bootcamp:progress:blurb'=> 'complete',
 	
 	'bootcamp:step:avatar:title' => 'Upload an avatar',
-	'bootcamp:step:avatar:content' => 'Click here to upload an avatar for your profile',
+	'bootcamp:step:avatar:content' => 'Click here to upload an avatar for your channel',
 	'bootcamp:step:channel:title' => 'Setup your channel',
 	'bootcamp:step:channel:content' => 'Your channel is a representation of you on Minds. Add some widgets and background and make your profile look great!',
 	'bootcamp:step:subscribe:title' => 'Subscribe to other channels',
 	'bootcamp:step:subscribe:content' => 'Connect to other channels on Minds and and follow people who interest you.',
 	'bootcamp:step:upload:title'=> 'Upload media',
-	'bootcamp:step:upload:content'=>'Contribute to piece of creative commons media to the community',
+	'bootcamp:step:upload:content'=>'Contribute a piece of creative commons media to the community',
 	
 );
 
