d83b421ce986aa578ee49f4820125b20e3dd7568
Mark Harding <mark@marksmusiconline.co.uk>
2013-01-15 14:20:16 +0000

    fixes double posting issue

--- mod/minds_comments/views/default/js/minds_comments.php
+++ mod/minds_comments/views/default/js/minds_comments.php
@@ -1,17 +1,10 @@
 <?php if (FALSE) : ?>
     <script type="text/javascript">
 <?php endif; ?>
-    elgg.provide('hj.comments');
+    elgg.provide('minds.comments');
 
-    /**
-     *  Initialize hypeAlive JS
-     */
-    hj.comments.init = function() {
-        if(window.ajaxcommentsready === undefined) {
-            window.ajaxcommentsready = true;
-        }
-        //hj.comments.triggerRefresh();
-        
+    minds.comments.init = function() {
+       
         $('body').on('focus', '.comments-input',function(e){ $(this).autosize();});
         
         $('body').on('keyup', '.comments-input',function(e){
@@ -25,19 +18,6 @@
 															 });
 
         var bar_loader = '<div class="hj-ajax-loader hj-loader-bar"></div>';
-
-        // Show comment input on click
-        $('.elgg-menu-item-comment')
-        .unbind('click')
-        .bind('click', function(event) {
-            event.preventDefault();
-
-            $(this)
-            .parents('div.hj-annotations-menu')
-            .siblings('ul:first')
-            .find('.hj-comments-input:first')
-            .toggle();
-        });
 
         // Toggle loading of older comments
         $('.hj-comments-summary')
@@ -58,50 +38,18 @@
                 ref.push(data);
                 console.log(data);
                 $(this).css({'height':'16px'}).html(bar_loader);
-                hj.comments.refresh(ref, 'old');
+                minds.comments.refresh(ref, 'old');
             });
         });
-
-       /* $('.hj-ajaxed-comment-save')
-        .removeAttr('onsubmit')
-        .unbind('submit')
-        .bind('submit', hj.comments.saveComment);*/
        
-       $('body').on('submit', '.hj-ajaxed-comment-save', hj.comments.saveComment);
+       $('body').on('submit', '.hj-ajaxed-comment-save', minds.comments.saveComment);
 
     };
 
-    hj.comments.triggerRefresh = function() {
-        var time = 25000;
-        if (!window.commentstimer) {
-            window.commentstimer = true;
-            var refresh_comments = window.setTimeout(function(){
-                var ref = new Array();
-                // Let's get the timestamp of the first item in the list (newest comment)
-                $('.hj-syncable.hj-comments')
-                .each(function() {
-                    var first = $('li.elgg-item:first', $(this));
-                    if (!first.length) {
-                        first = $(this);
-                    }
-                    var data = $(this).data('options');
-                    data.timestamp = first.data('timestamp');
-                    ref.push(data);
-                });
-                if (window.ajaxcommentsready) {
-                    //elgg.system_message(elgg.echo('hj:comments:refreshing'));
-                    hj.comments.refresh(ref, 'new');
-                }
-                window.commentstimer = false;
-            }, time);
-        }
-    }
-
-    hj.comments.refresh = function(data, sync) {
-		console.log('loaded');
+    minds.comments.refresh = function(data, sync) {
+		//console.log('loaded');
         if (!data.length) return true;
-        if (window.ajaxcommentsready || sync == 'old') {
-            window.ajaxcommentsready = false;
+
             elgg.action('action/comment/get', {
                 data : {
                     sync : sync,
@@ -118,35 +66,24 @@
                                 var new_item = $(val2).show();
                                 if (sync == 'new') {
                                     commentsList.append(new_item);
-                                    console.log(new_item);
+                                    //console.log(new_item);
                                 } else {
                                     commentsList
                                     .prepend(new_item);
-                                    console.log(new_item);
+                                    //console.log(new_item);
                                 }
                             });
                         });
                     }
                     $('.hj-comments-summary').has('.hj-ajax-loader').each(function() {$(this).hide()});
-                    window.ajaxcommentsready = true;
-                    elgg.trigger_hook('success', 'hj:framework:ajax');
                 }
             });
-        }
+       
     }
     
-    hj.comments.saveCommentNew = function(event) {
+    minds.comments.saveComment = function(e) {
     	        
-        event.preventDefault();
-        
-    	console.log('submit');
-    }
-
-    hj.comments.saveComment = function(e) {
-    	        
-        e.preventDefault();
-        $('body').off('submit', '.hj-ajaxed-comment-save');
-        console.log('hit');
+       // $('body').off('submit', '.hj-ajaxed-comment-save');
 
         var     values = $(this).serialize(),
         action = $(this).attr('action'),
@@ -157,9 +94,6 @@
 
         data.pid = $('input[name="pid"]', $(this)).val();
         data.type = $('input[name="type"]', $(this)).val();
-        
-        container = $('#hj-annotations-'+ id);
-        commentsList = container.find('ul.hj-syncable.hj-comments:first');
 
         ref.push(data);
 
@@ -170,8 +104,7 @@
         elgg.action(action + '?' + values, {
 	    contentType : 'application/json',
             success : function(output) {
-				console.log('saving');
-               // hj.comments.refresh(ref, 'new');
+				//console.log('saving');
                 var container = $('#minds-comments-'+ data.pid);
                 var commentsList = container.find('ul.hj-syncable .comments .minds-comments:first');
 				commentsList.append(output);
@@ -181,17 +114,12 @@
                 .val('')
                 .parents('div.hj-comments-input:first')
                 .toggle();
-                window.ajaxcommentsready = true;
-                    elgg.trigger_hook('success', 'hj:framework:ajax');
             }
         });
-
+ 		e.preventDefault();
     }
 
-    elgg.register_hook_handler('init', 'system', hj.comments.init);
-    //elgg.register_hook_handler('init', 'system', hj.comments.triggerRefresh);
-    elgg.register_hook_handler('success', 'hj:framework:ajax', hj.comments.init, 500);
-	
+    elgg.register_hook_handler('init', 'system', minds.comments.init);
 	
 	
 <?php if (FALSE) : ?></script><?php endif; ?>
