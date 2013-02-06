bb2f141d21b0d8eb649af04ea46787caa4d7eedc
Mark Harding <mark@marksmusiconline.co.uk>
2013-02-06 22:11:18 +0000

    fixes typo

--- mod/minds/views/default/output/carousel.php
+++ mod/minds/views/default/output/carousel.php
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	
 	// Using custom configuration
 	$("#<?php echo $id;?>").carouFredSel({
-		items				: 3r,
+		items				: 3,
 		direction			: "left",
 		swipe				: true,
 		infinite			: true,
