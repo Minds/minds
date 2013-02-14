839f1c0561913fdbe126e17600160d115b2e24c4
Mark Harding <mark@marksmusiconline.co.uk>
2013-02-14 14:12:37 +0000

    Url encodes image OG tag to see possible fix

--- mod/minds_social/start.php
+++ mod/minds_social/start.php
@@ -206,7 +206,7 @@ function minds_get_fbimage($description) {
   if(empty($fbimage)) {
     $fbimage = elgg_get_site_url() . 'mod/minds/graphics/minds_logo.png';
   }
-  return $fbimage;
+  return url_encode($fbimage);
 }
 
 elgg_register_event_handler('init','system','minds_social_init');		
