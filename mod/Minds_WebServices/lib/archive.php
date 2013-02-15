6725c1bd7ce719528cdbdfcb6c2b5b9fd707f2fd
minds <git@minds.com>
2013-02-15 18:21:41 +0000

    fixes media type

--- mod/Minds_WebServices/lib/archive.php
+++ mod/Minds_WebServices/lib/archive.php
@@ -16,15 +16,17 @@ require_once(dirname(dirname(dirname(__FILE__))) ."/kaltura_video/kaltura/api_cl
  *
  * @return array (entry id, ks, uploadtoken)
  */
-function archive_kaltura_create($filename, $filesize) {
+function archive_kaltura_create($filename, $filesize, $filetype) {
 			
 	$user = elgg_get_logged_in_user_entity();
 		
 	$kmodel = KalturaModel::getInstance();
 	$mediaEntry = new KalturaMediaEntry();
 	$mediaEntry->name = 'Temporary Entry ID: '.time();
+	$mediaEntry->mediaType = file_get_simple_type($mime_type) == 'audio' ? KalturaMediaType_AUDIO : KalturaMediaType_VIDEO;
 	$mediaEntry->description = '';
 	$mediaEntry->adminTags = KALTURA_ADMIN_TAGS;
+	$mediaEntry = $kmodel->addMediaEntry($mediaEntry);
 	
 	$kmodel = KalturaModel::getInstance();
 	$ks = $kmodel->getClientSideSession();
@@ -45,7 +47,8 @@ function archive_kaltura_create($filename, $filesize) {
 expose_function('archive.kaltura.create',
 				"archive_kaltura_create",
 				array(	'filename' => array ('type' => 'string', 'required' => true),
-					  	'filesize' => array ('type' => 'string', 'required' => true),
+					'filesize' => array ('type' => 'int', 'required' => true),
+					'filetype' => array ('type' => 'string', 'required' => true),
 					),
 				"Create a kaltura entry",
 				'POST',
