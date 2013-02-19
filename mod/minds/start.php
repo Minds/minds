aa443c3c78ccf54184e025ce8f29c2e79e0c3c34
minds <git@minds.com>
2013-02-19 16:49:46 +0000

    user is allowed to login without a password when using oauth from mobile

--- mod/minds/start.php
+++ mod/minds/start.php
@@ -94,6 +94,10 @@ function minds_init(){
 	elgg_register_action("minds/remind", "$actionspath/minds/remind.php");
 	elgg_register_action("minds/remind/external", "$actionspath/minds/remind_external.php");
 	elgg_register_action("friends/add", "$actionspath/friends/add.php");
+	
+	if(elgg_get_context() == 'oauth2'){
+		pam_auth_usertoken();//auto login users if they are using oauth step1
+	}
 }
 
 function minds_index($hook, $type, $return, $params) {
