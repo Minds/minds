0b196a06deb25c61fa9facf847d99f872214af6e
Mark Harding <mark@marksmusiconline.co.uk>
2013-03-09 10:01:49 +0000

    adds more ads

--- mod/minds/views/default/minds/ads.php
+++ mod/minds/views/default/minds/ads.php
@@ -14,4 +14,27 @@ if($type == 'content-side'){
 			<script type="text/javascript"
 			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 			</script>';
+	echo '<script type="text/javascript"><!--
+			google_ad_client = "ca-pub-9303771378013875";
+			/* Minds Content Sidebar */
+			google_ad_slot = "6555161626";
+			google_ad_width = 200;
+			google_ad_height = 200;
+			//-->
+			</script>
+			<script type="text/javascript"
+			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>';
+} elseif($type == 'content-foot'){
+	echo '<script type="text/javascript"><!--
+			google_ad_client = "ca-pub-9303771378013875";
+			/* Content Bottom Banner */
+			google_ad_slot = "9810862421";
+			google_ad_width = 728;
+			google_ad_height = 90;
+			//-->
+			</script>
+			<script type="text/javascript"
+			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>';
 }
