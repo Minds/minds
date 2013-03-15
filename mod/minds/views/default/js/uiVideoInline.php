84990874961d8c7833b1fefc9c640f919bce34b5
Mark Harding <mark@marksmusiconline.co.uk>
2013-03-15 22:50:59 +0000

    fixes pause issue

--- mod/minds/views/default/js/uiVideoInline.php
+++ mod/minds/views/default/js/uiVideoInline.php
@@ -3,8 +3,10 @@ elgg.provide('elgg.uiVideoInline');
 elgg.uiVideoInline.init = function() {
 	//Archive inlines
 	$('body').on('click', '.uiVideoInline.archive', function(e){ 
-													$(this).load(elgg.config.wwwroot + 'archive/inline/' + $(this).attr('video_id'));
-													//$(this).removeClass('uiVideoInline');
+													if(!$(this).hasClass('active')){
+														$(this).load(elgg.config.wwwroot + 'archive/inline/' + $(this).attr('video_id'));
+														$(this).addClass('active');
+													}
 													});
 	//Social embeds inline											
 	$('body').on('click', '.uiVideoInline.links', function(e){ 
