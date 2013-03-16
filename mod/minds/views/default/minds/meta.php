fd91a00fff3eca116ce76a58cb169befaa6c8ca3
Mark Harding <mark@marksmusiconline.co.uk>
2013-03-16 11:50:36 +0000

    fixes default description

--- mod/minds/views/default/minds/meta.php
+++ mod/minds/views/default/minds/meta.php
@@ -10,13 +10,13 @@
 	if($description){
 		echo "<meta name=\"description\" content=\"$description\" />";
 	} else {
-		echo "<meta name=\"description\" content=\"" . elgg_get_plugin_setting('default_description', 'minds_theme') . "\" />";
+		echo "<meta name=\"description\" content=\"" . elgg_get_plugin_setting('default_description', 'minds') . "\" />";
 	}
 	
 	if($keywords){ 
 		echo "<meta name=\"keywords\" content=\"$keywords>\"/>";
 	} else {
-		echo "<meta name=\"keywords\" content=\"" . elgg_get_plugin_setting('default_keywords', 'minds_theme') . "\" />";
+		echo "<meta name=\"keywords\" content=\"" . elgg_get_plugin_setting('default_keywords', 'minds') . "\" />";
 	}
 
 $request = $_SERVER['REQUEST_URI'];
