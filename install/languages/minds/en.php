1b137ea173e1f98254ea5450bb5c1efc28ade138
Marcus Povey <marcus@marcus-povey.co.uk>
2013-05-17 09:47:58 +0100

    Adding translations.

--- install/languages/minds/en.php
+++ install/languages/minds/en.php
@@ -1,6 +1,10 @@
 <?php
 $english = array(
     'install:minds' => 'Configure Minds',
+    'install:minds:label:minds_username' => 'Your Minds Username',
+    'install:minds:help:minds_username' => 'The username you use to log into Minds.com',
+    'install:minds:label:minds_password' => 'Your Minds password',
+    'install:minds:help:minds_password' => 'The password you use for your Minds.com account'
 );
 
 add_translation("en", $english);
