3c9ca1e47081099da9de589883abc9b2cda5abe6
Mark Harding <mark@marksmusiconline.co.uk>
2013-05-29 21:44:20 +0100

    adds breaks

--- mod/archive/pages/archive/unavailable.php
+++ mod/archive/pages/archive/unavailable.php
@@ -11,7 +11,7 @@ $sidebar = elgg_view('archive/sidebar');
 		$area3 = elgg_view('kaltura/categorylist',array('baseurl' => $CONFIG->wwwroot . 'search/?subtype=kaltura_video&tagtype=universal_categories&tag=','subtype' => 'kaltura_video'));
 */
 $body = elgg_view_layout(	"content", array(
-												'content' => '<h1> Sorry! We are upgrading and videos are unavailable.</h1> <h1>Please check back later! </h1>', 
+												'content' => '<h1> Sorry! We are upgrading and videos are unavailable.</h1><br/> <h1>Please check back later! </h1>', 
 												'sidebar' => $sidebar, 
 												//'title' => elgg_echo('archive:all'),
 											));
