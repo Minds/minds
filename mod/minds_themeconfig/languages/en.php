2b1355adb0fbbcc65e707db8013d9a8b28d7f83b
Marcus Povey <marcus@marcus-povey.co.uk>
2013-05-30 15:25:58 +0100

    Background and custom CSS editing

--- mod/minds_themeconfig/languages/en.php
+++ mod/minds_themeconfig/languages/en.php
@@ -7,6 +7,10 @@ $english = array(
         'minds_themeconfig:logo' => 'Upload your own logo',
         'minds_themeconfig:backgroundcolour' => 'Page background colour',
         'minds_themeconfig:textcolour' => 'Text colour',
+        'minds_themeconfig:background' => 'Upload background image',
+    
+        'minds_themeconfig:advanced' => 'Advanced settings...',
+        'minds_themeconfig:custom_css' => 'Custom CSS'
 );
 		
 add_translation("en", $english);
--- mod/minds_themeconfig/views/default/minds_themeconfig/css.php
+++ mod/minds_themeconfig/views/default/minds_themeconfig/css.php
@@ -6,10 +6,18 @@
     }
     <?php } ?>
     
+    <?php if (elgg_get_plugin_setting('background_override', 'minds_themeconfig') == 'true') { ?>
+    body {
+        background-image:url('<?php echo elgg_get_site_url(); ?>themeicons/background');
+    }
+    <?php } ?>
+    
     <?php if ($text_colour = elgg_get_plugin_setting('text_colour', 'minds_themeconfig')) { ?>
     body {
         color: #<?php echo $text_colour;?>;
     }
     <?php } ?>
+    
+    <?php echo elgg_get_plugin_setting('custom_css', 'minds_themeconfig'); ?>
 </style>
 				
