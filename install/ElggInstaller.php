b4a543180a25170726af3f0d65d81dfa7ef3203b
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-03 10:54:37 +0100

    Anypage not safe to turn on here...

--- install/ElggInstaller.php
+++ install/ElggInstaller.php
@@ -1486,7 +1486,7 @@ class ElggInstaller {
 							'minds_social',
 							'minds_webservices',
                                                         'minds_themeconfig',
-                                                        'anypage',
+                                                        //'anypage',
 							'persona',
 							'notifications',
 							'minds_connect',
