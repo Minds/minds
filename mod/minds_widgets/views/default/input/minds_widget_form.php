759b9887f734ad67cfc0dc0a51d083d6237928be
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-19 15:43:29 +0100

    Changing short code to long for for nginx support

--- mod/minds_widgets/views/default/input/minds_widget_form.php
+++ mod/minds_widgets/views/default/input/minds_widget_form.php
@@ -1,24 +1,24 @@
-<div id="minds-widget-<?=$vars['tab'];?>" class="minds-widget-form">
+<div id="minds-widget-<?php echo $vars['tab'];?>" class="minds-widget-form">
 
-    <p><?= elgg_echo("minds_widgets:{$vars['tab']}:comments");?></p>
+    <p><?php echo  elgg_echo("minds_widgets:{$vars['tab']}:comments");?></p>
     
     <form>
-        <?= $vars['form-body']; ?>
+        <?php echo  $vars['form-body']; ?>
         
-        <input id="minds-widget-<?=$vars['tab'];?>-submit" type="submit" value="Get the code..." />
+        <input id="minds-widget-<?php echo $vars['tab'];?>-submit" type="submit" value="Get the code..." />
     </form>
     
-    <div class="get-the-code" style="display:none; padding:20px; margin-top: 30px; background-color: #ddd;" title="<?= elgg_echo('minds_widgets:tab:'.$vars['tab']); ?>">
+    <div class="get-the-code" style="display:none; padding:20px; margin-top: 30px; background-color: #ddd;" title="<?php echo  elgg_echo('minds_widgets:tab:'.$vars['tab']); ?>">
         <textarea></textarea>
     </div>
 </div>
 <script>
     $(document).ready(function(){
-        $('#minds-widget-<?=$vars['tab'];?> form').submit(function() {
+        $('#minds-widget-<?php echo $vars['tab'];?> form').submit(function() {
             
-            $('#minds-widget-<?=$vars['tab'];?> textarea').load("<?= current_page_url() ?>/getcode", $(this).serialize());
+            $('#minds-widget-<?php echo $vars['tab'];?> textarea').load("<?php echo  current_page_url() ?>/getcode", $(this).serialize());
             
-             $('#minds-widget-<?=$vars['tab'];?> div.get-the-code').fadeIn();
+             $('#minds-widget-<?php echo $vars['tab'];?> div.get-the-code').fadeIn();
             
             return false;
         });
