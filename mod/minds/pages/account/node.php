23b6a323b99d47f76891991d16e0dd24d2fe22e5
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-24 12:14:38 +0100

    Tier selection page

--- mod/minds/pages/account/node.php
+++ mod/minds/pages/account/node.php
@@ -9,6 +9,9 @@ $content = elgg_view_title($title);
 
 // Display original form for logged in users
 if ($user) {
+    
+    if (!elgg_is_admin_logged_in())
+        forward(); // Only admins can access logged in register form stuff now, since we're taking payment etc
 
     // create the registration url - including switching to https if configured
     $register_url = elgg_get_site_url() . 'action/registernode';
@@ -23,15 +26,38 @@ if ($user) {
     $content .= elgg_view_form('node_loggedin', $form_params, $body_params);
 } else {
     
-    $register_url = elgg_get_site_url() . 'action/registernewnode';
-    $form_params = array(
-            'action' => $register_url,
-            'class' => 'elgg-form-account',
-    );
+    $payment_recieved = false; // TODO: Test to see if we've returned from a successful billing workflow
+    
+    // TODO: Payment hook here
+    
+    
+    
+    
+    // If paid for then allow registration
+    if ($payment_recieved) {
 
-    $body_params = array(
-    );
-    $content .= elgg_view_form('node', $form_params, $body_params);
+        $register_url = elgg_get_site_url() . 'action/registernewnode';
+        $form_params = array(
+                'action' => $register_url,
+                'class' => 'elgg-form-account',
+        );
+
+        $body_params = array(
+        );
+        $content .= elgg_view_form('node', $form_params, $body_params);
+    }
+    else
+    {
+        $register_url = elgg_get_site_url() . 'action/select_tier';
+        $form_params = array(
+                'action' => $register_url,
+                'class' => 'elgg-form-account',
+        );
+
+        $body_params = array(
+        );
+        $content .= elgg_view_form('select_tier', $form_params, $body_params);
+    }
     
 }
 
