a3eebc95ae26a0da91f8961825ef9b8c77e29179
Mark Harding <mark@minds.com>
2013-06-26 13:55:06 +0000

    mobile fixes since ui update

---
+++ mod/mobile/views/mobile/css/mobile.php
@@ -0,0 +1,26 @@
+input[type="text"], input[type="password"]{
+        width:100%;
+        height:50px !important;
+        box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+}
+.elgg-list{
+	width:auto;
+	height:auto;
+}
+.elgg-list .elgg-item{
+	width:auto;
+	margin:10px 0;
+	height:auto;
+	float:none;
+}
+.social_login{
+	margin:5px;
+}
+.elgg-form .elgg-menu li{
+	width:100%;
+	height:auto;
+	float:none;
+	clear:both;
+}
