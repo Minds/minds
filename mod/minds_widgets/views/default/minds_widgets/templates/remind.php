29266c516eba1de7b92de16baaafdf52fdd2e408
Marcus Povey <marcus@marcus-povey.co.uk>
2013-06-28 11:40:05 +0100

    Detecting page remind code is embedded on.

--- mod/minds_widgets/views/default/minds_widgets/templates/remind.php
+++ mod/minds_widgets/views/default/minds_widgets/templates/remind.php
@@ -1,4 +1,4 @@
-<a class="entypo minds-remind" href="<?php echo elgg_get_site_url(); ?>widgets/<?php echo $vars['tab']; ?>/service/?url=<?php echo urlencode(get_input('url')); ?>" title="ReMind (repost)" onClick='window.open(this.href, "Remind", "width=800,height=600"); return false;'><span class="mind_graf" style="display:none;">&#59159;</span><span class="mind_fall">Remind</span></a>
+<a class="entypo minds-remind" href="<?php echo elgg_get_site_url(); ?>widgets/<?php echo $vars['tab']; ?>/service/" title="ReMind (repost)" onClick='window.open(this.href + "?url=" + encodeURIComponent(document.URL), "Remind", "width=800,height=600"); return false;'><span class="mind_graf" style="display:none;">&#59159;</span><span class="mind_fall">Remind</span></a>
 <script type="text/javascript">
     
     var style = document.createElement('style');
