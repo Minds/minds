1060e006c13c6c4f961369b643a8671db1946da1
Christopher Harding <chris@kramnorth.com>
2013-06-29 16:50:30 +0100

    language update part 3

--- mod/Français-V1.8.13/Efface_version_en.bat
+++
@@ -1,1 +0,0 @@
-del en.php /s
--- mod/brazilian portuguese 1.8/mod/translationbrowser/bkp/actions/translationbrowser/hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    register_error(elgg_echo(">>>>> dentro <<<<"));
-    system_message(elgg_echo(">>>>> dentro <<<<"));
-    forward("translationbrowser/");
-
-exit;
-?>
