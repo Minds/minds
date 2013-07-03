1e78a544ab0830d03bb3668795a5205a4a1bc1ad
Christopher Harding <chris@kramnorth.com>
2013-07-03 21:16:34 +0100

    language update 6

--- mod/notifications/languages/es.php
+++ mod/notifications/languages/es.php
@@ -20,7 +20,13 @@ $spanish = array(
 	'notifications:subscriptions:groups:description' => 'Para recibir notificaciones de contenidos nuevos de los grupos en el que eres miembro, busca a continuaci&oacute;n y selecciona las notificaciones que desees recibir.',
 
 	'notifications:subscriptions:success' => 'Tu configuraci&oacute;n de notoficaciones ha sido guardada.',
-
+	
+	'notification' => 'Notificaciones',
+	'notifications' => 'Notificationes',
+	
+	'notifications:not_logged_in' => 'Su sesión ha expirado. Por favor, vuelva a iniciar sesión pulg.',
+	
+	'notifications:unread' => '%s nuevo',
 );
 
 add_translation("es", $spanish);
