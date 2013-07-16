f12596c54cde4b592ad8073cad58eddff9c58bbd
Marcus Povey <marcus@marcus-povey.co.uk>
2013-07-16 10:13:40 +0100

    Minds multisite installer language strings

--- languages/minds/en.php
+++ languages/minds/en.php
@@ -4,7 +4,10 @@ $english = array(
     'install:minds:label:minds_username' => 'Your Minds Username',
     'install:minds:help:minds_username' => 'The username you use to log into Minds.com',
     'install:minds:label:minds_password' => 'Your Minds password',
-    'install:minds:help:minds_password' => 'The password you use for your Minds.com account'
+    'install:minds:help:minds_password' => 'The password you use for your Minds.com account',
+    'install:success:minds' => 'Minds Connect successfully set up',
+    'install:fail:minds' => 'Could not set up Minds Connect'
+    
 );
 
 add_translation("en", $english);
