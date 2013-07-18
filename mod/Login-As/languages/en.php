5c22bc0516a05377776818c1ade538beb2d6fe55
Marcus Povey <marcus@marcus-povey.co.uk>
2013-07-18 09:57:51 +0100

    Adding login-As

---
+++ mod/Login-As/languages/en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * English strings
+ */
+
+$en = array(
+	'login_as:login_as' => 'Login as',
+	'login_as:logged_in_as_user' => 'You are now logged in as %s.',
+
+	'login_as:return_to_user' => 'Logout as %s',
+
+	'login_as:unknown_user' => 'Unknown user. Could not login.',
+	'login_as:could_not_login_as_user' => 'Could not login as %s',
+);
+
+add_translation('en', $en);
