83bce4a46fa885cac4f63d1a3168ef3b36e7f6af
Mark Harding <mark@marksmusiconline.co.uk>
2013-09-20 04:41:04 +0000

    improves parse

--- parseSQL.php
+++ parseSQL.php
@@ -1,13 +1,14 @@
 <?php
 
-require('install/vendors/PHP-SQL-Parser/php-sql-parser.php');
 require('engine/start.php');
+
+$GUID = new GUID();
 
 ini_set('memory_limit', '4G');
 set_time_limit ( 0 );
 error_reporting(E_ERROR);
 
-$tables = array('objects_entity', 'users_entity', 'entities', 'entity_subtypes', 'metadata', 'metastrings', 'private_settings');
+$tables = array('objects_entity', 'users_entity', 'entities', 'entity_subtypes', 'entity_relationships', 'metadata', 'metastrings', 'private_settings');
 
 //@todo make this recieve variab;es
 $mysql = mysqli_connect("192.168.200.16","minds","","minds") or die("Error " . mysqli_error($link));
@@ -42,6 +43,12 @@ foreach($data->entities as $guid => $entity){
 
 	//fix subtype issue
 	$entity->subtype = $data->entity_subtypes[$entity->subtype]->subtype;
+
+	//make widgets into type
+        if($entity->subtype == 'widget'){
+                $entity->type = 'widget';
+                $entity->subtype = '';
+        }
 	
 	$data->entities[$guid] = $entity;
 }
@@ -68,240 +75,42 @@ echo "Data merges complete... \n";
 
 echo "Now saving entities to Cassandra... this may take a while \n";
 
-foreach($data->entities as $row){
-	$row->guid = (int)$row->guid;
-	$row->owner_guid = (int)$row->owner_guid;
-	$row->access_id = (int)$row->access_id;
+$errors = array();
 
-	if($row->type =='group' || $row->type == 'plugin' ||  $row->subtype == 'oauthnonce' || !$row->type){ continue; }
-	$entity = entity_row_to_elggstar($row, $row->type);
-	$guid = $entity->save();
-	echo "Migrated: {$row->type}:{$row->subtype}:$guid \n";
-}
+try{
+	foreach($data->entities as $row){ 
+		$guid = $GUID->migrate($row->guid);
+		$row->guid = $guid;
+		$row->owner_guid = $GUID->migrate($row->owner_guid);//we need to move owners too
+		$row->access_id = (int)$row->access_id;
+
+		if($row->type =='group' || $row->subtype == 'oauth2_access_token' || $row->subtype == 'plugin' ||  $row->subtype == 'oauthnonce' || !$row->type){ continue; }
+		$entity = entity_row_to_elggstar($row, $row->type);
+		$entity->save();
+		echo "Migrated: {$row->type}:{$row->subtype}:$guid \n";
+	}
+
+	echo "\n\n Beginning subscriptions transfer \n";
+
+	foreach($data->entity_relationships as $relationship){
+		if($relationship->relationship == 'friend'){
+			$user = get_entity( $GUID->migrate($relationship->guid_one), 'user');
+			$user2 = get_entity( $GUID->migrate($relationship->guid_two),'user');
+			$user->addFriend( $GUID->migrate($relationship->guid_two));
+		echo "{$user->name} is now following {$user2->name}\n";
+		}
+	}
 
 echo "Migration complete... please test!\n";
-
-exit;
-
-if ($script = file_get_contents($dumpfile)) {
-	// Remove MySQL -- style comments
-	$script = preg_replace('/\-\-.*\n/', '', $script);
-
-	echo "File has been loaded... now parsing \n";
-	
-	// Statements must end with ; and a newline
-		$sql_statements = preg_split('/;[\n\r]+/', $script);
-		
-		foreach ($sql_statements as $statement) {
-			$statement = trim($statement);
-			if(strpos($statement,'INSERT') !== false){
-
-				//GET THE METASTRING
-				if(strpos($statement,'metastrings` VALUES') !== false){
-					$statement = explode('VALUES ',$statement);
-					$statement = $statement[1];
-					$explode = explode('),',$statement);
-					foreach($explode as $item){
-						$item = str_replace('(','',$item);
-						$item = explode(",'", $item);
-						$id = $item[0];
-						$data->metastring[$id]['id'] = $id;
-						$data->metastring[$id]['value'] = $item[1];
-					}
-				}
-
-				//get the metadata
-                                if(strpos($statement,'metadata` VALUES') !== false){
-					$statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement); 
-                                	foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                                                $item = explode(",", $item);
-                                                $id = $item[0];
-						$data->metadata[$id]['id'] = $id;
-						$data->metadata[$id]['guid'] = $item[1];
-						$data->metadata[$id]['name_id'] = $item[2];
-						$data->metadata[$id]['value_id'] = $item[3];
-                                        }
-					
-				}
-
-				//get the users
-				if(strpos($statement,'users_entity` VALUES') !== false){
-                                        $statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement); 
-                                        foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                   				$item = str_replace("'", '', $item);
-		                           	$item = explode(",", $item);
-                                                $guid = $item[0];
-                                                $data->user[$guid]['guid'] = $guid;
-                                                $data->user[$guid]['name'] = $item[1];
-                        			$data->user[$guid]['username'] = $item[2];
-						$data->user[$guid]['password'] = $item[3];
-						$data->user[$guid]['salt'] = $item[4];
-						$data->user[$guid]['email'] = $item[5];                
-						$data->user[$guid]['language'] = $item[6]; 
-						$data->user[$guid]['code'] = $item[7]; 
-						$data->user[$guid]['banned'] = $item[8]; 
-						$data->user[$guid]['admin'] = $item[9]; 
-						$data->user[$guid]['last_action'] = $item[10]; 
-					}
-                                        
-                                }
-
-				//get the objects
-				if(strpos($statement,'objects_entity` VALUES') !== false){
-                                        $statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement); 
-                                        foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                                            	$item = str_replace("'", '', $item);
-						$item = explode(",", $item);
-						var_dump($item); exit;
-                                                $guid = $item[0];
-                                                $data->object[$guid]['guid'] = $guid;
-                                                $data->object[$guid]['title'] = $item[1];
-                                                $data->object[$guid]['description'] = $item[2];
-				        }
-                                } 
-				
-				//get all entites
-				if(strpos($statement,'entities` VALUES') !== false){
-                                        $statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement);
-                                        foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                                                $item = str_replace("'", '', $item);
-						$item = explode(",", $item);
-                                                $guid = $item[0];
-						$type = $item [1];
-						$data->{$type}[$guid]['guid'] = $guid;
-                                                $data->{$type}[$guid]['type'] = $type;
-                                                $data->{$type}[$guid]['subtype'] = $item[2];
-						$data->{$type}[$guid]['owner_guid'] = $item[3];
-                                        }
-
-                                }
-
-				//get subtypes
-                                if(strpos($statement,'entity_subtypes` VALUES') !== false){
-                                        $statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement);
-                                        foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                                                $item = str_replace("'", '', $item);
-                                                $item = explode(",", $item);
-                                                $id = $item[0];
-                                                $data->subtypes[$id]['id'] = $id;
-                                                $data->subtypes[$id]['type'] = $item[1];
-                                                $data->subtypes[$id]['subtype'] = $item[2];
-                                        }
-
-                                }	
-
-				if(strpos($statement,'datalists` VALUES') !== false){
-					$statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement);
-                                        foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                                                $item = str_replace("'", '', $item);
-                                                $item = explode(",", $item);
-                                                $id = $item[1];
-                                                $data->datalist[$id]['name'] = $item[0];
-                                                $data->datalist[$id]['id'] = $item[1];
-                                        }
-				}
-
-				if(strpos($statement,'private_settings` VALUES') !== false){
-                                        $statement = explode('VALUES ',$statement);
-                                        $statement = $statement[1];
-                                        $explode = explode('),',$statement);
-                                        foreach($explode as $item){
-                                                $item = str_replace('(','',$item);
-                                                $item = str_replace("'", '', $item);
-                                                $item = explode(",", $item);
-                                                $id = $item[0];
-                                                $data->private[$id]['id'] = $id;
-                                                $data->private[$id]['entity_guid'] = $item[1];
-						$data->private[$id]['name'] = $item[2];
-						$data->private[$id]['value'] = $item[3];
-                                        }
-                                }
-			}
-		}
+} catch(Exception $e){
+	$errors[] = $e->getMessage();
 }
 
-//now we have the data, let merge the metadata with the entities
-$types = array('user', 'object');
-foreach($data->metadata as $metadata){
-	foreach($types as $type){
-		$entity = $data->{$type}[$metadata['guid']];
-		if(!$entity){ continue; }
-		$name = $data->metastring[$metadata['name_id']]['value'];
-		$value = $data->metastring[$metadata['value_id']]['value'];
-		$data->{$type}[$metadata['guid']][$name] = $value;	
+if(!empty($errors)){
+	$count = count($errors);
+	echo "There were $count errors:\n";
+	foreach($errors as $error){
+		echo $error;
 	}
 }
 
-//now add private settings to entitites
-foreach($data->private as $ps){
-        foreach($types as $type){
-                $entity = $data->{$type}[$ps['entity_guid']];
-                if(!$entity){ continue; }
-                $data->{$type}[$ps['entity_guid']][$ps['name']] = $ps['value'];
-        }
-}
-
-echo "File finished parsing and merging... beginning cassandrsa import \n";
-
-unset($data->metadata);
-unset($data->metastring);
-
-//free up a bit of memory
-
-$stats = array(	'users' => count($data->user),
-		'objects' => count($data->object)
-		);
-
-//Convert arrays to objects
-function array_to_object($array){
-	$object = new stdClass();
-	foreach($array as $k=>$v){
-		if(!$k || !$v){ continue; }
-		$object->$k = $v;
-	}
-	return $object;
-}
-
-echo "NOW IMPORTING " . $stats['users'] . " USERS...\n";
-foreach($data->user as $user){
-	$user = new ElggUser(array_to_object($user));
-	$user->save();
-}
-
-echo "USERS IMPORTED!! \n\n";
-
-echo "NOW IMPORTING " . $stats['objects'] . " OBJECTS...\n";
-
-foreach($data->object as $object){
-        $subtype = $data->subtypes[$object['subtype']]['subtype'];
-	if($subtype == 'plugin' || $subtype == 'widget'){//remove plugins
-                continue;
-        }
-	$object = new ElggObject(array_to_object($object));
-	if(!$object->title){
-		echo 'There was an issue with GUID:'. $object->guid;
-		continue;
-	}
-	$object->subtype = $subtype;
-	$object->save();
-}
-
-echo "All done! Now test and debug \n";
