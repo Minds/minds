5ef20a755fc09d734633fe27ef418e613a70f5b2
Mark Harding <mark@marksmusiconline.co.uk>
2013-10-15 15:46:10 +0000

    languages changes

--- mod/orientation/languages/en.php
+++ mod/orientation/languages/en.php
@@ -11,7 +11,7 @@ $english = array(
 	
 	'orientation:step:avatar:title' => 'Upload an avatar',
 	'orientation:step:avatar:content' => 'Click here to upload an avatar for your channel',
-	'orientation:step:channel:title' => 'Setup your channel',
+	'orientation:step:channel:title' => 'Set up your channel',
 	'orientation:step:channel:content' => 'Your channel is a representation of you on Minds. Add some widgets and background and make your profile look great!',
 	'orientation:step:subscribe:title' => 'Subscribe to channels',
 	'orientation:step:subscribe:content' => 'Connect to other channels on Minds and and follow people who interest you',
@@ -21,7 +21,7 @@ $english = array(
 	'orientation:step:wallpost:content'=>'Post a status to the news to engage with other users',
 	'orientation:step:invite:title'=> 'Invite',
 	'orientation:step:invite:content'=>'Introduce your friends to Minds and help to grow the community',
-	'orientation:step:blog:title'=> 'Create a blog post',
+	'orientation:step:blog:title'=> 'Create a blog',
 	'orientation:step:blog:content'=>'Click here to create your first blog post',
 	'orientation:step:search:title'=> 'Search',
 	'orientation:step:search:content'=>'Search Minds',
