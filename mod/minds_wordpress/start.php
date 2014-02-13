99c01835dd71e18b6f329bf12276decb32746b94
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-13 17:04:17 +0000

    Topbar JS endpoint

--- mod/minds_wordpress/start.php
+++ mod/minds_wordpress/start.php
@@ -2,5 +2,12 @@
 
 elgg_register_event_handler('init','system',function(){
     
+    // JS for topbar
+    elgg_register_page_handler('minds_topbar_js', function ($pages) {
+        
+        
+        require_once(dirname(__FILE__) . '/pages/topbar_js.php');
+        
+    });
     
 });	
