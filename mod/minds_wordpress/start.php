fedd8c99fe6aa4a7f27bd7a168899d90a094cf27
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-14 10:54:25 +0000

    Linking to custom CSS

--- mod/minds_wordpress/start.php
+++ mod/minds_wordpress/start.php
@@ -3,10 +3,14 @@
 elgg_register_event_handler('init','system',function(){
     
     // JS for topbar
-    elgg_register_page_handler('minds_topbar_js', function ($pages) {
+    elgg_register_page_handler('minds_topbar', function ($pages) {
         
+        switch ($pages[0]) {
+            case 'css' : require_once(dirname(__FILE__) . '/pages/topbar_css.php'); break;
+            case 'js' : require_once(dirname(__FILE__) . '/pages/topbar_js.php'); break;
+        }
         
-        require_once(dirname(__FILE__) . '/pages/topbar_js.php');
+        return true;
         
     });
     
