6b2f1b27788c9bf1f40d73a7f5dcfc272d042c93
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-14 13:20:07 +0000

    Constructing wordpress toolbar

--- mod/minds_wordpress/pages/topbar_js.php
+++ mod/minds_wordpress/pages/topbar_js.php
@@ -1,8 +1,40 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// JS Embed a minds toolbar on a wordpress site. This is a bit of a hack, there are probably easier ways...
 
+header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); 
+header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); 
+header( 'Cache-Control: no-store, no-cache, must-revalidate' ); 
+header( 'Cache-Control: post-check=0, pre-check=0', false ); 
+header( 'Pragma: no-cache' ); 
+
+?>
+var div = document.getElementById("Minds-Toolbar");
+
+
+function mindsGetToolbarCSS() {
+    return "<?php
+    
+        $css = "<style type=\"text/css\">\n" . elgg_view('wordpress/css/css') . "</style>";
+        $css = str_replace("\n", ' ', $css);
+        $css = addslashes($css);
+        
+        echo $css;
+    
+    ?>";
+}
+
+function mindsGetToolbar() {
+    return "<?php 
+    
+        $toolbar = elgg_view('wordpress/page/elements/toolbar');
+        $toolbar = str_replace("\n", ' ', $toolbar);
+        $toolbar = addslashes($toolbar);
+        
+        echo $toolbar;
+    
+    ?>";
+
+}
+
+div.innerHTML= mindsGetToolbarCSS() + mindsGetToolbar();
