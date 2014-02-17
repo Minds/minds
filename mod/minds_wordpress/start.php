cfef2768531bd00142421027b0831ff92c1280cd
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-17 18:32:07 +0000

    Ping source with comment data; need to find a way of testing this without elastic search...

--- mod/minds_wordpress/start.php
+++ mod/minds_wordpress/start.php
@@ -14,4 +14,49 @@ elgg_register_event_handler('init','system',function(){
         
     });
     
+    // Listen for new comments
+    elgg_register_event_handler('comment:create', 'comment', function($event, $object_type, $data) {
+        
+        global $CONFIG;
+        
+        // Ping our comment at our linked server permalink, the server then uses the API to verify the comment before posting
+        if (($data['_source']['pid']) && ($post = get_entity($data['_source']['pid'], 'object'))) {
+            
+            // Ok we've got a post, does it have a remote permalink?
+            if ($permalink = $post->ex_permalink) {
+                
+                // Ping data to the minds wordpress plugin
+
+                $ch = curl_init();
+                
+                $query = http_build_query($data);
+
+                if (strpos($permalink, '?') === false) // Tell the wordpress blog that we're pinging with a comment action
+                    $permalink .= '?minds-connect=comment-on';
+                else
+                    $permalink .= '&minds-connect=comment-on';
+                
+                curl_setopt($ch,CURLOPT_URL, $permalink);
+                curl_setopt($ch,CURLOPT_POST, 1);
+                curl_setopt($ch,CURLOPT_POSTFIELDS, $query);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_USERAGENT, "Minds Site at " . current_page_url());
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+                //execute post
+                $result = curl_exec($ch);
+                if ($CONFIG->debug)
+                    error_log("Result from ping: $result");
+                
+                curl_close($ch);
+            }
+            
+        }
+        
+    });
+    
+    
 });	
