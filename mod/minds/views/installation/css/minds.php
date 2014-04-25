97e1c5337d94b247043167faf7b2c3b5b3f8a251
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-25 12:56:58 +0100

    Installation minds

---
+++ mod/minds/views/installation/css/minds.php
@@ -0,0 +1,9 @@
+@font-face {
+    font-family: 'entypo';
+    src: url('<?php echo elgg_get_site_url();?>mod/minds/vendors/entypo/entypo.eot?') format('eot'),
+         url('<?php echo elgg_get_site_url();?>mod/minds/vendors/entypo/entypo.woff') format('woff'),
+         url('<?php echo elgg_get_site_url();?>mod/minds/vendors/entypo/entypo.ttf') format('truetype'),
+         url('<?php echo elgg_get_site_url();?>mod/minds/vendors/entypo/entypo.svg') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
