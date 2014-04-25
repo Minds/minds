cb23dea1dccf5e33c291035ec45177ae015de6c9
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-25 13:04:07 +0100

    Installation minds

--- mod/minds/views/installation/css/minds.php
+++ mod/minds/views/installation/css/minds.php
@@ -1,3 +1,4 @@
+
 @font-face {
     font-family: 'entypo';
     src: url('<?php echo elgg_get_site_url();?>mod/minds/vendors/entypo/entypo.eot?') format('eot'),
@@ -6,6 +7,16 @@
          url('<?php echo elgg_get_site_url();?>mod/minds/vendors/entypo/entypo.svg') format('svg');
     font-weight: normal;
     font-style: normal;
+}
+@font-face {
+  font-family: 'fontello';
+  src: url('<?php echo elgg_get_site_url();?>mod/minds/vendors/fontello/font/fontello.eot?96059246');
+  src: url('<?php echo elgg_get_site_url();?>mod/minds/vendors/fontello/font/fontello.eot?96059246#iefix') format('embedded-opentype'),
+       url('<?php echo elgg_get_site_url();?>mod/minds/vendors/fontello/font/fontello.woff?96059246') format('woff'),
+       url('<?php echo elgg_get_site_url();?>mod/minds/vendors/fontello/font/fontello.ttf?96059246') format('truetype'),
+       url('<?php echo elgg_get_site_url();?>mod/minds/vendors/fontello/font/fontello.svg?96059246#fontello') format('svg');
+  font-weight: normal;
+  font-style: normal;
 }
 
 
