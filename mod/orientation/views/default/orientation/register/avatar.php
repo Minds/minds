f87c8f336250e3db2f0c74591f19cfa6b221ec6e
Mark Harding <mark@marksmusiconline.co.uk>
2014-05-13 14:10:53 +0000

    fixes avatar orientation bug

--- mod/orientation/views/default/orientation/register/avatar.php
+++ mod/orientation/views/default/orientation/register/avatar.php
@@ -7,9 +7,8 @@ $user = elgg_get_logged_in_user_entity();
 	A cool avatar can get you noticed. 
 </div>
 <?php
-
 // only offer the crop view if an avatar has been uploaded
-if (isset($user->icontime)) {
+if (isset($user->icontime) && $user->icontime) {
 	echo elgg_view('forms/avatar/crop', array('entity' => $user));
 } else {
 	echo elgg_view('forms/avatar/upload', array('entity' => $user));
