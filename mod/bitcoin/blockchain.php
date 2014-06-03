7729e712cee658e4b4201c4c3c9713eff5d79c7e
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-03 17:00:22 +0100

    Curl utility function to call API ends

--- mod/bitcoin/blockchain.php
+++ mod/bitcoin/blockchain.php
@@ -5,5 +5,76 @@ namespace minds\plugin\bitcoin;
 use minds\core;
 
 class blockchain extends bitcoin {
-    
+
+    private $blockchain_base = "https://blockchain.info";
+
+    /**
+     * Make an API call.
+     */
+    private function __make_call($verb, $endpoint, array $params = null, array $headers = null) {
+	$req = "";
+	if ($params) {
+	    $req = http_build_query($params);
+	}
+
+	$curl_handle = curl_init();
+
+	switch (strtolower($verb)) {
+	    case 'post':
+		curl_setopt($curl_handle, CURLOPT_POST, 1);
+		curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $req);
+		break;
+	    case 'delete':
+		curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, 'DELETE'); // Override request type
+		curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $req);
+		break;
+	    case 'put':
+		curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, 'PUT'); // Override request type
+		curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $req);
+		break;
+	    case 'get':
+	    default:
+		curl_setopt($curl_handle, CURLOPT_HTTPGET, true);
+		if (strpos($endpoint, '?') !== false) {
+		    $endpoint .= '&' . $req;
+		} else {
+		    $endpoint .= '?' . $req;
+		}
+		break;
+	}
+
+	curl_setopt($curl_handle, CURLOPT_URL, $endpoint);
+	curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 5);
+	curl_setopt($curl_handle, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl_handle, CURLOPT_AUTOREFERER, true);
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl_handle, CURLOPT_USERAGENT, "Minds Bitcoin Agent");
+	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, 1);
+	curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
+
+	// Allow plugins and other services to extend headers, allowing for plugable authentication methods on calls
+	$new_headers = \Idno\Core\site()->triggerEvent('webservice:headers', ['headers' => $headers, 'verb' => $verb]);
+	if (!empty($new_headers) && (is_array($new_headers))) {
+	    if (empty($headers))
+		$headers = [];
+	    $headers = array_merge($headers, $new_headers);
+	}
+
+	if (!empty($headers) && is_array($headers)) {
+	    curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
+	}
+
+	$buffer = curl_exec($curl_handle);
+	$http_status = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+
+	if ($error = curl_error($curl_handle)) {
+	    error_log($error);
+	}
+
+	curl_close($curl_handle);
+
+	return ['content' => $buffer, 'response' => $http_status, 'error' => $error];
+    }
+
 }
