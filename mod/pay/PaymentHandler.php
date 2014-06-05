64c41f4f5908efa8b58eaa05f8c5cafb4ec76e06
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-05 14:44:11 +0100

    Define a payment interface

---
+++ mod/pay/PaymentHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Define an interface for an OO payment handler.
+ */
+
+namespace minds\plugin\pay {
+    
+    interface PaymentHandler {
+
+	/**
+	 * Handle a payment, forwarding to a payment endpoint as appropriate.
+	 * @param type $params
+	 */
+	static function paymentHandler($params);
+	
+	/**
+	 * Callback, called by pay with an order ID.
+	 * @param type $order_guid
+	 */
+	static function paymentCallback($order_guid);
+	
+	/**
+	 * Called by pay to cancel a recurring payment.
+	 * @param type $order_guid
+	 */
+	static function cancelRecurringPaymentCallback($order_guid);
+    }
+    
+}
