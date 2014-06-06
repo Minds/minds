cb7265c313520aff024a4b1a8e21549fa054e891
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-06 16:04:57 +0100

    Central bitcoin account details

---
+++ mod/bitcoin/views/default/plugins/bitcoin/settings.php
@@ -0,0 +1,6 @@
+<p>
+    <label>Minds central bitcoin account: <br />
+	<?= elgg_view('input/text',array('name'=>'params[central_bitcoin_account]','value'=> elgg_get_plugin_setting('central_bitcoin_account', 'bitcoin'))); ?>
+    </label>
+    This is the central bitcoin account, used as a bin for bitcoins that are sent or earned by users who don't have a bitcoin account.
+</p>
