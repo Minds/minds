26f944a7750087bf3c128e98a2e5d2666d80c7cc
Mark Harding <mark@marksmusiconline.co.uk>
2014-06-12 20:41:56 +0100

    outline for the network (decentralised) plugin

---
+++ mod/network/views/default/guard/twofactor/authorise.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This extends the login box to ask for a twofacor authentication code
+ */
+if(get_input('twofactor'))
+	echo elgg_view('input/text', array('name'=>'code', 'placeholder'=>'We just sent you an SMS with a code, please type it in here'));
