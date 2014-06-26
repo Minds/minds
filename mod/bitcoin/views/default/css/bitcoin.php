46e870d8d350819a972d3720ec6257dd22d95b90
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-26 16:07:58 +0100

    More CSS

--- mod/bitcoin/views/default/css/bitcoin.php
+++ mod/bitcoin/views/default/css/bitcoin.php
@@ -2,6 +2,10 @@ div.wallet {
     margin: 20px;
 }
 
+div.wallet div.report {
+    margin-top: 20px;
+}
+
 div.wallet div.header {
     border-bottom: 1px solid #eee;
     padding-bottom: 10px;
@@ -9,4 +13,8 @@ div.wallet div.header {
 
 div.wallet p.balance {
     font-size: 2em;
+}
+
+div.bitcoin-transaction span.action {
+    text-transform:capitalize;
 }
