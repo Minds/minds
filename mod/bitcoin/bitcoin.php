9590a9d8139ca7f3ef813ebe7acb1cee3101d07d
Marcus Povey <marcus@marcus-povey.co.uk>
2014-06-26 16:03:07 +0100

    Adding wallet link to menu

--- mod/bitcoin/bitcoin.php
+++ mod/bitcoin/bitcoin.php
@@ -134,6 +134,15 @@ abstract class bitcoin extends \ElggPlugin
 	    return true;
 	});
 	
+	// Bitcoin wallet menu
+	elgg_register_menu_item('site', array(
+		'name' => 'bitcoin',
+		'text' => 'My Wallet', // TODO: Replace me with a nice graphic
+		'href' => 'bitcoin/mywallet',
+		'title' => elgg_echo('bitcoin'),
+		'priority' => 10
+	));
+	
     }
     
     
