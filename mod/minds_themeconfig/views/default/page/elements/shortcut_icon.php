a2c6754bdbfb65720bfdcf9e898f3e7599af7955
Mark Harding <mark@Marks-iMac.local>
2014-09-23 17:50:52 +0100

    nicer favicon

--- mod/minds_themeconfig/views/default/page/elements/shortcut_icon.php
+++ mod/minds_themeconfig/views/default/page/elements/shortcut_icon.php
@@ -7,7 +7,7 @@ if ($ts = elgg_get_plugin_setting('logo_favicon_ts','minds_themeconfig')) {
 else
 {
 ?>
-<link rel="SHORTCUT ICON" href="<?php echo elgg_get_site_url(); ?>mod/minds/graphics/favicon.ico" />
+<link rel="icon" type="image/png" href="<?php echo elgg_get_site_url();?>_graphics/icon.png" />
 <?php
 }
 ?>
