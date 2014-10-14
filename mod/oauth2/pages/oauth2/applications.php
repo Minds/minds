137a8df81093bec7c342e5f003c58d5e1edc871f
Mark Harding <mark@Marks-iMac.local>
2014-10-14 19:54:26 +0100

    oauth2 changes as per https://app.asana.com/0/16785308773868/17810989983178

--- mod/oauth2/pages/oauth2/applications.php
+++ mod/oauth2/pages/oauth2/applications.php
@@ -7,12 +7,14 @@
  * @copyright Minds.com 2013
  * @link http://minds.com
  */
-
+elgg_set_context('settings');
 $options = array(
     'type'       => 'object',
     'subtype'    => 'oauth2_client',
     'limit'      => 10,    
     'full_view'  => false,
+    'list_class' => 'x1',
+    'masonry' => false
 );
     
 if (elgg_is_admin_logged_in() && $page[1] == 'all') {
@@ -24,14 +26,15 @@ if (elgg_is_admin_logged_in() && $page[1] == 'all') {
     
 elgg_register_title_button('oauth2', 'add');
 
-$content = elgg_list_entities($options);
+$content = elgg_view_form('oauth2/register');
+$content .= elgg_list_entities($options);
 
 $params = array(
     'title'   => $title, 
     'content' => $content,
-    'filter'  => ''
+    'filter'  => '',
+	'sidebar_class' => 'elgg-sidebar-alt'
 );
-
-$body = elgg_view_layout('content', $params);
+$body = elgg_view_layout('one_sidebar', $params);
 
 echo elgg_view_page($title, $body);
---
+++ mod/oauth2/views/default/oauth2/css.php
@@ -0,0 +1,9 @@
+.oauth2-authorize{
+	display:block;
+	margin:auto;
+	min-width:300px;
+	width:40%;
+	border:1px solid #EEE;
+	border-radius:2px;
+	padding:16px;
+}
