543c93c1fda39a2359b58cd008a4dd9c0516857f
Mark Harding <mark@Marks-iMac.local>
2014-11-25 11:55:26 +0000

    sending from stored chat now also send to the other users live chat

--- mod/gatherings/views/default/js/gatherings/stored.php
+++ mod/gatherings/views/default/js/gatherings/stored.php
@@ -19,22 +19,27 @@ minds.conversations.init = function() {
 	 * Check to see if we have a connection to the user on the other side
 	 */
 	$('.elgg-form-conversation').on('submit', function(e){
-		//e.preventDefault();
 		
-		user_guid = $(this).find('input[name="user_guid"]').val();
-		portal.find().send("message", 
-						{ 
-							to_guid: user_guid,
-							message:'test',
-							from:'mark'
-						},
-					 	function(data){
-					 		console.log('typing');
-					 	},
-					 	function(data){
-					 		console.log('typing errored');
-					 	}
-				);
+		var user_guid = $(this).find('input[name="user_guid"]').val();
+		var message = $(this).find('textarea').val();
+		
+		function encrypt(guid, message){
+			var jse = new JSEncrypt();
+		 	var pub = JSON.parse(window.localStorage.getItem('publickey:'+guid));
+			jse.setPublicKey(pub);
+			
+			return jse.encrypt(message);
+		}
+		
+		encrypted = encrypt(user_guid, message);
+		own = encrypt(elgg.get_logged_in_user_guid(), message);
+		
+		var data = { to_guid: user_guid, message: 'encrypted...', from_name:elgg.get_logged_in_user_entity().name};
+		data["message:"+ user_guid] = encrypted;
+		data["message:"+ elgg.get_logged_in_user_guid()] = own;
+		
+		console.log(user_guid);
+		portal.find().send("message", data);
 	});
 		
 }
