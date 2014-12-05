5aea3e0a93140b38bc20824a428ee3aadddf27d5
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-05 12:10:22 +0000

    Don't quash error on include

--- mod/minds_nodes/actions/edit.php
+++ mod/minds_nodes/actions/edit.php
@@ -4,8 +4,10 @@ $node_guid = get_input('node_guid');
 $node = new MindsNode($node_guid);
 
 if(!$node->domain){
-	register_error('The following domain could not be found');
-	return false;
+	register_error('The domain could not be found');
+	
+	forward(REFERER);
+	//return false;
 }
 
 $domain_at_minds = get_input('domain_at_minds');
@@ -23,3 +25,5 @@ $node->renameNode($domain);
 
 
 $node->save();
+
+forward(REFERER);
