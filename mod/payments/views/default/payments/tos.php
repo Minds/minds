de6b9cb950222cd7f3284c07b017d0e4eeaa9072
Mark Harding <mark@Marks-iMac.local>
2014-12-05 16:52:07 +0000

    TOS options

---
+++ mod/payments/views/default/payments/tos.php
@@ -0,0 +1,6 @@
+<div class="elgg-module elgg-module-info">
+	<div class="elgg-head">
+		<h3>Partner Program Terms of Service</h3>
+	</div>
+	<?php echo elgg_view_form('payments/tos', array('action'=>'settings/payments/payouts')); ?>
+</div>
