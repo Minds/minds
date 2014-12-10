bba0662b4dc47129dd2dbe5fa98f19dabdd8430a
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-10 17:15:45 +0000

    Using lookup table for counter

--- mod/minds_widgets/views/default/minds_widgets/data/remind.php
+++ mod/minds_widgets/views/default/minds_widgets/data/remind.php
@@ -1,9 +1,9 @@
 <?php
 
-$cacher = \minds\core\data\cache\factory::build();
+$lookup = new \minds\core\data\lookup('count:external:reminds');
+    
+$count = (int)$lookup->get(get_input('url'));
 
-$key = "remind-".md5(get_input('url'));
-$count = (int)$cacher->get($key);
 
 if ($count >= 1000 ) {
     $count = sprintf('%0.1f', $count / 1000);
