ee652346edb534d04859246fa9ea95263eb49449
Mark <mark@Marks-MacBook-Pro.local>
2015-01-14 16:07:24 -0500

    removes junk...

--- misc/testComments.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require(dirname(dirname(__FILE__)) . '/engine/start.php');
-
-$mc = new MindsComments();
-$create = $mc->create('entity', '301885764336095232', 'hello!');
-var_dump($create);
--- misc/testRiver.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(dirname(dirname(__FILE__)) . '/engine/start.php');
-
-
-$item = new ElggRiverItem('347040960221286400');
-var_dump($item);
