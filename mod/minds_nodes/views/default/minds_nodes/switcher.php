1952c14b26bba8b3952d97b33f3bc1e0661fcea9
Mark Harding <mark@marksmusiconline.co.uk>
2015-01-21 15:38:14 +0000

    fixes some bugs

--- mod/minds_nodes/views/default/minds_nodes/switcher.php
+++ mod/minds_nodes/views/default/minds_nodes/switcher.php
@@ -16,7 +16,7 @@
 	}
     
     ?>
-	<li><a ref="<?= elgg_get_site_url(); ?>nodes/launch" class="elgg-button elgg-button-action">+ Launch a node</a></li>
+	<li><a href="<?= elgg_get_site_url(); ?>nodes/launch" class="elgg-button elgg-button-action">+ Launch a node</a></li>
 	</ul>
     </div>
 </div>
