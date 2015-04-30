aa72df27989978530f1d4032cd2470cd561a73d8
Mark Harding <mark@marksmusiconline.co.uk>
2015-04-30 10:23:59 +0000

    api for geolocation now returns array of results

--- pages/api/v1/geolocation.php
+++ pages/api/v1/geolocation.php
@@ -15,25 +15,30 @@ use Minds\Api\Factory;
 class geolocation implements interfaces\api, interfaces\ApiIgnorePam{
 
     public function get($pages){
-        
+
         $url = "http://nominatim.openstreetmap.org/search.php?q=" . urlencode($_GET['q']) . "&format=json&addressdetails=1";
-        
+                 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         $output = curl_exec($ch); 
         curl_close($ch);  
-        
+                                 
         $data = json_decode($output, true);
-        var_dump($data[0]);
-        
-        $city = isset($data[0]['address']['city']) ? $data[0]['address']['city'] : $data[0]['address']['town'];
-        $coorinates = $data[0]['lat'] . ',' . $data[0]['lon'];
 
-        return Factory::response(array(
-            'city' => $city,
-            'coordinates' => $coorinates
-        ));
+        switch($pages[0]){
+            case 'list':
+                return Factory::response(array('results'=>$data));
+                break;
+            default:
+                $city = isset($data[0]['address']['city']) ? $data[0]['address']['city'] : $data[0]['address']['town'];
+                $coorinates = $data[0]['lat'] . ',' . $data[0]['lon'];
+
+                return Factory::response(array(
+                    'city' => $city,
+                    'coordinates' => $coorinates
+                ));
+        }
     }
     
     public function post($pages){
