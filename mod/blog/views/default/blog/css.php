436bf5b99385c741594e3870b42a066bd8e73d16
Mark Harding <mark@marksmusiconline.co.uk>
2015-05-22 16:49:07 +0000

    tinymc tweaks and padding on images

--- mod/blog/views/default/blog/css.php
+++ mod/blog/views/default/blog/css.php
@@ -42,6 +42,17 @@
 	margin:10px 0;
 }
 
+.blog-post img[style*="left"]{
+    padding:8px 8px 8px 0;
+}
+.blog-post img[style*="right"]{
+    padding:8px 0px 8px 8px;
+}
+
+.blog-post-edit img{
+    padding:8px;
+}
+
 /* Blog Plugin */
 
 /* force tinymce input height for a more useful editing / blog creation area */
