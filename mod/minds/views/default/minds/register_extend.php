403828cf5344a1d0ceda077d3d90cb31893b96eb
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-17 11:23:02 +0000

    fixes reg page link

--- mod/minds/views/default/minds/register_extend.php
+++ mod/minds/views/default/minds/register_extend.php
@@ -6,6 +6,6 @@
 <div>
 <label>
 	<input type="checkbox" name="tcs" value="true">
-	<?php echo elgg_echo('minds:register:terms:read')."<a href=\"{$vars['url']}terms\">".elgg_echo('minds:regsiter:terms:link')."</a>" ?>
+	<?php echo elgg_echo('minds:register:terms:read')."<a href=\"{$vars['url']}p/terms\">".elgg_echo('minds:regsiter:terms:link')."</a>" ?>
 	<input type="checkbox" name="terms" value="false">
 </label></div>
