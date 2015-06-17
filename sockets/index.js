ed64f14df3482f29f8b3e5648cf2952dfb2cb6e1
Mark Harding <mark@minds.com>
2015-06-17 09:39:30 -0400

    sockets for webrtc

--- sockets/index.js
+++ sockets/index.js
@@ -2,8 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var _ = require('lodash-node');
+var twilio = require('twilio')('AC2e0a25157a4e150f3a87fd6e2f21730c', 'add6d113b8a62e1111c4795e375071de');
 
 var users = [];
+var call_queue = [];
 
 app.get('/', function (req, res){
   res.sendfile('index.html');
@@ -25,6 +27,11 @@ io.on('connection', function (socket) {
       return; 
     }
 
+    if(!guid){
+        socket.emit('connect_error', 'Guid must be set..');
+        return;
+    }
+
     users.push({ 
       guid: guid,
       socket: socket.id
@@ -33,7 +40,10 @@ io.on('connection', function (socket) {
     socket.emit('connect_successful', _.pluck(users, 'guid'));
     socket.broadcast.emit('online', guid);
 
+    io.to(socket).emit('connect', guid);
+
     console.log(guid + ' logged in');
+
   });
 
   socket.on('sendMessage', function (guid, message) {
@@ -51,7 +61,47 @@ io.on('connection', function (socket) {
     
     io.to(contact.socket)
       .emit('messageReceived', currentUser.guid, message);
-    console.log(guid + "was sent a " + message.type);
+    console.log(guid + " was sent a " + message.type);
+  });
+
+  socket.on('turnToken', function(guid){
+    var currentUser = _.find(users, { socket: socket.id });
+    if(!currentUser) {
+        console.log("could not find current user.. logged in?");
+        return;
+    }
+
+    twilio.tokens.create(function(err, response){
+        if(err){
+            console.log(err);
+        } else {
+            io.to(currentUser.socket).emit('turnToken', response);
+            console.log(response);
+            console.log(currentUser.guid + ' sent a twillio token');
+        }
+    });
+  });
+
+  socket.on('queue', function(guid){
+    var currentUser = _.find(users, { socket: socket.id });
+    if (!currentUser) {
+        console.log("could not find current user.. logged in?");
+        return;
+    }
+  
+    if(!guid){
+        var queue = _.find(call_queue, { receiver: currentUser.guid });
+        io.to(currentUser.socket)
+              .emit('messageReceived', currentUser.guid, {type:'queue', queue: queue});
+        console.log('sending queue to ' + currentUser.guid);
+        return;
+    }
+    
+    call_queue.push({
+        caller: currentUser.guid, 
+        receiver: guid
+    });
+    console.log("Added " + guid + " to the caller queue");   
   });
 
   socket.on('disconnect', function () {
