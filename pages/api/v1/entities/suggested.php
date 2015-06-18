83c8f0ead26b3bc1fa351ae516fd3b317f46ed6b
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-18 18:27:23 +0000

    removes yet more logging

--- pages/api/v1/entities/suggested.php
+++ pages/api/v1/entities/suggested.php
@@ -102,7 +102,11 @@ class suggested implements interfaces\api, interfaces\ApiIgnorePam{
             
                 if(isset($_GET['nearby']) && $_GET['nearby'] === "true"){
                     //error_log($_GET['coordinates']);
-                    $result= Data\Client::build('Neo4j')->requestRead($prepared->getUserByLocation(Core\session::getLoggedInUser(), isset($_GET['coordinates']) && $_GET['coordinates'] != "false" ? $_GET['coordinates'] : NULL, isset($_GET['distance']) ? $_GET['distance'] : 25, 12, $_GET['skip']));
+                    $p = $prepared->getUserByLocation(Core\session::getLoggedInUser(), isset($_GET['coordinates']) && $_GET['coordinates'] != "false" ? $_GET['coordinates'] : NULL, isset($_GET['distance']) ? $_GET['distance'] : 25, 12, $_GET['skip']);
+                    if($p)
+                        $result= Data\Client::build('Neo4j')->requestRead($p);
+                    else 
+                        $result = false;
                     if(!$result){
                          return Factory::response(array('status'=>'error', 'message'=>'not found'));
                     }
