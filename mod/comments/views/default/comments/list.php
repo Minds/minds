23a5524ff68ce2bdeaf0d0857c023fd757bf1cae
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-18 23:57:14 +0000

    fixes issue with comment

--- mod/comments/views/default/comments/list.php
+++ mod/comments/views/default/comments/list.php
@@ -1,9 +1,11 @@
 <?php
-
 echo '<ul class="minds-comments">';
 if($vars['comments']){
 	foreach($vars['comments'] as $comment){
-		echo '<li class="minds-comment" data-guid="'.$comment->guid.'">';
+        if(!$comment instanceof Minds\plugin\comments\entities\comment){
+               continue;
+        }
+        echo '<li class="minds-comment" data-guid="'.$comment->guid.'">';
 		echo $comment->view();
 		echo '</li>';
 	}
