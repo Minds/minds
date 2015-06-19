e08671488cb3a2a99f84ed2668b7e61f47ef68c6
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-19 13:43:49 +0000

    potential search xss

--- mod/search/views/default/search/filter.php
+++ mod/search/views/default/search/filter.php
@@ -1,6 +1,7 @@
 <?php
 $path = elgg_get_site_url() . 'search/?q=' . get_input('q');
 $path= htmlspecialchars($path);
+$q = htmlspecialchars(get_input('q'));
 
 $categories = elgg_get_site_entity()->categories;
 
@@ -21,11 +22,11 @@ echo elgg_view_menu('filter', array('sort_by' => 'priority', 'class' => 'elgg-me
 
 <ul class="search-filter">
 
-	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= get_input('q') ?>"  class="<?= !get_input('subtype') && !get_input('type') ? 'active' : '' ?> ">All</a></li>
-	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= get_input('q') ?>&type=user" class="<?= get_input('type') == 'user' ? 'active' : '' ?> ">Channels</a></li>
-	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= get_input('q') ?>&subtype=blog" class="<?= get_input('subtype') == 'blog' ? 'active' : '' ?> ">Blogs</a></li>
-	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= get_input('q') ?>&subtype=video" class="<?= get_input('subtype') == 'video' ? 'active' : '' ?> ">Videos</a></li>
-	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= get_input('q') ?>&subtype=image" class="<?= get_input('subtype') == 'image' ? 'active' : '' ?> ">Images</a></li>
-	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= get_input('q') ?>&type=activity" class="<?= get_input('type') == 'activity' ? 'active' : '' ?> ">News</a></li>
+	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= $q ?>"  class="<?= !get_input('subtype') && !get_input('type') ? 'active' : '' ?> ">All</a></li>
+	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= $q ?>&type=user" class="<?= get_input('type') == 'user' ? 'active' : '' ?> ">Channels</a></li>
+	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= $q ?>&subtype=blog" class="<?= get_input('subtype') == 'blog' ? 'active' : '' ?> ">Blogs</a></li>
+	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= $q ?>&subtype=video" class="<?= get_input('subtype') == 'video' ? 'active' : '' ?> ">Videos</a></li>
+	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= $q ?>&subtype=image" class="<?= get_input('subtype') == 'image' ? 'active' : '' ?> ">Images</a></li>
+	<li><a href="<?= elgg_get_site_url() ?>search?q=<?= $q ?>&type=activity" class="<?= get_input('type') == 'activity' ? 'active' : '' ?> ">News</a></li>
 
 </ul>
