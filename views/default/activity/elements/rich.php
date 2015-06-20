2489b536ac0e95c6588475764b7a1822dbe7c322
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-20 22:55:56 +0000

    fixes potential xss on newsposts

--- views/default/activity/elements/rich.php
+++ views/default/activity/elements/rich.php
@@ -5,16 +5,16 @@ if(strpos($activity -> perma_url, parse_url(elgg_get_site_url(), PHP_URL_HOST)) 
 	$target = '_self';
 ?>
 <div class="activity-rich-post">
-	<a href="<?= $activity -> perma_url ?>" target="<?= $target ?>">
+	<a href="<?=  htmlspecialchars($activity->perma_url) ?>" target="<?= $target ?>">
 		
 		<?php if($activity->thumbnail_src){?>
 			<div class="thumbnail-wrapper">
-				<img src="<?= $activity->thumbnail_src ?>" class="thumbnail"/>
+				<img src="<?= htmlspecialchars($activity->thumbnail_src) ?>" class="thumbnail"/>
 			</div>
 		<?php } ?>
 		
-		<h3><?= htmlspecialchars($activity->title, ENT_QUOTES, 'UTF-8') ?></h3>
-		<p><?= htmlspecialchars($activity->blurb, ENT_QUOTES, 'UTF-8') ?></p>
+		<h3><?= strip_tags($activity->title) ?></h3>
+		<p><?= strip_tags($activity->blurb) ?></p>
 		
 		<p class="url"><?= parse_url($activity -> perma_url, PHP_URL_HOST) ?></p>
 	</a>
