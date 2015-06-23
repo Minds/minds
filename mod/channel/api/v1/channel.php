a977bf1c3380406473cccdf268a64c19e35f199c
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-23 17:21:44 +0000

    reverting due to bug

--- mod/channel/api/v1/channel.php
+++ mod/channel/api/v1/channel.php
@@ -122,11 +122,11 @@ class channel implements interfaces\api{
                 break;
             case "banner":
                 //remove all older banners
-                $db = new Core\Data\Call('entities_by_time');
+                /*$db = new Core\Data\Call('entities_by_time');
                 $banners = $db->getRow("object:carousel:user:" . elgg_get_logged_in_user_guid());
                 if($banners){
                     $db->removeRow("object:carousel:user:" . elgg_get_logged_in_user_guid());
-                }
+                }*/
              
                 $item = new \minds\entities\carousel();
                 $item->title = '';
