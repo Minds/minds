ebdb656139054890d594e87fecebbbef188390ad
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-23 15:20:00 +0000

    returns correct result for p2p

--- pages/api/v1/boost.php
+++ pages/api/v1/boost.php
@@ -70,11 +70,12 @@ class boost implements interfaces\api{
                 $db = new Core\Data\Call('entities_by_time');
                 $queue_guids = $db->getRow("boost:channel:" . Core\session::getLoggedinUser()->guid  . ":review");
                 if($queue_guids){
-                    $entities =  core\entities::get(array('guids'=>$queue_guids));
-                    foreach($entities as $guid => $entity){
-                        $entities[$guid]->points = $queue_guids[$guid];   
+                    $entities =  core\entities::get(array('guids'=>array_keys($queue_guids)));
+                    foreach($entities as $guid =>$entity){
+                        $entities[$guid]->points = $queue_guids[$entity->guid];
                     }
                     $response['boosts'] = factory::exportable($entities, array('points'));
+
                 }
                 break;
     	}
