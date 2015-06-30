acd6f7c0ea723cc3416f84c2a44dfdcc84a4d6e1
Mark Harding <mark@marksmusiconline.co.uk>
2015-06-30 12:35:57 +0000

    removes taboola tracking tags

--- views/default/page/elements/head.php
+++ views/default/page/elements/head.php
@@ -75,15 +75,3 @@ if ($metatags) {
 	echo $metatags;
 }
 ?>
-
-<script type="text/javascript">
-  window._taboola = window._taboola || [];
-  _taboola.push({article:'auto'});
-  !function (e, f, u) {
-    e.async = 1;
-    e.src = u;
-    f.parentNode.insertBefore(e, f);
-  }(document.createElement('script'),
-  document.getElementsByTagName('script')[0],
-  'https://cdn.taboola.com/libtrc/minds/loader.js');
-</script>
