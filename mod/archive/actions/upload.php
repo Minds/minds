6dbaa93950ae323f9b3bcde6c8bfb27d20a79870
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-13 13:43:17 +0100

    Code formatting; prevent generic files from being uploaded.

--- mod/archive/actions/upload.php
+++ mod/archive/actions/upload.php
@@ -1,4 +1,5 @@
 <?php
+
 // Get variables
 $title = get_input("title");
 $desc = get_input("description");
@@ -19,209 +20,97 @@ $container_guid = elgg_get_logged_in_user_guid();
 $user_guid = elgg_get_logged_in_user_guid();
 $batch = new minds\plugin\archive\entities\batch(get_input('batch_guid'));
 
-switch($mime_type){
-	case "video":
-		if($guid)
-			$entity = new minds\plugin\archive\entities\video($guid);
-		else
-			$entity = new minds\plugin\archive\entities\video();
-		$entity->title = $title;
-		$entity->description = $desc;
-		$entity->owner_guid = elgg_get_logged_in_user_guid();
-		$entity->license = $license;
-		
-		if(!$guid)
-			$entity->upload($_FILES['fileData']['tmp_name']);
-		$entity->access_id = 2;
-		
-		if($guid = $entity->save()){
+switch ($mime_type) {
+    case "video":
+	if ($guid)
+	    $entity = new minds\plugin\archive\entities\video($guid);
+	else
+	    $entity = new minds\plugin\archive\entities\video();
+	$entity->title = $title;
+	$entity->description = $desc;
+	$entity->owner_guid = elgg_get_logged_in_user_guid();
+	$entity->license = $license;
 
-            $batch->addToList($entity->guid);			
-		    	echo strval($guid);
-			//	system_message(elgg_echo('archive:upload:success'));
-			$activity = new minds\entities\activity();
-			$activity->setCustom('video', array(
-                			'thumbnail_src'=>$entity->getIconUrl(),
-             				'guid'=>$entity->guid))
-				->setTitle($entity->title)
-				->setBlurb($entity->description)
-				->setFromEntity($entity)
-				->save();
+	if (!$guid)
+	    $entity->upload($_FILES['fileData']['tmp_name']);
+	$entity->access_id = 2;
 
-			exit;
-			
-		} else {
-			system_message(elgg_echo('archive:upload:failed'));
-		}
-	
-		break;
-	case "audio":
-        if($guid)
-            $entity = new minds\plugin\archive\entities\audio($guid);
-        else
-            $entity = new minds\plugin\archive\entities\audio();
-        $entity->title = $title;
-        $entity->description = $desc;
-        $entity->owner_guid = elgg_get_logged_in_user_guid();
-        $entity->license = $license;
+	if ($guid = $entity->save()) {
 
-        if(!$guid)
-            $entity->upload($_FILES['fileData']['tmp_name']);
-        $entity->access_id = 2;
-        
-        if($guid = $entity->save()){
-            $batch->addToList($entity->guid);  
-            echo "$guid";
+	    $batch->addToList($entity->guid);
+	    echo strval($guid);
+	    //	system_message(elgg_echo('archive:upload:success'));
+	    $activity = new minds\entities\activity();
+	    $activity->setCustom('video', array(
+			'thumbnail_src' => $entity->getIconUrl(),
+			'guid' => $entity->guid))
+		    ->setTitle($entity->title)
+		    ->setBlurb($entity->description)
+		    ->setFromEntity($entity)
+		    ->save();
 
-            $activity = new minds\entities\activity();
-            $activity->setCustom('video', array(
-                            'thumbnail_src'=> elgg_get_site_url() . 'mod/archive/graphics/wave.png',
-                            'guid'=>$entity->guid))
-                ->setTitle($entity->title)
-                ->setBlurb($entity->description)
-                ->setFromEntity($entity)
-                ->save();
+	    exit;
+	} else {
+	    system_message(elgg_echo('archive:upload:failed'));
+	}
 
-            exit;
-        }
+	break;
+    case "audio":
+	if ($guid)
+	    $entity = new minds\plugin\archive\entities\audio($guid);
+	else
+	    $entity = new minds\plugin\archive\entities\audio();
+	$entity->title = $title;
+	$entity->description = $desc;
+	$entity->owner_guid = elgg_get_logged_in_user_guid();
+	$entity->license = $license;
 
-        echo "failed"; exit;
+	if (!$guid)
+	    $entity->upload($_FILES['fileData']['tmp_name']);
+	$entity->access_id = 2;
 
-        break;	
-	case "image":
-		
-		$image = new minds\plugin\archive\entities\image();
-		
-		if(!$title)
-			$title = $_FILES['fileData']['name'];
-		
-		$image->title = $title;
-		$image->description = $description;
-		$image->container_guid = $container_guid; //the container guid is usually blank, as it is the batch who is control at the upload level
-		$image->batch_guid = get_input('batch_guid');
-		$image->access_id = 2;
-		$image->upload($_FILES['fileData']);
-		$image->createThumbnails();
-		
-		//we don't know of an album yet, this is done by an alternative batch command
-		echo $image->save(get_input('force_public', false));
-		
-		//add this image to the batch
-		$batch->addToList($image->guid);
-		exit;
-		break;
-	default:
-		//anything else should be forwarded to files. Elgg File entity. 
-		if ($guid){
- 			$file = get_entity($guid, 'object');
-		} else {
-        		$file = new ElggFile();
-    		}
+	if ($guid = $entity->save()) {
+	    $batch->addToList($entity->guid);
+	    echo "$guid";
 
-    		$file->subtype = "file";
-		$file->super_subtype = 'archive';
+	    $activity = new minds\entities\activity();
+	    $activity->setCustom('video', array(
+			'thumbnail_src' => elgg_get_site_url() . 'mod/archive/graphics/wave.png',
+			'guid' => $entity->guid))
+		    ->setTitle($entity->title)
+		    ->setBlurb($entity->description)
+		    ->setFromEntity($entity)
+		    ->save();
 
-		// if no title on new upload, grab filename
-		if (empty($title)) {
-        		$title = $_FILES['fileData']['name'];
-    		}
+	    exit;
+	}
 
-    		$file->title = $title;
-    		$file->description = $desc;
- 		   $file->access_id = $access_id;
-	    	$file->container_guid = $container_guid;
-	    	$file->tags = $tags;
-	    	$file->license = $license;
+	echo "failed";
+	exit;
 
-    		$tags = explode(",", $tags);
-    		$file->tags = $tags;
+	break;
+    case "image":
 
-	    // we have a file upload, so process it
-	    if (isset($_FILES['fileData']['name']) && !empty($_FILES['fileData']['name'])) {
+	$image = new minds\plugin\archive\entities\image();
 
-		$prefix = "file/";
-		$filestorename = elgg_strtolower(time().$_FILES['fileData']['name']);
+	if (!$title)
+	    $title = $_FILES['fileData']['name'];
 
+	$image->title = $title;
+	$image->description = $description;
+	$image->container_guid = $container_guid; //the container guid is usually blank, as it is the batch who is control at the upload level
+	$image->batch_guid = get_input('batch_guid');
+	$image->access_id = 2;
+	$image->upload($_FILES['fileData']);
+	$image->createThumbnails();
 
-		$mime_type = $file->detectMimeType($_FILES['fileData']['tmp_name'], $_FILES['fileData']['type']);
-		$file->setFilename($prefix . $filestorename);
-		$file->setMimeType($mime_type);
-		$file->originalfilename = $_FILES['fileData']['name'];
-		$file->simpletype = file_get_simple_type($mime_type);
+	//we don't know of an album yet, this is done by an alternative batch command
+	echo $image->save(get_input('force_public', false));
 
-		//save the space so we can add it to our quota.
-		$file->size = $_FILES['fileData']['size'];
-
-		// Open the file to guarantee the directory exists
-		$file->open("write");
-		$file->close();
-
-		move_uploaded_file($_FILES['fileData']['tmp_name'], $file->getFilenameOnFilestore());
-
-		$guid = $file->save();
-
-		// if image, we need to create thumbnails (this should be moved into a function)
-		if ($guid && $file->simpletype == "image") {
-		    $file->icontime = time();
-
-		    $thumbnail = get_resized_image_from_existing_file($file->getFilenameOnFilestore(), 60, 60, true);
-		    if ($thumbnail) {
-			$thumb = new ElggFile();
-			$thumb->setMimeType($_FILES['fileData']['type']);
-
-			$thumb->setFilename($prefix."thumb".$filestorename);
-			$thumb->open("write");
-			$thumb->write($thumbnail);
-			$thumb->close();
-
-			$file->thumbnail = $prefix."thumb".$filestorename;
-			unset($thumbnail);
-		    }
-
-		    $thumbsmall = get_resized_image_from_existing_file($file->getFilenameOnFilestore(), 153, 153, true);
-		    if ($thumbsmall) {
-			$thumb->setFilename($prefix."smallthumb".$filestorename);
-			$thumb->open("write");
-			$thumb->write($thumbsmall);
-			$thumb->close();
-			$file->smallthumb = $prefix."smallthumb".$filestorename;
-			unset($thumbsmall);
-		    }
-
-		    $thumblarge = get_resized_image_from_existing_file($file->getFilenameOnFilestore(), 600, 600, false);
-		    if ($thumblarge) {
-			$thumb->setFilename($prefix."largethumb".$filestorename);
-			$thumb->open("write");
-			$thumb->write($thumblarge);
-			$thumb->close();
-			$file->largethumb = $prefix."largethumb".$filestorename;
-			unset($thumblarge);
-		    }
-		}
-	    } else {
-		// not saving a file but still need to save the entity to push attributes to database
-		$file->save();
-	    }
-
-	    if ($guid) {
-		$message = elgg_echo("file:saved");
-		system_message($message);
-		//add_to_river('river/object/file/create', 'create', elgg_get_logged_in_user_guid(), $file->guid);
-		echo $file->guid;
-		exit;
-	//        forward($file->getURL());
-	    } else {
-		// failed to save file object - nothing we can do about this
-		$error = elgg_echo("file:uploadfailed");
-		register_error($error);
-		echo $file->guid;
-		exit;
-	    }
-
-	    $container = get_entity($container_guid);
-	    if (elgg_instanceof($container, 'group')) {
-		forward("archive/group/$container->guid/all");
-	    } else {
-		forward("archive/$container->username");
-	    }
+	//add this image to the batch
+	$batch->addToList($image->guid);
+	exit;
+	break;
+    default:
+	exit;
 }
