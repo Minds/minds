4e4c0fb3921f3ba51325d259109ffec688414d1a
Mark Harding <mark@marksmusiconline.co.uk>
2015-07-22 11:21:02 +0000

    videos newsfeed style fix

--- views/default/activity/elements/video.php
+++ views/default/activity/elements/video.php
@@ -4,7 +4,7 @@ $data = is_array($vars['data']) ? $vars['data'] : json_decode(json_encode($vars[
 <div class="activity-video-post">
         <a href="<?= elgg_get_site_url() ?>archive/view/<?= $data['guid']?>">
 
-                <div class="thumbnail-wrapper archive archive-video t1">
+                <div class="thumbnail-wrapper archive archive-video t1" style="margin-left:-5%">
 			<span></span>
                 	<img src="<?= $data['thumbnail_src'] ?>" class="thumbnail"/>
                  </div>
