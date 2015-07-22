68bfc1b5345a2bab3dfc1ba405dbd84c898248aa
Mark Harding <mark@marksmusiconline.co.uk>
2015-07-22 11:14:44 +0000

    rich embed not border

--- views/default/activity/elements/rich.php
+++ views/default/activity/elements/rich.php
@@ -4,7 +4,7 @@ $target = "_blank";
 if(strpos($activity -> perma_url, parse_url(elgg_get_site_url(), PHP_URL_HOST)) !== FALSE)
 	$target = '_self';
 ?>
-<div class="activity-rich-post">
+<div class="activity-rich-post" style="width:100%; margin-left:-2.5%">
 	<a href="<?=  htmlspecialchars($activity->perma_url) ?>" target="<?= $target ?>">
 		
 		<?php if($activity->thumbnail_src){?>
