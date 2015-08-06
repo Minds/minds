40d346db4045e0dbdc5e2378463573abf3f2a235
Marcus Povey <marcus@marcus-povey.co.uk>
2015-08-06 10:33:30 +0100

    Title on video newsfeed

--- views/default/activity/elements/video.php
+++ views/default/activity/elements/video.php
@@ -1,9 +1,10 @@
 <?php
 $data = is_array($vars['data']) ? $vars['data'] : json_decode(json_encode($vars['data']), true);
+$entity = get_entity($data['guid']);
 ?>
 <div class="activity-video-post">
-        <a href="<?= elgg_get_site_url() ?>archive/view/<?= $data['guid']?>">
-
+    <h3><a href="<?= elgg_get_site_url() ?>archive/view/<?= $data['guid']?>"><?= $entity->title;	?></a></h3>
+        <a href="<?= elgg_get_site_url() ?>archive/view/<?= $data['guid']?>">	
                 <div class="thumbnail-wrapper archive archive-video t1" style="margin-left:-5%">
 			<span></span>
                 	<img src="<?= $data['thumbnail_src'] ?>" class="thumbnail"/>
