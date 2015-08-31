e393afb2c392726cb9c2681b618e9ce429f3b5ce
Mark Harding <mark@marksmusiconline.co.uk>
2015-08-31 14:57:38 +0000

    daily reward campaign

--- pages/api/v1/analytics.php
+++ pages/api/v1/analytics.php
@@ -47,6 +47,22 @@ class analytics implements interfaces\api{
         switch($pages[0]){
             case 'open':
                 Helpers\Analytics::increment("app-opens");
+                //CAMPAIGN:: Reward 10 points per day if a user opens their app
+                $db = new Core\Data\Call('entities_by_time');
+                $ts = Helpers\Analytics::buildTS("day", time());
+                $row = $db->getRow("analytics:rewarded:day:$ts", array('offset'=>Core\session::getLoggedinUser()->guid, 'limit'=>1));
+                if(!$row || key($row) != Core\session::getLoggedinUser()->guid){
+                  $db->insert("analytics:rewarded:day:$ts", array(Core\session::getLoggedinUser()->guid => time()));
+
+                  \Minds\plugin\payments\start::createTransaction(Core\session::getLoggedinUser()->guid, 10, Core\session::getLoggedinUser()->guid, "Daily login reward.");
+                  Core\Events\Dispatcher::trigger('notification', 'elgg/hook/activity', array(
+                      'to'=>array(Core\session::getLoggedinUser()->guid),
+                      'from' => 100000000000000519,
+                      'notification_view' => 'custom_message',
+                      'params' => array('message'=>"We gave you 10 points for logging in!"),
+                      'message'=>"We gave you 10 points for logging in!"
+                      ));
+                }
             break;
         }
 
