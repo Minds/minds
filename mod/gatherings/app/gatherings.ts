105f33a12468c791d686553980a0d48037093102
Ignacio Sarnaglia <ignacio@argeniss.com>
2015-09-06 14:55:51 -0300

    more work on gatherings
    Validation for passwords. Store key if everything work fine.
    Check if key is in storage in the constructor of gatherings.

--- mod/gatherings/app/gatherings.ts
+++ mod/gatherings/app/gatherings.ts
@@ -2,10 +2,11 @@ import { Component, View, NgFor, NgIf, NgClass, Observable, FORM_DIRECTIVES} fro
 import { RouterLink } from "angular2/router";
 import { MessengerConversation } from "./messenger-conversation";
 import { MessengerSetup } from "./messenger-setup";
-
+import { Storage } from 'src/services/storage';
 import { Client } from 'src/services/api';
 import { SessionFactory } from 'src/services/session';
 import { Material } from 'src/directives/material';
+
 
 @Component({
   selector: 'minds-gatherings',
@@ -21,15 +22,17 @@ export class Gatherings {
   session = SessionFactory.build();
   conversations : [];
   next : string =  "";
-  setup : boolean = true;
+  setup : boolean = false;
   hasMoreData : boolean =  true;
   inprogress : boolean = false;
   cb : Date = new Date();
   search : {};
   baseMindsURL: string = "http://dev.minds.io";
+  storage: Storage;
 
-	constructor(public client: Client){
-
+  constructor(public client: Client){
+    this.storage = new Storage();
+    this.checkSetup();
     this.loadMore(true, true);
 
   }
@@ -38,300 +41,321 @@ export class Gatherings {
 
   }
 
-  loadMore(refresh: boolean, fakeData: boolean) {
-      var self = this;
-      /*
-			//if (this.inprogress || !storage.get('private-key')) {*/
-      if (this.inprogress){
-				return false;
-			}
-			this.inprogress = true;
-      console.log("loadMore " + refresh);
-			this.client.get('api/v1/conversations', {	limit: 12,offset: this.next, cb: this.cb
-			})
-      .then(function(data) {
-        if (!fakeData){
-          if (!data.conversations) {
-  					self.hasMoreData = false;
-            self.inprogress = false;
-  					return false;
-  				} else {
-  					self.hasMoreData = true;
-  				};
+  checkSetup(){
+    var self = this;
+    var key = this.storage.get('private-key');
+    if (key){
+      this.setup = true;
+    }
+    /*
+    this.client
+    .get('api/v1/channel/me', {})
+    .then(function(me) {
+    console.log("Chat: " + me.channel.chat);
+    if (me.channel.chat) {
+    self.setup = true;
+  }
+})
+.catch(function(error){
+console.log(error);
+});
+*/
+}
 
-  				if (refresh) {
-  					self.conversations = data.conversations;
-  				} else {
-              for (var _i = 0, _a = data.conversations; _i < _a.length; _i++) {
-                      var conversation = _a[_i];
-                      self.conversations.push(conversation);
-                }
-  				}
+loadMore(refresh: boolean, fakeData: boolean) {
+  var self = this;
+  /*
+  //if (this.inprogress || !storage.get('private-key')) {*/
+  if (this.inprogress){
+    return false;
+  }
+  this.inprogress = true;
+  console.log("loadMore " + refresh);
+  this.client.get('api/v1/conversations', {	limit: 12,offset: this.next, cb: this.cb
+  })
+  .then(function(data) {
+    if (!fakeData){
+      if (!data.conversations) {
+        self.hasMoreData = false;
+        self.inprogress = false;
+        return false;
+      } else {
+        self.hasMoreData = true;
+      };
+
+      if (refresh) {
+        self.conversations = data.conversations;
+      } else {
+        for (var _i = 0, _a = data.conversations; _i < _a.length; _i++) {
+          var conversation = _a[_i];
+          self.conversations.push(conversation);
         }
-        else {
-            self.getFakeConversations();
-        }
-
-				self.next = data['load-next'];
-				//this.$broadcast('scroll.infiniteScrollComplete');
-				//this.$broadcast('scroll.refreshComplete');
-				self.inprogress = false;
-			})
-      .catch( function(error) {
-        console.log("got error" + error);
-				self.inprogress = true;
-			});
-		};
-
-		doSearch(query: string) {
-      var self = this;
-      if (!query){
-        console.log("clearing");
-        this.refresh();
-        return true;
       }
-      console.log("searching " + query);
-			this.client.get('api/v1/gatherings/search', {q: query,type: 'user',view: 'json'})
-        .then(function(success) {
-				  self.conversations = success.user[0];
-			  })
-        .catch(function(error){
-          console.log(error);
-      });
-	   };
-
-    showHovercard(conversation: {}){
-      console.log("mouseover " + conversation);
-      conversation.hover = true;
+    }
+    else {
+      self.getFakeConversations();
     }
 
-    doneTyping($event) {
-      console.log("typing " + $event.target.value);
-      if($event.which === 13) {
-        this.doSearch($event.target.value)
-        $event.target.value = null;
-      }
-    };
-    refresh() {
-			this.search = {};
-			this.inprogress = false;
-			this.next = "";
-			this.previous = "";
-			this.cb = new Date();
-			this.hasMoreData = true;
-			this.loadMore(true);
-		};
+    self.next = data['load-next'];
+    //this.$broadcast('scroll.infiniteScrollComplete');
+    //this.$broadcast('scroll.refreshComplete');
+    self.inprogress = false;
+  })
+  .catch( function(error) {
+    console.log("got error" + error);
+    self.inprogress = true;
+  });
+};
 
-    subscribe($index) {
-			this.conversations[$index].subscribed = true;
-			if (!this.conversations[$index].subscriber) {
-				alert({
-					title: 'Subscribed',
-					template: 'You can chat with ' + this.conversations[$index].name + ' when they subscribe to you too'
-				});
-			} else {
-				window.location.href = '#/tab/gatherings/conversations/' + this.conversations[$index].guid + '/' + this.conversations[$index].name;
-			}
-			this.client.post('api/v1/subscribe/' + this.conversations[$index].guid, {})
-        .then(function() {
-			  })
-        .catch(function() {
-			  });
-		};
+doSearch(query: string) {
+  var self = this;
+  if (!query){
+    console.log("clearing");
+    this.refresh();
+    return true;
+  }
+  console.log("searching " + query);
+  this.client.get('api/v1/gatherings/search', {q: query,type: 'user',view: 'json'})
+  .then(function(success) {
+    self.conversations = success.user[0];
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+};
 
-		invite() {
+showHovercard(conversation: {}){
+  console.log("mouseover " + conversation);
+  conversation.hover = true;
+}
 
-			/*$ionicModal.fromTemplateUrl('templates/invite/invite.html', {
-				scope: $scope,
-				animation: 'slide-in-up'
-			}).then(function(modal) {
-				$scope.modal = modal;
-				$scope.modal.show();
-			})*;*/
-			//need to get username first
-			this.client.get('api/v1/channel/' + $rootScope.user_guid, {})
-        .then(function(success) {
-        /*	window.plugins.socialsharing.share("Hey! If you install the Minds app and tag me @" + success.channel.username + " we both get 100 points! \n\n",
-  													'Join Minds and we both get 100 points to go viral!',
-  													null,
-  													$rootScope.node_url
-  													);*/
+doneTyping($event) {
+  console.log("typing " + $event.target.value);
+  if($event.which === 13) {
+    this.doSearch($event.target.value)
+    $event.target.value = null;
+  }
+};
+refresh() {
+  this.search = {};
+  this.inprogress = false;
+  this.next = "";
+  this.previous = "";
+  this.cb = new Date();
+  this.hasMoreData = true;
+  this.loadMore(true);
+};
 
-	      })
-        .catch(function(error) {
+subscribe($index) {
+  this.conversations[$index].subscribed = true;
+  if (!this.conversations[$index].subscriber) {
+    alert({
+      title: 'Subscribed',
+      template: 'You can chat with ' + this.conversations[$index].name + ' when they subscribe to you too'
+    });
+  } else {
+    window.location.href = '#/tab/gatherings/conversations/' + this.conversations[$index].guid + '/' + this.conversations[$index].name;
+  }
+  this.client.post('api/v1/subscribe/' + this.conversations[$index].guid, {})
+  .then(function() {
+  })
+  .catch(function() {
+  });
+};
 
-          });
-		};
-    getFakeConversations(){
-      this.conversations = [{
-      "guid": "100000000000000134",
-      "type": "user",
-      "subtype": false,
-      "time_created": "1348444800",
-      "time_updated": "1380486497",
-      "container_guid": "0",
-      "owner_guid": "100000000000000000",
-      "site_guid": "1",
-      "access_id": "2",
-      "name": "John",
-      "username": "john",
-      "language": "en",
-      "icontime": "1437947637",
-      "legacy_guid": "134",
-      "featured_id": "382261146771525632",
-      "website": "",
-      "briefdescription": "Information wants to be free",
-      "dob": "",
-      "gender": "",
-      "city": "Wilton, CT",
-      "subscribed": true,
-      "subscriber": true,
-      "subscribers_count": 2583,
-      "subscriptions_count": 1628,
-      "unread": 0,
-      "last_msg": 1441287862
-    },
-    {
-      "guid": "100000000000000599",
-      "type": "user",
-      "subtype": false,
-      "time_created": "1349979579",
-      "time_updated": "1378867544",
-      "container_guid": "0",
-      "owner_guid": "100000000000000000",
-      "site_guid": "1",
-      "access_id": "2",
-      "name": "kram",
-      "username": "markna",
-      "language": "en",
-      "icontime": "1435855391",
-      "legacy_guid": "599",
-      "featured_id": false,
-      "website": "",
-      "briefdescription": "test account",
-      "dob": "2015-04",
-      "gender": "male",
-      "city": "NYC",
-      "subscribed": true,
-      "subscriber": true,
-      "subscribers_count": 2600,
-      "subscriptions_count": 36,
-      "unread": 0,
-      "last_msg": 1441277148
-    },
-    {
-      "guid": "481158088242503691",
-      "type": "user",
-      "subtype": false,
-      "time_created": "1440093027",
-      "time_updated": false,
-      "container_guid": "0",
-      "owner_guid": "0",
-      "site_guid": false,
-      "access_id": "2",
-      "name": "3 Feet High and Rising",
-      "username": "Fredrik1991",
-      "language": "en",
-      "icontime": "1440201096",
-      "legacy_guid": false,
-      "featured_id": false,
-      "website": "",
-      "briefdescription": "Anti Austerity - Big on Charity",
-      "dob": "",
-      "gender": "",
-      "city": "Stoke-on-Trent",
-      "subscribed": true,
-      "subscriber": true,
-      "subscribers_count": 38,
-      "subscriptions_count": 158,
-      "unread": 0,
-      "last_msg": 1441044753
-    },
-    {
-      "guid": "100000000000000341",
-      "type": "user",
-      "subtype": false,
-      "time_created": "1349106435",
-      "time_updated": "1380469351",
-      "container_guid": "0",
-      "owner_guid": "100000000000000000",
-      "site_guid": "1",
-      "access_id": "2",
-      "name": "Bill Ottman",
-      "username": "ottman",
-      "language": "en",
-      "icontime": "1437409536",
-      "legacy_guid": "341",
-      "featured_id": "373789547047161856",
-      "website": "",
-      "briefdescription": "Co-creator, Founder, CEO",
-      "dob": "",
-      "gender": "",
-      "city": "NYC",
-      "subscribed": true,
-      "subscriber": true,
-      "subscribers_count": 10796,
-      "subscriptions_count": 4297,
-      "unread": 0,
-      "last_msg": 1440707171
-    },
-    {
-      "guid": "458569479538880512",
-      "type": "user",
-      "subtype": false,
-      "time_created": "1434707483",
-      "time_updated": false,
-      "container_guid": "0",
-      "owner_guid": "0",
-      "site_guid": false,
-      "access_id": "2",
-      "name": "busssard",
-      "username": "busssard",
-      "language": "en",
-      "icontime": "1434710156",
-      "legacy_guid": false,
-      "featured_id": "464134655193395213",
-      "website": "http:\/\/mundivagus.wordpress.com",
-      "briefdescription": "",
-      "dob": "",
-      "gender": "",
-      "city": "",
-      "subscribed": true,
-      "subscriber": true,
-      "subscribers_count": 730,
-      "subscriptions_count": 271,
-      "unread": 0,
-      "last_msg": 1440666401
-    },
-    {
-      "guid": "459039753216471040",
-      "type": "user",
-      "subtype": false,
-      "time_created": "1434819605",
-      "time_updated": false,
-      "container_guid": "0",
-      "owner_guid": "0",
-      "site_guid": false,
-      "access_id": "2",
-      "name": "Paulholtphotography",
-      "username": "Paulholtphotography",
-      "language": "en",
-      "icontime": "1435352220",
-      "legacy_guid": false,
-      "featured_id": false,
-      "website": "",
-      "briefdescription": "",
-      "dob": "",
-      "gender": "male",
-      "city": "Bradford",
-      "subscribed": true,
-      "subscriber": true,
-      "subscribers_count": 526,
-      "subscriptions_count": 6171,
-      "unread": 0,
-      "last_msg": 1440377632
-    }
-  ];
-    }
+invite() {
+
+  /*$ionicModal.fromTemplateUrl('templates/invite/invite.html', {
+  scope: $scope,
+  animation: 'slide-in-up'
+}).then(function(modal) {
+$scope.modal = modal;
+$scope.modal.show();
+})*;*/
+//need to get username first
+this.client.get('api/v1/channel/' + $rootScope.user_guid, {})
+.then(function(success) {
+  /*	window.plugins.socialsharing.share("Hey! If you install the Minds app and tag me @" + success.channel.username + " we both get 100 points! \n\n",
+  'Join Minds and we both get 100 points to go viral!',
+  null,
+  $rootScope.node_url
+);*/
+
+})
+.catch(function(error) {
+
+});
+};
+getFakeConversations(){
+  this.conversations = [{
+    "guid": "100000000000000134",
+    "type": "user",
+    "subtype": false,
+    "time_created": "1348444800",
+    "time_updated": "1380486497",
+    "container_guid": "0",
+    "owner_guid": "100000000000000000",
+    "site_guid": "1",
+    "access_id": "2",
+    "name": "John",
+    "username": "john",
+    "language": "en",
+    "icontime": "1437947637",
+    "legacy_guid": "134",
+    "featured_id": "382261146771525632",
+    "website": "",
+    "briefdescription": "Information wants to be free",
+    "dob": "",
+    "gender": "",
+    "city": "Wilton, CT",
+    "subscribed": true,
+    "subscriber": true,
+    "subscribers_count": 2583,
+    "subscriptions_count": 1628,
+    "unread": 0,
+    "last_msg": 1441287862
+  },
+  {
+    "guid": "100000000000000599",
+    "type": "user",
+    "subtype": false,
+    "time_created": "1349979579",
+    "time_updated": "1378867544",
+    "container_guid": "0",
+    "owner_guid": "100000000000000000",
+    "site_guid": "1",
+    "access_id": "2",
+    "name": "kram",
+    "username": "markna",
+    "language": "en",
+    "icontime": "1435855391",
+    "legacy_guid": "599",
+    "featured_id": false,
+    "website": "",
+    "briefdescription": "test account",
+    "dob": "2015-04",
+    "gender": "male",
+    "city": "NYC",
+    "subscribed": true,
+    "subscriber": true,
+    "subscribers_count": 2600,
+    "subscriptions_count": 36,
+    "unread": 0,
+    "last_msg": 1441277148
+  },
+  {
+    "guid": "481158088242503691",
+    "type": "user",
+    "subtype": false,
+    "time_created": "1440093027",
+    "time_updated": false,
+    "container_guid": "0",
+    "owner_guid": "0",
+    "site_guid": false,
+    "access_id": "2",
+    "name": "3 Feet High and Rising",
+    "username": "Fredrik1991",
+    "language": "en",
+    "icontime": "1440201096",
+    "legacy_guid": false,
+    "featured_id": false,
+    "website": "",
+    "briefdescription": "Anti Austerity - Big on Charity",
+    "dob": "",
+    "gender": "",
+    "city": "Stoke-on-Trent",
+    "subscribed": true,
+    "subscriber": true,
+    "subscribers_count": 38,
+    "subscriptions_count": 158,
+    "unread": 0,
+    "last_msg": 1441044753
+  },
+  {
+    "guid": "100000000000000341",
+    "type": "user",
+    "subtype": false,
+    "time_created": "1349106435",
+    "time_updated": "1380469351",
+    "container_guid": "0",
+    "owner_guid": "100000000000000000",
+    "site_guid": "1",
+    "access_id": "2",
+    "name": "Bill Ottman",
+    "username": "ottman",
+    "language": "en",
+    "icontime": "1437409536",
+    "legacy_guid": "341",
+    "featured_id": "373789547047161856",
+    "website": "",
+    "briefdescription": "Co-creator, Founder, CEO",
+    "dob": "",
+    "gender": "",
+    "city": "NYC",
+    "subscribed": true,
+    "subscriber": true,
+    "subscribers_count": 10796,
+    "subscriptions_count": 4297,
+    "unread": 0,
+    "last_msg": 1440707171
+  },
+  {
+    "guid": "458569479538880512",
+    "type": "user",
+    "subtype": false,
+    "time_created": "1434707483",
+    "time_updated": false,
+    "container_guid": "0",
+    "owner_guid": "0",
+    "site_guid": false,
+    "access_id": "2",
+    "name": "busssard",
+    "username": "busssard",
+    "language": "en",
+    "icontime": "1434710156",
+    "legacy_guid": false,
+    "featured_id": "464134655193395213",
+    "website": "http:\/\/mundivagus.wordpress.com",
+    "briefdescription": "",
+    "dob": "",
+    "gender": "",
+    "city": "",
+    "subscribed": true,
+    "subscriber": true,
+    "subscribers_count": 730,
+    "subscriptions_count": 271,
+    "unread": 0,
+    "last_msg": 1440666401
+  },
+  {
+    "guid": "459039753216471040",
+    "type": "user",
+    "subtype": false,
+    "time_created": "1434819605",
+    "time_updated": false,
+    "container_guid": "0",
+    "owner_guid": "0",
+    "site_guid": false,
+    "access_id": "2",
+    "name": "Paulholtphotography",
+    "username": "Paulholtphotography",
+    "language": "en",
+    "icontime": "1435352220",
+    "legacy_guid": false,
+    "featured_id": false,
+    "website": "",
+    "briefdescription": "",
+    "dob": "",
+    "gender": "male",
+    "city": "Bradford",
+    "subscribed": true,
+    "subscriber": true,
+    "subscribers_count": 526,
+    "subscriptions_count": 6171,
+    "unread": 0,
+    "last_msg": 1440377632
+  }
+];
+}
 
 }
--- mod/gatherings/app/messenger-setup.ts
+++ mod/gatherings/app/messenger-setup.ts
@@ -2,7 +2,9 @@ import { Component, View, NgFor, NgIf, NgClass, Observable, FORM_DIRECTIVES} fro
 import { RouterLink } from "angular2/router";
 import { Client } from 'src/services/api';
 import { SessionFactory } from 'src/services/session';
+import { Storage } from 'src/services/storage';
 import { Material } from 'src/directives/material';
+
 
 @Component({
   selector: 'minds-messenger-setup',
@@ -15,14 +17,78 @@ import { Material } from 'src/directives/material';
 
 export class MessengerSetup {
   session = SessionFactory.build();
+  configured: boolean = false;
+  data: {};
+  storage: Storage;
 
-	constructor(public client: Client){
-	}
+  constructor(public client: Client){
+    this.storage = new Storage();
+  }
 
-  setup(passwords){
+  setupTest(passwords){
     console.log(passwords);
+    console.log(passwords.value);
     passwords.value = {};
     return true;
   }
+  unlock(password) {
+    console.log("UNLOCK: "+password);
+    var self = this;
+    this.client.get('api/v1/keys', {
+      password: password.value.password,
+      new_password: 'abc123'
+    })
+    .then(function(data) {
+      if (data.key) {
+        self.storage.set('private-key', data.key);
+        //$state.go('tab.chat');
+      } else {
+        alert({
+          title: 'Ooops..',
+          template: 'We couldn\'t unlock your chat. Please check your password is correct.'
+        });
+      }
+    })
+    .catch(function(error) {
+    });
+  };
 
+  setup(passwords) {
+    var self = this;
+    if (!passwords.value.password1) {
+      alert({
+        title: 'Ooops..',
+        template: 'You must enter a password.'
+      });
+      console.log("You must enter a password.");
+      return false;
+    }
+    if (passwords.value.password1 != passwords.value.password2) {
+      alert({
+        title: 'Ooops..',
+        template: 'Your passwords must match.'
+      });
+      console.log("Your passwords must match.");
+      return false;
+    }
+
+    this.client.post('api/v1/keys/setup', {
+      password: passwords.value.password1
+    })
+    .then(function(data) {
+      console.log("Data: " + data.key+ " Storage: "+ self.storage);
+      if (data.key) {
+        self.storage.set('private-key', data.key);
+        //$state.go('tab.chat');
+      } else {
+        alert({
+          title: 'Ooops..',
+          template: 'We couldn\'t unlock your chat. Please check your password is correct.'
+        });
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  };
 }
