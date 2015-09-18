5fb67a3cf5a9a5e5606354b046459fa15154dcb8
Mark Harding <mark@marksmusiconline.co.uk>
2015-09-18 20:54:08 +0000

    adds campaigns for push notifications

---
+++ bin/campaigns/push-rewards.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+
+$entities = new Minds\Core\Data\Call('entities');
+$indexes = new Minds\Core\Data\Call('entities_by_time');
+$lu = new Minds\Core\Data\Call('user_index_to_guid');
+
+echo "Sending push reward notification \n";
+
+$config = new Surge\Config(array(
+     'Apple' => array(
+         'cert'=> '/var/secure/apns-production.pem'
+      //   'sandbox'=>true,
+      //   'cert'=> '/var/secure/apns.pem'
+     ),
+     'Google' => array(
+         'api_key' => 'AIzaSyCp0LVJLY7SzTlxPqVn2-2zWZXQKb1MscQ'
+     )));
+
+$count = 0;
+$offset = isset($argv[1]) ? $argv[1] : "";
+while(true){
+    $guids = $indexes->getRow('user', array('limit' => 750, 'offset' => $offset));
+    $users = $entities->getRows(array_keys($guids), array('offset'=>'surge_token', 'limit'=>1));
+    if(count($guids) <= 1)
+        break;
+
+    echo "Notifying ";
+
+    foreach($users as $guid => $data){
+        if(!isset($data['surge_token']))
+          continue;
+
+        $msg = "Use Minds today to get 10 free points!";
+        $message = Surge\Messages\Factory::build($data['surge_token'])
+            ->setTitle($msg)
+            ->setMessage($msg)
+            ->setURI('newsfeed')
+            ->setSound('default');
+
+        //Surge\Surge::send($message, $config);
+
+        $count++;
+        echo ".";
+    }
+
+    end($guids);
+    $offset = key($guids);
+    echo "Done up to $offset \n";
+
+}
+echo "\n Sent $count push notifications \n";
+
