2cd470d2660edd03776a7ea5a79e033c011f8154
Mark Harding <mark@kramnorth.com>
2015-09-25 10:04:00 +0100

    activity objects now use reusable thumbs and remind buttons

---
+++ front/app/src/components/buttons/comment.ts
@@ -0,0 +1,30 @@
+import { Component, View, CORE_DIRECTIVES } from 'angular2/angular2';
+import { Client } from "src/services/api";
+
+
+@Component({
+  selector: 'minds-button-comment',
+  properties: ['_object: object']
+})
+@View({
+  template: `
+    <a class="mdl-color-text--blue-grey-800" (click)="remind()">
+      <i class="material-icons">chat_bubble</i>
+      <counter [ng-class]="{'has-value': object['comments:count'] > 0}">{{object['comments:count']}}</counter>
+    </a>
+  `,
+  directives: [CORE_DIRECTIVES]
+})
+
+export class CommentButton {
+
+  object;
+
+  constructor(public client : Client) {
+  }
+
+  set _object(value : any){
+    this.object = value;
+  }
+
+}
