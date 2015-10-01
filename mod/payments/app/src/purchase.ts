34d2e1111268138fa0e1b1323fc3a54b881bcb39
Mark Harding <mark@kramnorth.com>
2015-10-01 11:45:41 +0100

    adds views to purchase card

--- mod/payments/app/src/purchase.ts
+++ mod/payments/app/src/purchase.ts
@@ -1,68 +1,40 @@
-import { Component, View, NgFor, NgIf, NgClass, Observable, FORM_DIRECTIVES} from 'angular2/angular2';
+import { Component, View, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
 import { RouterLink } from "angular2/router";
 import { Client } from 'src/services/api';
 import { SessionFactory } from 'src/services/session';
-import { Material } from 'src/directives/material';
+import { MDL_DIRECTIVES } from 'src/directives/material';
 import { InfiniteScroll } from 'src/directives/infinite-scroll';
 
 @Component({
-  selector: 'minds-wallet',
+  selector: 'minds-wallet-purchase',
   viewBindings: [ Client ]
 })
 @View({
-  templateUrl: 'templates/plugins/payments/wallet.html',
-  directives: [ NgFor, NgIf, NgClass, Material, FORM_DIRECTIVES, InfiniteScroll ]
+  templateUrl: 'templates/plugins/payments/purchase.html',
+  directives: [ CORE_DIRECTIVES, MDL_DIRECTIVES, FORM_DIRECTIVES, InfiniteScroll ]
 })
 
-export class Wallet {
+export class WalletPurchase {
 
   session = SessionFactory.build();
-  points : Number = 0;
-  transactions : Array<any> = [];
-  offset: string = "";
-  inProgress : boolean = false;
-  moreData : boolean = true;
-
-	constructor(public client: Client){
-    this.getBalance();
-    this.loadTransactions();
-	}
-
-  getBalance(){
-    var self = this;
-    this.client.get('api/v1/wallet/count', {})
-      .then((response : any) => {
-        self.points = response.count
-        });
+  card : any = {
+    number: "",
+    type: "",
+    name: "",
+    expiry: "",
+    cvc: ""
   }
 
-  loadTransactions(refresh : boolean = false){
-    var self = this;
-    this.inProgress = true;
-    this.client.get('api/v1/wallet/transactions', { limit: 12, offset: this.offset})
-      .then((response : any) => {
+	constructor(public client: Client){
 
-        if(!response.transactions){
-          self.moreData = false;
-          self.inProgress = false;
-          return false;
-        }
+	}
 
-        if(refresh){
-          self.transactions = response.transactions
-        } else {
-          if(self.offset)
-            response.transactions.shift();
-          for(let transaction of response.transactions)
-            self.transactions.push(transaction);
-        }
+  validate(){
 
-        self.offset = response['load-next'];
-        self.inProgress = false;
-      })
-      .catch((e)=>{
+  }
 
-      });
+  purchase(){
+
   }
 
 }
