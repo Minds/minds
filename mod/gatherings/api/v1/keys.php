aee774214dfe0814cfd54566e0c03d44c69e7c0a
Mark Harding <mark@kramnorth.com>
2015-10-02 15:05:59 +0100

    fixes a mobile glitch with encryption passwords

--- mod/gatherings/api/v1/keys.php
+++ mod/gatherings/api/v1/keys.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Minds Newsfeed API
- * 
+ *
  * @version 1
  * @author Mark Harding
  */
@@ -18,77 +18,94 @@ class keys implements interfaces\api{
     /**
      * Returns the private key belonging to a user
      * @param array $pages
-     * 
+     *
      * API:: /v1/keys
-     */      
+     */
     public function get($pages){
 
-       // $_SESSION['user'] = new \Minds\entities\user($_SESSION['user']->guid, false);        
+        $response = array();
+       // $_SESSION['user'] = new \Minds\entities\user($_SESSION['user']->guid, false);
         $unlock_password = get_input('password');
         $new_password = get_input('new_password');
-        $tmp = helpers\openssl::temporaryPrivateKey(\elgg_get_plugin_user_setting('privatekey', elgg_get_logged_in_user_guid(), 'gatherings'), $unlock_password, NULL);
         $pub = \elgg_get_plugin_user_setting('publickey', elgg_get_logged_in_user_guid(), 'gatherings');
-       
-	    if($tmp){
-            $response['key'] = $tmp;
-        } else {
-            $response['status'] = 'error';
-            $response['message'] = "please check your password";
+        $priv = \elgg_get_plugin_user_setting('privatekey', elgg_get_logged_in_user_guid(), 'gatherings');
+
+        //legacy password update
+        if(helpers\openssl::verify($pub, $priv, $unlock_password) === FALSE){ //hint: this should fail if pswd is set
+          $priv = helpers\openssl::temporaryPrivateKey($priv, $unlock_password, $unlock_password);
+          \elgg_set_plugin_user_setting('privatekey', $priv, elgg_get_logged_in_user_guid(), 'gatherings');
         }
-    
+
+        $tmp = helpers\openssl::temporaryPrivateKey($priv, $unlock_password, NULL);
+
+        if(!$tmp || !$unlock_password){
+          $response['status'] = 'error';
+          $response['message'] = "please check your password";
+        }
+
         return Factory::response($response);
-        
+
     }
-    
+
     public function post($pages){
 
         switch($pages[0]){
             case "setup":
-                $keypair = \Minds\plugin\gatherings\helpers\openssl::newKeypair(get_input('passphrase'));
+                $response = array();
+                $keypair = \Minds\plugin\gatherings\helpers\openssl::newKeypair(get_input('password'));
                 //error_log(print_r($_POST,true));
                 \elgg_set_plugin_user_setting('publickey', $keypair['public'], elgg_get_logged_in_user_guid(), 'gatherings');
                 \elgg_set_plugin_user_setting('option', '1', elgg_get_logged_in_user_guid(), 'gatherings');
                 \elgg_set_plugin_user_setting('privatekey', $keypair['private'], elgg_get_logged_in_user_guid(), 'gatherings');
-         
-                 $tmp = helpers\openssl::temporaryPrivateKey($keypair['private'], get_input('passphrase'), NULL);
-                 $response['key'] = $tmp;             
- 
+
+                if(!isset($_GET['download']) || $_GET['download']){
+                  $tmp = helpers\openssl::temporaryPrivateKey($keypair['private'], get_input('password'), NULL);
+                  $response['key'] = $tmp;
+                } else {
+                  $tmp = helpers\openssl::temporaryPrivateKey($keypair['private'], get_input('password'), $_GET['unlock_password']);
+                }
+
                 break;
             case "unlock":
             default:
+              $response = array();
 
                 $unlock_password = get_input('password');
                 $new_password = get_input('new_password');
-                $tmp = helpers\openssl::temporaryPrivateKey(\elgg_get_plugin_user_setting('privatekey', elgg_get_logged_in_user_guid(), 'gatherings'), $unlock_password, NULL);
                 $pub = \elgg_get_plugin_user_setting('publickey', elgg_get_logged_in_user_guid(), 'gatherings');
-              
-                $enc = base64_encode(helpers\openssl::encrypt("hello", $pub));
-               // $tmp = helpers\openssl::decrypt(base64_decode($enc), $tmp);
-                
-                if($tmp){
-                    $response['key'] = $tmp;
+
+                $enc = helpers\openssl::encrypt("hello", $pub);
+                // $tmp = helpers\openssl::decrypt(base64_decode($enc), $tmp);
+                if(!isset($_POST['download']) || (isset($_POST['download']) && $_POST['download'] === 'true')){
+                  $tmp = helpers\openssl::temporaryPrivateKey(\elgg_get_plugin_user_setting('privatekey', elgg_get_logged_in_user_guid(), 'gatherings'), $unlock_password, NULL);
+                  $response['key'] = $tmp;
                 } else {
+                  $new_pswd = base64_encode(openssl_random_pseudo_bytes(128));
+                  $tmp = helpers\openssl::temporaryPrivateKey(\elgg_get_plugin_user_setting('privatekey', elgg_get_logged_in_user_guid(), 'gatherings'), $unlock_password, $new_pswd);
+
+                }
+
+                if(!$tmp){
                     $response['status'] = 'error';
                     $response['message'] = "please check your password";
                 }
-        }   
- 
-        return Factory::response($response);  
-       
-        
+        }
+
+        return Factory::response($response);
+
+
     }
-    
+
     public function put($pages){
-        
+
         return Factory::response(array());
-        
+
     }
-    
+
     public function delete($pages){
-        
+
         return Factory::response(array());
-        
+
     }
-    
+
 }
-        
--- mod/gatherings/helpers/openssl.php
+++ mod/gatherings/helpers/openssl.php
@@ -2,83 +2,101 @@
 /**
  * Minds OpenSSL helper class
  */
- 
+
 namespace minds\plugin\gatherings\helpers;
 
 class openssl{
-	
+
 	/**
 	 * Returns a new keypair
-	 * 
+	 *
 	 * @param string $password
 	 * @return array - Public & Private
 	 */
 	static public function newKeypair($password = NULL){
-			
+
 		$config = array(
 		    "digest_alg" => "sha512",
 		    "private_key_bits" => 4096,
 		    "private_key_type" => OPENSSL_KEYTYPE_RSA,
 		    'encrypt_key' => true
 		);
-		    
+
 		$res = openssl_pkey_new($config);
-		
+
 		//private key
 		openssl_pkey_export($res, $privKey, $password);
-		
+
 		//public key
 		$pubKey = openssl_pkey_get_details($res);
 		$pubKey = $pubKey["key"];
-		
+
 		return array(
 			'public' => $pubKey,
 			'private' => $privKey
 			);
 	}
-	
+
 	/**
 	 * Encrypts data
-	 * 
+	 *
 	 * @param string $data
 	 * @param string $public_key
 	 * @return string
 	 */
 	static public function encrypt($data, $public_key){
-		
+
 		openssl_public_encrypt($data, $encrypted, $public_key);
-		
+
 		return $encrypted;
-		
+
 	}
-	
+
 	/**
 	 * Decrypt data
-	 * 
+	 *
 	 * @param string $encrypted
 	 * @param string $private_key
 	 * @param string @password - default = ''
 	 * @return string
 	 */
 	static public function decrypt($encrypted, $private_key, $password = NULL){
-			
+
 		openssl_private_decrypt($encrypted, $decrypted, openssl_get_privatekey($private_key, $password));
-		
+
 		return $decrypted;
 	}
-	
+
 	/**
 	 * Return a temporary key
-	 * 
+	 *
 	 * @param string $private_key - the protected private key
 	 * @param string $password - the password to unlock the private key
 	 * @param string $newpass (optional) - the new password for the temporary key
 	 * @return string - the new key
 	 */
 	static public function temporaryPrivateKey($private_key, $password = NULL, $newpass = NULL){
-		$private_key = openssl_get_privatekey($private_key, $password);
-		openssl_pkey_export($private_key, $pkeyout, $newpass);
-	    return $pkeyout;
-    }
-	
+  	$private_key = openssl_get_privatekey($private_key, $password);
+  	openssl_pkey_export($private_key, $pkeyout, $newpass);
+      return $pkeyout;
+  }
+
+  /**
+   * Verify the password for the key works
+   */
+  static public function verify($public_key, $private_key, $unlock_password = NULL){
+    $message = "hello world";
+    $encrypted = self::encrypt($message, $public_key);
+    //should fail
+    $decrypted = self::decrypt($encrypted, $private_key);
+    if($decrypted == $message)
+      return false;
+
+    //should pass
+    $decrypted = self::decrypt($encrypted, $private_key, $unlock_password);
+    if($decrypted == $message)
+      return true;
+
+  }
+
 }
