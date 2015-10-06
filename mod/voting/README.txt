ce3490dea6f4cdcb3cd518f74023dec7322491e8
Mark Harding <mark@kramnorth.com>
2015-10-06 09:06:54 +0100

    remove deprecated voting plugin

--- mod/voting/README.txt
+++
@@ -1,2 +0,0 @@
-Put this in a mod/polls directory in your Elgg install. There are a few plugin
-settings you might want to check out but the defaults make sense for most Elgg sites.
--- mod/voting/views/default/polls/filter.php
+++
@@ -1,4 +0,0 @@
-<div class="clearfloat"></div>
-<div id="poll-container-<?php echo $vars['entity']->guid; ?>" class="poll_post">
-	<?php echo elgg_view('polls/poll_widget_content',$vars); ?>
-</div>
