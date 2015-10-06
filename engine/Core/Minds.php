c1611a9d3bfe453e4621370e9d7f0df95f96aac1
Mark Harding <mark@kramnorth.com>
2015-10-06 09:25:38 +0100

    removes legacy web services

--- engine/Core/Minds.php
+++ engine/Core/Minds.php
@@ -106,7 +106,7 @@ class minds extends base{
 			'private_settings.php', 'sessions.php',
 			'sites.php', 'statistics.php',
 			'user_settings.php', 'users.php', 'upgrade.php', 'views.php',
-			'web_services.php', 'widgets.php', 'xml.php', 'xml-rpc.php',
+			'widgets.php', 'xml.php', 'xml-rpc.php',
 
 			// backward compatibility
 			'deprecated-1.7.php', 'deprecated-1.8.php',
