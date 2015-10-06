c244c817b3bdeec37f397753b5f63172da6e78ee
Mark Harding <mark@kramnorth.com>
2015-10-06 09:08:05 +0100

    removes deprecated persona plugin

--- mod/persona/views/default/persona/login.php
+++
@@ -1,5 +0,0 @@
-<div id="persona_login_container">
-    <a href="#" id="persona_login" onclick="navigator.id.request(); return false;">
-	<img src="<?php echo elgg_get_site_url() . 'mod/persona/graphics/email_sign_in_blue.png' ?>" alt="<?php echo elgg_echo('persona:login'); ?>" />
-    </a>
-</div>
