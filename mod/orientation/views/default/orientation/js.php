d0bf5bc1d131a3440b5e7dce8703c72d312b0e61
Mark Harding <mark@kramnorth.com>
2015-10-06 09:02:22 +0100

    remove deprecated orientation plugin

--- mod/orientation/views/default/orientation/js.php
+++
@@ -1,7 +0,0 @@
-<?php if(0){ ?><script type="application/javascript"><?php }?>
-
-$('.orientation-content .connect-network').on('click', function(e){
-	e.preventDefault();
-	
-	window.open($(this).attr('href'), "Authorize", "width=1200,height=500");
-});
