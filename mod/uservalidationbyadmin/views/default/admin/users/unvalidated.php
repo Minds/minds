9f7123b76e6c7ca971b0abe834c6882cf3942f6c
Mark Harding <mark@kramnorth.com>
2015-10-06 09:07:44 +0100

    removes deprecated admin validation plugin

--- mod/uservalidationbyadmin/views/default/admin/users/unvalidated.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * List of unvalidated users
- */
-
-echo elgg_view_form('uservalidationbyadmin/bulk_action', array(
-	'id' => 'uservalidationbyadmin-form',
-	'action' => 'action/uservalidationbyadmin/bulk_action'
-));
