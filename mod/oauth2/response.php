b153583da7fd9576eb99656cc6eeeb5e7cbf575d
Mark Harding <mark@marksmusiconline.co.uk>
2015-10-07 15:23:51 +0000

    removes irritating oauth debug

--- mod/oauth2/response.php
+++ mod/oauth2/response.php
@@ -12,7 +12,7 @@ class response extends OAuth2\Response{
     public function setError($statusCode, $name, $description = null, $uri = null){
         parent::setError($statusCode, $name, $description, $uri);
         
-        error_log("OAuth-DEBUG:: Status: $statusCode. Name: $name. Description:". print_r($description, true));
+ //       error_log("OAuth-DEBUG:: Status: $statusCode. Name: $name. Description:". print_r($description, true));
         
     }
    
