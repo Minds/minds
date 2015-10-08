0e8690c1bbda2757f33a5432554d6e5eed457ffe
Mark Harding <mark@kramnorth.com>
2015-10-08 14:32:23 +0100

    (fix): fixes regex and rendering of urls #181

--- front/app/src/directives/tags.ts
+++ front/app/src/directives/tags.ts
@@ -8,21 +8,25 @@ import { Directive, Inject, ElementRef }  from 'angular2/angular2';
 export class TagsLinks {
 
   element : any;
+  rendered : boolean = false;
 
   constructor(@Inject(ElementRef) _element : ElementRef) {
     this.element = _element.nativeElement;
     setTimeout(() => {
-  //    this.render();
+      this.render();
     });
   }
 
   render() {
 
+    if(!this.rendered)
+      return;
+
     var value = this.element.innerHTML;
 
     //<a>tag
-    //var url = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    //value = value.replace(url, '<a href="$1" target="_blank"  class="mdl-color-text--blue-grey-600">$1</a>');
+    var url = /(\b(?:https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+    value = value.replace(url, '<a href="$1" target="_blank"  class="mdl-color-text--blue-grey-600">$1</a>');
 
     //#hashtag
     var hash = /(^|\s)#(\w*[a-zA-Z_]+\w*)/gim;
@@ -33,6 +37,7 @@ export class TagsLinks {
     value = value.replace(at, '$1<a href="/$2" target="_blank" class="mdl-color-text--blue-grey-500">@$2</a>');
 
     this.element.innerHTML = value;
+    this.rendered = true;
   }
 
 }
