ec916f4e9eda1f7210c1d9ce2b82dc1a54576c05
Mark Harding <mark@kramnorth.com>
2015-10-25 16:05:35 +0000

    (fix): adds back components

---
+++ Components/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * A base object for plugins
+ * 
+ * 
+ * @todo this is a work in progress and will replace the ElggPlugin object
+ */
+ 
+namespace Minds\Components;
+
+class Plugin extends \ElggPlugin{
+	
+	public function start($flags = null){
+		//only legacy plugins use the start function
+		$this->registerViews();
+		$this->registerLanguages();
+	}
+	
+	public function init(){
+		
+	}
+	
+}
