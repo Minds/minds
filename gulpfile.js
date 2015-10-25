3a7c90f743280effe88b03fdf864073b4a2fb25b
Mark Harding <mark@kramnorth.com>
2015-10-25 15:05:24 +0000

    (fix): gulp init fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,60 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
+var runSequence = require('run-sequence');
 var shell = require('gulp-shell');
 
 var PLUGINS_LIST = ['gatherings', 'groups', 'notifications', 'oauth2', 'payments'];
 
-gulp.task('init', ['clone', 'install'], function(){
-
+gulp.task('init', function(done){
+  runSequence('clone', 'install', 'build', done);
 });
 
-gulp.task('clone', ['init.clone.front', 'init.clone.engine', 'init.clone.plugins']);
+gulp.task('clone', function(done){
+  runSequence('clone.front', 'clone.engine', 'clone.sockets', 'clone.plugins', done);
+});
 
-gulp.task('clone.front', function(){
+gulp.task('clone.front', function(done){
   git.clone('https://github.com/minds/front.git',  { args: './front' }, function(err){
+    if(!err)
+      return done();
     console.error(err);
-  })
+    process.exit(1);
+  });
 });
 
-gulp.task('clone.engine', function(){
+gulp.task('clone.engine', function(done){
   git.clone('https://github.com/minds/engine.git',  { args: './engine' }, function(err){
+    if(!err)
+      return done();
     console.error(err);
+    process.exit(1);
   })
 });
 
-gulp.task('clone.plugins', function(){
+gulp.task('clone.sockets', function(done){
+  git.clone('https://github.com/minds/sockets.git',  { args: './sockets' }, function(err){
+    if(!err)
+      return done();
+    console.error(err);
+    process.exit(1);
+  })
+});
+
+gulp.task('clone.plugins', function(done){
   //coming soon
+  done();
 });
 
 gulp.task('install', ['install.front', 'install.engine']);
 
-gulp.task('install.front', shell.task([
+gulp.task('install.front', ['install.front-typings'], shell.task([
   'cd front; npm install;'
+]));
+
+gulp.task('install.front-typings', shell.task([
+  'tsd reinstall --overwrite',
+  'tsd link',
+  'tsd rebundle'
 ]));
 
 gulp.task('install.engine', shell.task([
