bde4ef211228f1eabd00edc512cf20959506f949
Mark Harding <mark@kramnorth.com>
2015-11-09 16:18:00 +0000

    (fix): video subscription owner fix

--- Core/Events/Defaults.php
+++ Core/Events/Defaults.php
@@ -20,45 +20,46 @@ class Defaults
     public function init()
     {
 
-    //Channel object reserializer
-    Dispatcher::register('export:extender', 'all', function ($event) {
+        //Channel object reserializer
+        Dispatcher::register('export:extender', 'all', function ($event) {
             $params = $event->getParameters();
             $export = array();
             if ($params['entity']->ownerObj && is_array($params['entity']->ownerObj)) {
+                $export['ownerObj'] = Entities\Factory::build($params['entity']->ownerObj)->export();
                 //$export['ownerObj'] = \Minds\Helpers\Export::sanitize($params['entity']->ownerObj);
               //  $export['ownerObj']['guid'] = (string) $params['entity']->ownerObj['guid'];
               $event->setResponse($export);
             }
         });
 
-    //Comments count export extender
-    Dispatcher::register('export:extender', 'all', function ($event) {
-        $params = $event->getParameters();
-        $export = array();
-        $cacher = Core\Data\cache\factory::build();
+        //Comments count export extender
+        Dispatcher::register('export:extender', 'all', function ($event) {
+            $params = $event->getParameters();
+            $export = array();
+            $cacher = Core\Data\cache\factory::build();
 
-                if ($params['entity']->type != 'activity') {
-                    return false;
-                }
+            if ($params['entity']->type != 'activity') {
+                return false;
+            }
 
-        $db = new Core\Data\Call('entities_by_time');
-                if ($params['entity']->entity_guid) {
-                    $guid = $params['entity']->entity_guid;
-                } else {
-                    $guid = $params['entity']->guid;
-                }
+            $db = new Core\Data\Call('entities_by_time');
+            if ($params['entity']->entity_guid) {
+                $guid = $params['entity']->entity_guid;
+            } else {
+                $guid = $params['entity']->guid;
+            }
 
-              $cached = $cacher->get("comments:count:$guid");
-                if ($cached !== false) {
-                    $count = $cached;
-                } else {
-                    $count = $db->countRow("comments:$guid");
-                    $cacher->set("comments:count:$guid", $count);
-                }
+            $cached = $cacher->get("comments:count:$guid");
+            if ($cached !== false) {
+                $count = $cached;
+            } else {
+                $count = $db->countRow("comments:$guid");
+                $cacher->set("comments:count:$guid", $count);
+            }
 
-        $export['comments:count'] = $count;
-        $event->setResponse($export);
-    });
+            $export['comments:count'] = $count;
+            $event->setResponse($export);
+        });
     }
 
     public static function _()
