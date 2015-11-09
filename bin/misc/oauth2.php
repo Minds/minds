60b233022a7dfe3027eb21767e003550fb3dca1f
Mark Harding <mark@kramnorth.com>
2015-11-09 14:14:30 +0000

    (feat): oauth setup hack

---
+++ bin/misc/oauth2.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
+
+$entity = new minds\plugin\oauth2\Entities\client();
+$entity->subtype    = 'oauth2_client';
+$entity->owner_guid = 0;
+$entity->access_id  = ACCESS_PRIVATE;
+
+$entity->title       = "test";
+$entity->description = "test oauth";
+
+$entity->client_id     = $entity->guid;
+$entity->client_secret = \minds\plugin\oauth2\start::generateSecret();
+
+if (!$entity->save()) {
+    echo "failed to set oauth keys... \n";
+}
+
+
+echo "\n\n Your keys are: \n";
+echo "client_id: $entity->guid \n";
+echo "client_secret: $entity->client_secret \n"; 
