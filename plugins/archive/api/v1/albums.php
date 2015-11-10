8924f3e7491ead295b94f794812db973027b5f17
Mark Harding <mark@kramnorth.com>
2015-11-10 11:51:08 +0000

    (fix): albums api endpoint now supported

--- plugins/archive/api/v1/albums.php
+++ plugins/archive/api/v1/albums.php
@@ -22,24 +22,51 @@ class albums implements Interfaces\Api{
      * API:: /v1/archive/albums || :guid
      */
     public function get($pages){
-        $response = array();
-        $album_guid = $pages[0];
+        $response = [];
 
-        $db = new Core\Data\Call('entities_by_time');
-        $guids = $db->getRow("object:container:$album_guid", array(
-          'limit' => isset($_GET['limit']) ? $_GET['limit'] : 12,
-          'offset' => isset($_GET['offset']) ? $_GET['offset'] : ""
-        ));
-        if(isset($_GET['offset']))
-          unset($guids[$_GET['offset']]);
+        if(!isset($pages[0]))
+            $pages = ['list'];
 
-        if(!$guids){
-          return Factory::response();
+        switch($pages[0]){
+            case "list":
+
+                $entities = Core\Entities::get([
+                  'subtype' => 'album',
+                  'owner_guid' => Core\Session::getLoggedInUser()->guid
+                ]);
+
+                if(!$entities){
+                  $album = new entities\album();
+                  $album->title = "My Album";
+                  $album->save();
+                  $entities = [$album];
+                }
+
+                break;
+            case "children":
+            default;
+                if(is_numeric($pages[0]))
+                  $album_guid = $pages[0];
+                else
+                  $album_guid = $pages[1];
+
+                $db = new Core\Data\Call('entities_by_time');
+                $guids = $db->getRow("object:container:$album_guid", [
+                  'limit' => isset($_GET['limit']) ? $_GET['limit'] : 12,
+                  'offset' => isset($_GET['offset']) ? $_GET['offset'] : ""
+                ]);
+                if(isset($_GET['offset']))
+                  unset($guids[$_GET['offset']]);
+
+                if(!$guids){
+                  return Factory::exportable([]);
+                }
+
+                $entities = Core\Entities::get([
+                  'guids' => array_keys($guids)
+                ]);
+
         }
-
-        $entities = Core\Entities::get(array(
-          'guids' => array_keys($guids)
-        ));
 
         if($entities){
           $response["entities"] = Factory::exportable($entities);
@@ -61,14 +88,14 @@ class albums implements Interfaces\Api{
 
         if(!isset($pages[0])){
 
-          $album = new entities\album();
-          $album->title = $_POST['title'];
-          $album->save();
+            $album = new entities\album();
+            $album->title = $_POST['title'];
+            $album->save();
 
-          return Factory::response(array(
-            'guid' => $guid,
-            'album' => $album->export()
-          ));
+            return Factory::response(array(
+              'guid' => $guid,
+              'album' => $album->export()
+            ));
         }
 
         $album = new entities\album($pages[0]);
