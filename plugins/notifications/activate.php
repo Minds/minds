b64a74d4ae23a8c87727fbce656334e8303d8063
Mark Harding <mark@kramnorth.com>
2015-11-16 12:55:11 +0000

    (refactor): remove old notifications plugins

--- plugins/notifications/activate.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** 
- * Create the notification column family with it's indexes.
- */
-
-//create_cfs('notification', array('to_guid'=>'UTF-8'));
-
--- plugins/notifications/views/default/notifications/popup.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-echo elgg_view_module('popup', null, 'Loading', array(
-														'id' => 'notification',
-														'class' => 'notifications popup hidden',
-													));
-													
-													
-								
