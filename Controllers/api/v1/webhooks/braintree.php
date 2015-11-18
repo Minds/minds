bfa50885b133e6c535862ba136fcc175188015fa
Mark Harding <mark@marksmusiconline.co.uk>
2015-11-18 11:39:47 +0000

    (feat): notifications for payments callbacks

--- Controllers/api/v1/webhooks/braintree.php
+++ Controllers/api/v1/webhooks/braintree.php
@@ -12,8 +12,8 @@ use Minds\Entities;
 use Minds\Interfaces;
 use Minds\Api\Factory;
 use Minds\Helpers;
+use Minds\Core\Payments;
 
-use Braintree_WebhookNotification;
 use Braintree_WebhookNotification;
 
 class braintree implements Interfaces\Api, Interfaces\ApiIgnorePam{
@@ -31,18 +31,37 @@ class braintree implements Interfaces\Api, Interfaces\ApiIgnorePam{
    */
   public function post($pages){
 
-    $notification = Braintree_WebhookNotification::parse($_POST['btSignature'], $_POST['btPayload']);
+    Payments\Factory::build('braintree');
+   
+
+    $notification = Braintree_WebhookNotification::parse($_POST['bt_signature'], $_POST['bt_payload']);
     error_log(print_r($notification, true));
 
     switch($notification->kind){
-      case Braintree_WebhookNotification::SUB_MERCHANT_ACCOUNT_APPROVED;
-
+      case Braintree_WebhookNotification::SUB_MERCHANT_ACCOUNT_APPROVED:
+        //send a notification to the user letting them know we just approved them
+        $message = "Congrats, you are now a Minds Merchant";
+        Core\Events\Dispatcher::trigger('notification', 'elgg/hook/activity', array(
+              'to'=>[$notification->merchantAccount->id],
+              'from' => 100000000000000519,
+              'notification_view' => 'custom_message',
+              'params' => array('message'=>$message),
+              'message'=>$message
+              ));
         break;
-      case Braintree_WebhookNotification::SUB_MERCHANT_ACCOUNT_DECLINED;
+      case Braintree_WebhookNotification::SUB_MERCHANT_ACCOUNT_DECLINED:
+        $reason = $notification->message;  
+        $message = "Sorry, we could not approve your Merchant Account: $reason";
+        Core\Events\Dispatcher::trigger('notification', 'elgg/hook/activity', array(
+              'to'=>[$notification->merchantAccount->id],
+              'from' => 100000000000000519,
+              'notification_view' => 'custom_message',
+              'params' => array('message'=>$message),
+              'message'=>$message
+              ));
+        break;
     }
 
-
-    return Factory::response($response);
 
   }
 
