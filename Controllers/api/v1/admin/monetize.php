e6afcc708f763c0bcd05aa1440974bb180a9796b
Mark Harding <mark@kramnorth.com>
2015-11-24 14:15:24 +0000

    (feat): monetize api

---
+++ Controllers/api/v1/admin/monetize.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Minds Admin: Monetize
+ *
+ * @version 1
+ * @author Mark Harding
+ *
+ */
+namespace Minds\Controllers\api\v1\admin;
+
+use Minds\Core;
+use Minds\Helpers;
+use Minds\Entities;
+use Minds\Interfaces;
+use Minds\Api\Factory;
+
+class monetize implements Interfaces\Api, Interfaces\ApiAdminPam
+{
+    /**
+     *
+     */
+    public function get($pages)
+    {
+        $response = array();
+        return Factory::response($response);
+    }
+
+    /**
+     * @param array $pages
+     */
+    public function post($pages)
+    {
+        return Factory::response(array());
+    }
+
+    /**
+     * Monetize a post
+     * @param array $pages
+     */
+    public function put($pages)
+    {
+        $entity = Entities\Factory::build($pages[0]);
+
+        if (!$entity) {
+            return Factory::response(array(
+              'status' => 'error',
+              'message' => "Entity not found"
+            ));
+        }
+
+        $entity->monetized = true;
+        $entity->save();
+
+        return Factory::response(array());
+    }
+
+    /**
+     * @param array $pages
+     */
+    public function delete($pages)
+    {
+        $entity = Entities\Factory::build($pages[0]);
+
+        if (!$entity) {
+            return Factory::response(array(
+              'status' => 'error',
+              'message' => "Entity not found"
+            ));
+        }
+
+        $entity->monetized = false;
+        $entity->save();
+
+        return Factory::response(array());
+    }
+}
