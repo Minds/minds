ca64b31dd70a034411963e7be6a6bcbe93919c70
Mark Harding <mark@kramnorth.com>
2015-11-30 13:54:21 +0000

    (fix): adds seo to newsfeed posts

--- Core/SEO/Defaults.php
+++ Core/SEO/Defaults.php
@@ -19,22 +19,39 @@ class Defaults
     public function init()
     {
 
-    /**
-     * Channel default SEO roots
-     */
-    Manager::add('/', function ($slugs = array()) {
-      if (isset($slugs[0]) && is_string($slugs[0])) {
-          $user = new Entities\User($slugs[0]);
-          if (!$user->guid) {
-              return array();
-          }
+        /**
+         * Channel default SEO roots
+         */
+        Manager::add('/', function ($slugs = array()) {
+          if (isset($slugs[0]) && is_string($slugs[0])) {
+              $user = new Entities\User($slugs[0]);
+              if (!$user->guid) {
+                  return array();
+              }
 
-          return $meta = array(
-          'title' => $user->name,
-          'description' => "Subscribe to @$user->username on Minds. " . strip_tags($user->briefdescription)
-        );
-      }
-    });
+              return $meta = array(
+              'title' => $user->name,
+              'description' => "Subscribe to @$user->username on Minds. " . strip_tags($user->briefdescription)
+            );
+          }
+        });
+
+        /**
+         * Activity SEO default
+         */
+        Manager::add('/newsfeed', function ($slugs = []) {
+          if (isset($slugs[0]) && is_numeric($slugs[0])) {
+              $activity = new Entities\Activity($slugs[0]);
+              if (!$activity->guid) {
+                  return [];
+              }
+
+              return $meta = [
+                'title' => $activity->message ?: $activity->title,
+                'description' => "@{$activity->ownerObj['name']} on Minds"
+              ];
+          }
+        });
     }
 
     public static function _()
