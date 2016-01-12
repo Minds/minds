ae1a93bddd2c3b60bda29fe708a47493bd430225
Mark Harding <mark@marksmusiconline.co.uk>
2016-01-12 21:19:38 +0000

    (refactor): removes legacy boost

--- Core/Boost/Network.php
+++ Core/Boost/Network.php
@@ -287,13 +287,8 @@ class Network implements BoostHandlerInterface
             Helpers\Counters::increment(0, "boost_impressions", 1);
             $count = Helpers\Counters::get((string) $data['_id'], "boost_impressions", false);
 
-            if($data['_id']->getTimestamp() > 1451320200){
-                $boost = $this->getBoostEntity($data['guid']);
-                $legacy_boost = false;
-            } else {
-                $entity = new \Minds\Entities\Activity($data['guid']);
-                $legacy_boost = true;
-            }
+            $boost = $this->getBoostEntity($data['guid']);
+            $legacy_boost = false;
 
             if ($count > $impressions) {
 
