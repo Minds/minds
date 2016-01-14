11146c9e532eac6ce7c24d455db333e7927e3666
Mark Harding <mark@kramnorth.com>
2016-01-14 10:11:25 -0500

    (feat): demo of providers)

---
+++ Core/Events/EventsProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Minds Config Provider
+ */
+
+namespace Minds\Core\Config;
+
+use Minds\Core\Di\Provider;
+
+class ConfigProvider extends Provider
+{
+
+    public function register()
+    {
+    }
+
+}
