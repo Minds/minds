49fe06b998e99c81e3d1bcb99526e12f9a34ef82
Mark Harding <mark@marksmusiconline.co.uk>
2016-01-20 23:42:45 +0000

    (fix): ensure comments are given points

--- Controllers/api/v1/comments.php
+++ Controllers/api/v1/comments.php
@@ -95,7 +95,7 @@ class comments implements Interfaces\Api
                     unset($subscribers[$comment->owner_guid]);
                 }
 
-                if($comment->owner_guid != Core\Session::getLoggedinUser()->guid)
+                if($parent->owner_guid != Core\Session::getLoggedinUser()->guid)
                     Helpers\Wallet::createTransaction(Core\Session::getLoggedinUser()->guid, 1, $pages[0], 'comment');
 
                 Core\Events\Dispatcher::trigger('notification', 'all', array(
