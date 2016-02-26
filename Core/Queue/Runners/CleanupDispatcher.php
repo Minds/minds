3acb6b0d589d3fcb2846dfe8ff006a557427f73e
edgebal <edgebal@gmail.com>
2016-02-26 12:48:55 -0300

    (feat): Cleanup queue dispatcher

---
+++ Core/Queue/Runners/CleanupDispatcher.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Cleanup Dispatcher - Used by
+ */
+namespace Minds\Core\Queue\Runners;
+
+use Minds\Core\Queue\Interfaces;
+use Minds\Core\Queue;
+use Minds\Core\Events\Dispatcher;
+
+/**
+* Queued Notifications
+*/
+class CleanupDispatcher implements Interfaces\QueueRunner
+{
+    public function run()
+    {
+        $client = Queue\Client::Build();
+        $client
+        ->setExchange("mindsqueue", "direct")
+        ->setQueue("CleanupDispatcher")
+        ->receive(function ($data) {
+            $type = isset($data['type']) ? $data['type'] : 'all';
+            Dispatcher::trigger('cleanup:dispatch', $type, $data);
+        });
+    }
+}
