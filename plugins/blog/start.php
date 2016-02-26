04e050a7a1a232b7a44fb44836e57db60e9b2183
Mark Harding <mark@marksmusiconline.co.uk>
2016-02-26 10:15:52 +0000

    (fix): prevent https redirect loop

--- plugins/blog/start.php
+++ plugins/blog/start.php
@@ -31,7 +31,7 @@ class start extends Components\Plugin
                 'description' => htmlspecialchars(strip_tags($blog->description)),
                 'og:title' => $blog->title,
                 'og:description' => htmlspecialchars(strip_tags($blog->description)),
-                'og:url' => str_replace($blog->getPermaUrl(), 'https://', 'http://'),
+                'og:url' => str_replace('http://', 'https://', $blog->getPermaUrl()),
                 'og:type' => 'article',
                 'og:image' => $blog->getIconUrl(800),
                 'og:image:width' => 2000,
