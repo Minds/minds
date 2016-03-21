31c97210b719ed829d3e0aa1ea22021b2f5f3829
Mark Harding <mark@kramnorth.com>
2016-03-21 16:17:49 +0000

    (fix): boost fetch fix

--- Controllers/api/v1/boost/fetch.php
+++ Controllers/api/v1/boost/fetch.php
@@ -38,11 +38,7 @@ class fetch implements Interfaces\Api, Interfaces\ApiIgnorePam
                 }
               break;
           case 'newsfeed':
-            //$boosts = Core\Boost\Factory::build($pages[0])->getBoosts(isset($_GET['limit']) ? $_GET['limit'] : 2);
-            $boosts = Core\Entities::get([
-              'type'=>'activity',
-              'owner_guid' => Core\Session::getLoggedinUser()->guid
-            ]);
+            $boosts = Core\Boost\Factory::build($pages[0])->getBoosts(isset($_GET['limit']) ? $_GET['limit'] : 2);
             foreach ($boosts as $entity) {
                 $response['boosts'][] = $entity->export();
                 //bug: sometimes views weren't being calculated on scroll down
