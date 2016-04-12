f600350db0255d88389fb7de06274aee9c0a752d
Mark Harding <mark@kramnorth.com>
2016-04-12 14:52:47 +0100

    (fix): queue provider fallback and namespace used

--- Core/Queue/QueueProvider.php
+++ Core/Queue/QueueProvider.php
@@ -9,6 +9,9 @@ use Minds\Core;
 use Minds\Core\Data;
 use Minds\Core\Di\Provider;
 
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 class QueueProvider extends Provider
 {
 
@@ -19,10 +22,10 @@ class QueueProvider extends Provider
             return new RabbitMQ\Client(
               $config,
               new AMQPConnection(
-                    $config->rabbitmq['host'],
-                    $config->rabbitmq['port'],
-                    $config->rabbitmq['username'],
-                    $config->rabbitmq['password'],
+                    $config->rabbitmq['host'] ?: 'localhost',
+                    $config->rabbitmq['port'] ?: 5672,
+                    $config->rabbitmq['username'] ?: 'guest',
+                    $config->rabbitmq['password'] ?: 'guest',
                     '/'
                 ));
         }, ['useFactory'=>true]);
