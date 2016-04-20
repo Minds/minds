844aea79aeefd23271a6066dd9bfcb083f79fc8d
Mark Harding <mark@kramnorth.com>
2016-04-20 16:27:02 +0100

    (fix): typo fix for seo

--- plugins/Groups/Core/SEO.php
+++ plugins/Groups/Core/SEO.php
@@ -25,11 +25,11 @@ class SEO
 
             return $meta = [
                 'title' => $group->getName(),
-                'description' => $group->getBriefDescription(),
+                'description' => strip_tags($group->getBriefDescription()),
                 'og:title' => $group->getName(),
-                'og:description' => $group->getBriefDescription(),
+                'og:description' => strip_tags($group->getBriefDescription()),
                 'og:url' => Di::_()->get('Config')->site_url . $group->username,
-                'og:image' => Di::_()->get('Config')->cdn_url . 'fs/v1/banner/' . $group->guid,
+                'og:image' => Di::_()->get('Config')->cdn_url . 'fs/v1/banners/' . $group->guid,
                 'og:image:width' => 2000,
                 'og:image:height' => 1000
             ];
