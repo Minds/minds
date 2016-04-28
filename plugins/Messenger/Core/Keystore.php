cd498f348882d682e2777ec4b9c7c30c67c426e7
Mark Harding <mark@kramnorth.com>
2016-04-28 17:57:09 +0100

    (feat): implements a new keystore

---
+++ plugins/Messenger/Core/Keystore.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Minds encryption keystore
+ */
+
+namespace Minds\Plugin\Messenger\Core;
+
+class Keystore
+{
+
+    static public $tmpPrivateKey;
+
+    private $handler;
+    private $user;
+
+    public function __construct($handler = NULL)
+    {
+        $this->handler = $handler ?: new Encryption\OpenSSL();
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getPrivateKey()
+    {
+        return $this->user->{"plugin:user_setting:gatherings:privatekey"};
+    }
+
+    public function getPublicKey()
+    {
+        return $this->user->{"plugin:user_setting:gatherings:publickey"};
+    }
+
+    public function unlockPrivateKey($password = "")
+    {
+         self::$tmpPrivateKey = $this->handler->unlockPrivateKey($this->getPrivteKey, $password);
+         return $this;
+    }
+
+    public function setPublicKey($public)
+    {
+        $this->user->{"plugin:user_setting:gatherings:publickey"} = $public;
+        return $this;
+    }
+
+    public function setPrivateKey($private)
+    {
+        $this->user->{"plugin:user_setting:gatherings:privatekey"} = $private;
+        return $this;
+    }
+
+    public function save()
+    {
+        //todo
+    }
+
+}
--- plugins/Messenger/Spec/Core/Encryption/OpenSSLSpec.php
+++ plugins/Messenger/Spec/Core/Encryption/OpenSSLSpec.php
@@ -5,6 +5,11 @@ namespace Messenger\Spec\Minds\Plugin\Messenger\Core\Encryption;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Minds\Entities\User;
+use Minds\Plugin\Messenger\Entities\Message;
+use Minds\Plugin\Messenger\Core\Conversation;
+use Minds\Plugin\Messenger\Core\Keystore;
+
 class OpenSSLSpec extends ObjectBehavior
 {
 
@@ -12,5 +17,55 @@ class OpenSSLSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Minds\Plugin\Messenger\Core\Encryption\EncryptionInterface');
     }
+
+    function it_should_set_a_message_entity(Message $message)
+    {
+        $this->setMessage($message)->getMessage()->shouldHaveType('Minds\Plugin\Messenger\Entities\Message');
+    }
+
+    function it_should_encrypt_a_message(Message $message, Conversation $conversation, Keystore $keystore,
+      User $user1, User $user2)
+    {
+        $this->beConstructedWith($keystore);
+
+        $keypair = $this->generateKeypair('foobar');
+
+        $keystore->setUser(Argument::any())->shouldBeCalled()->willReturn($keystore);
+        $keystore->getPublicKey()->willReturn($keypair['public']);
+
+        $user1->guid = 'abc';
+        $user2->guid = 'def';
+
+        $conversation->getParticipants()->willReturn(['abc' => $user1, 'def' => $user2]);
+        $this->setConversation($conversation);
+
+        $message->getMessage()->willReturn("helloworld");
+        $message->setMessages(Argument::type('string'), Argument::type('string'))
+          ->shouldBeCalled();
+
+        $this->setMessage($message);
+        $this->encrypt();
+    }
+
+    /*function it_should_decrypt_a_message(Message $message, Keystore $keystore, User $user)
+    {
+        $this->beConstructedWith($keystore);
+
+        $keypair = $this->generateKeypair('foobar');
+
+        $keystore->setUser(Argument::any())->shouldBeCalled()->willReturn($keystore);
+        $keystore->getPrivateKey()->willReturn($keypair['public']);
+
+        $user1->guid = 'abc';
+        $user2->guid = 'def';
+
+        $this->setConversation($conversation);
+
+        $message->getMessage('abc')->willReturn("");
+
+        $this->setUser($user);
+        $this->setMessage($message);
+        $this->encrypt();
+    }*/
 
 }
