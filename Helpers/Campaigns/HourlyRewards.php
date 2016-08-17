4a7dc79d9bc31ae0b42801a4369fa24f9884570d
Mark Harding <mark@marksmusiconline.co.uk>
2016-08-17 09:55:57 +0000

    (chore): language change

--- Helpers/Campaigns/HourlyRewards.php
+++ Helpers/Campaigns/HourlyRewards.php
@@ -34,7 +34,7 @@ class HourlyRewards
             $db->insert("analytics:rewarded:hourly:$ts", array(Core\Session::getLoggedinUser()->guid => time()));
 
             Helpers\Wallet::createTransaction(Core\Session::getLoggedinUser()->guid, 20, Core\Session::getLoggedinUser()->guid, "Hourly reward.");
-            $message = "You have received 20 points as an hourly reward. Stick around receive more points!";
+            $message = "You have received 20 points as an hourly reward. Hit the newsfeed every hour to receive more points!";
             Core\Events\Dispatcher::trigger('notification', 'dailyReward', array(
               'to'=>array(Core\Session::getLoggedinUser()->guid),
               'from' => 100000000000000519,
