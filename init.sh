71a7fc4b0fb56137887e36f0ef42d64b28b45003
Mark Harding <mark@kramnorth.com>
2016-12-12 18:44:19 +0000

    (feat): public install script

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Clone the main repo
+git clone http://github.com/Minds/minds
+
+# Setup the other repos
+cd minds
+git clone http://github.com/Minds/front
+git clone http://github.com/Minds/engine
