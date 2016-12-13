021fc144a252117c3f14c0b19e42d817187212c9
Mark Harding <mark@kramnorth.com>
2016-12-13 13:00:17 +0000

    (feat): api for list of supporters

---
+++ Controllers/api/v1/payments/subscribers.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Minds Payments API - Subscribers
+ *
+ * @version 1
+ * @author Mark Harding
+ */
+namespace Minds\Controllers\api\v1\payments;
+
+use Minds\Core;
+use Minds\Helpers;
+use Minds\Interfaces;
+use Minds\Api\Factory;
+use Minds\Core\Payments;
+
+class subscribers implements Interfaces\Api
+{
+    /**
+     * Returns an entities subscribers
+     * @param array $pages
+     *
+     * API:: /v1/payments/subscribers/:plan
+     */
+    public function get($pages)
+    {
+        if (!isset($pages[0])) {
+            return Factory::response([]);
+        }
+        $offset = isset($_GET['offset']) ? $_GET['offset'] : '';
+        $repo = new Payments\Plans\Repository();
+
+        $guids = $repo
+            ->setEntityGuid(Core\Session::getLoggedInUser()->guid)
+            ->getAllSubscribers($pages[0], [
+                'offset' => $offset
+            ]);
+
+        if ($offset) {
+            array_shift($guids);
+        }
+
+        $response = [];
+
+        if ($guids) {
+            $subscribers = Core\Entities::get([ 'guids' => $guids ]);
+
+            $response['subscribers'] = Factory::exportable($subscribers);
+
+            if ($subscribers) {
+                $response['load-next'] = (string) end($subscribers)->guid;
+            }
+        }
+
+        return Factory::response($response);
+    }
+
+    public function post($pages)
+    {
+        return Factory::response([]);
+    }
+
+    public function put($pages)
+    {
+        return Factory::response([]);
+    }
+
+    public function delete($pages)
+    {
+        return Factory::response([]);
+    }
+}
