2fc1230aa544e86a714385d99730e9d02c60fbbd
Mark Harding <mark@kramnorth.com>
2016-12-21 10:35:00 +0000

    (feat): improved s3 integration

--- Core/Storage/Services/S3.php
+++ Core/Storage/Services/S3.php
@@ -8,10 +8,25 @@ use Minds\Core\Config;
 class S3 implements ServiceInterface
 {
 
+    public $s3;
     public $filepath;
+    public $mode;
+
+    private $modes = [
+      'read',
+      'read-uri',
+      'redirect',
+      'write'
+    ];
 
     public function open($path, $mode)
     {
+
+        if ($mode && !in_array($mode, $this->modes)) {
+            throw new \Exception("$mode is not a supported type");
+        }
+
+        $this->mode = $mode;
 
         $this->s3 = S3Client::factory([
           'key' => Config::_()->aws['key'],
@@ -30,23 +45,41 @@ class S3 implements ServiceInterface
 
     public function write($data)
     {
-        return $this->s3->putObject([
+
+        //TODO: check mime performance here
+        $finfo = new \finfo(FILEINFO_MIME_TYPE);
+        $mimeType = $finfo->buffer($data);
+
+        $write =  $this->s3->putObject([
           'ACL' => 'public-read',
-          'Bucket' => 'cinemr',
+          'Bucket' => Config::_()->aws['bucket'],
           'Key' => $this->filepath,
+          'ContentType' => $mimeType,
           'ContentLength' => strlen($data),
           //'ContentLength' => filesize($file),
           'Body' => $data,
         ]);
+
+        return true;
     }
 
     public function read($length = 0)
     {
-        //get temorary url and return..
-        $url = $this->s3->getObjectUrl("cinemr", $this->filepath, "+15 minutes");
-        var_dump($url); exit;
-        header("Location: $url");
-        //return fread($this->resource, $length);
+
+        switch ($this->mode) {
+            case "read-uri":
+                $url = $this->s3->getObjectUrl("cinemr", $this->filepath, "+15 minutes");
+                return $url;
+                break;
+            case "read":
+            //    break;
+            case "redirect":
+            default:
+                $url = $this->s3->getObjectUrl("cinemr", $this->filepath, "+15 minutes");
+                header("Location: $url");
+                exit;
+        }
+
     }
 
     public function seek($offset = 0)
