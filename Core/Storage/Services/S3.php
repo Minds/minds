effc4a5da2f5ed4f92f4f46a399d9968edec971e
Mark Harding <mark@marksmusiconline.co.uk>
2017-01-24 09:30:15 +0000

    (feat): disable disk fallback for s3

--- Core/Storage/Services/S3.php
+++ Core/Storage/Services/S3.php
@@ -81,13 +81,13 @@ class S3 implements ServiceInterface
             case "redirect":
             default:
                 //for now, check if the file exists, and fallback to disk if not!
-                if (!$this->s3->doesObjectExist(Config::_()->aws['bucket'], $this->filepath)) {
+                /*if (!$this->s3->doesObjectExist(Config::_()->aws['bucket'], $this->filepath)) {
                     $disk = new Disk();
                     $disk->open($this->filepath, 'read');
                     $content = $disk->read();
                     $disk->close();
                     return $content;
-                }
+                }*/
 
                 $url = $this->s3->getObjectUrl(Config::_()->aws['bucket'], $this->filepath, "+15 minutes");
                 header("Location: $url");
