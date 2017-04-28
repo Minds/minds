1aa854e56369041a581328bd161d9dee187206ef
Mark Harding <mark@marksmusiconline.co.uk>
2017-04-28 16:29:09 +0000

    (fix): show rekey on setup

--- plugins/Messenger/app/setup-chat/setup-chat.ts
+++ plugins/Messenger/app/setup-chat/setup-chat.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 
+import { MessengerEncryptionService } from '../encryption/service';
 import { animations } from '../animations';
 import { SessionFactory } from '../../../services/session';
 
@@ -8,10 +9,16 @@ import { SessionFactory } from '../../../services/session';
   selector: 'minds-messenger-setup-chat',
   templateUrl: 'setup-chat.html',
   animations: animations,
-})
+  })
+
 export class MessengerSetupChat {
+
   open: boolean = true;
   attentionNeededTrigger: any;
+  encryption = this.injector.get(MessengerEncryptionService);
+
+  constructor(private injector : Injector) {}
+
 
   toggle() {
     this.open = !this.open;
