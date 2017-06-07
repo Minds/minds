ab9b0d3451910ba07bf83227dcd99054569cf605
alejandro <alejandronegri@outlook.com>
2017-06-07 16:34:30 -0300

    (feat):show visibility on blog cards

--- plugins/blog/app/card/card.ts
+++ plugins/blog/app/card/card.ts
@@ -4,6 +4,8 @@ import { Client } from '../../../services/api';
 import { SessionFactory } from '../../../services/session';
 import { MindsBlogListResponse } from '../../../interfaces/responses';
 import { AttachmentService } from '../../../services/attachment';
+import { ACCESS } from '../../../services/list-options';
+
 
 @Component({
   moduleId: module.id,
@@ -16,15 +18,15 @@ import { AttachmentService } from '../../../services/attachment';
 export class BlogCard {
 
   minds;
-
   blog;
   session = SessionFactory.build();
+  access = ACCESS;
 
-  constructor(public attachment: AttachmentService){
-      this.minds = window.Minds;
+  constructor(public attachment: AttachmentService) {
+    this.minds = window.Minds;
   }
 
-  set _blog(value : any){
+  set _blog(value: any) {
     this.blog = value;
   }
 
