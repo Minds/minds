56f80063df117b6966155569ed60610065051e13
Mark Harding <mark@kramnorth.com>
2017-08-08 11:26:48 +0100

    (feat): return sums via api

---
+++ Controllers/api/v1/wire/sums.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Minds Wire Sums
+ *
+ * @version 1
+ * @author Mark Harding
+ *
+ */
+namespace Minds\Controllers\api\v1\wire;
+
+use Minds\Interfaces;
+use Minds\Api\Factory;
+use Minds\Core;
+use Minds\Core\Wire;
+use Minds\Entities;
+use Minds\Entities\User;
+
+class sums implements Interfaces\Api
+{
+    /**
+     * GET
+     */
+    public function get($pages)
+    {
+        $response = [];
+
+        switch ($pages[0]) {
+            case "receiver":
+                $guid = $pages[1];
+                $method = isset($pages[2]) ? $pages[2] : 'points';
+                $response['method'] = $method;
+                $thirtyDaysAgoTS = (new \DateTime('midnight'))->modify("-30 days");
+                $response['sum'] = Wire\Counter::getSumByReceiver($guid, $method, $thirtyDaysAgoTS);
+                break;
+        }
+
+        return Factory::response($response);
+    }
+
+    /**
+     * POST
+     */
+    public function post($pages)
+    {
+        return Factory::response([]);
+    }
+
+    /**
+     * PUT
+     */
+    public function put($pages)
+    {
+        return Factory::response([]);
+    }
+
+    /**
+     * DELETE
+     */
+    public function delete($pages)
+    {
+        return Factory::response([]);
+    }
+}
