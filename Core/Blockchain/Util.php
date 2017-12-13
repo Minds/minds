cf02ac49ff0078b1ac6870c53ac21d7f56a597e8
eiennohi <malrogf@gmail.com>
2017-12-13 15:22:05 -0300

    (fix): transactions don't have correct amount because of dechex()

--- Core/Blockchain/Util.php
+++ Core/Blockchain/Util.php
@@ -35,7 +35,7 @@ class Util
             return $dec;
         }
 
-        return '0x' . dechex($dec);
+        return '0x' . self::dec_to_hex($dec);
     }
 
     /**
@@ -52,5 +52,41 @@ class Util
         return array_map(function ($part) {
             return '0x' . ltrim($part, '0');
         }, str_split(substr($hex, 2), 64));
+    }
+
+    private static function dec_to_hex($dec)
+    {
+        $sign = ""; // suppress errors
+        if ($dec < 0) {
+            $sign = "-";
+            $dec = abs($dec);
+        }
+
+        $hex = [
+            0 => 0,
+            1 => 1,
+            2 => 2,
+            3 => 3,
+            4 => 4,
+            5 => 5,
+            6 => 6,
+            7 => 7,
+            8 => 8,
+            9 => 9,
+            10 => 'a',
+            11 => 'b',
+            12 => 'c',
+            13 => 'd',
+            14 => 'e',
+            15 => 'f'
+        ];
+
+        $h = null;
+        do {
+            $h = $hex[($dec % 16)] . $h;
+            $dec /= 16;
+        } while ($dec >= 1);
+
+        return $sign . $h;
     }
 }
