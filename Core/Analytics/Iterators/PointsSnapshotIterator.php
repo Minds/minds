ebf0dd8ca766227a7c84fc75b1d6a3ca04dc0433
Mark Harding <mark@kramnorth.com>
2018-01-29 18:16:09 -0500

    (feat): points snapshot email

---
+++ Core/Analytics/Iterators/PointsSnapshotIterator.php
@@ -0,0 +1,153 @@
+<?php
+namespace Minds\Core\Analytics\Iterators;
+
+use Minds\Core;
+use Minds\Core\Entities;
+use Minds\Core\Data;
+use Minds\Core\Analytics\Timestamps;
+
+/**
+ * Iterator that loops through all signups after a set period
+ */
+class PointsSnapshotIterator implements \Iterator
+{
+    private $cursor = -1;
+    private $period = 0;
+
+    private $item;
+
+    private $limit = 400;
+    private $token = "";
+    private $offset = "";
+    private $rows;
+    private $data = [];
+
+    private $valid = true;
+
+    public function __construct($db = null)
+    {
+        $this->db = $db ?: Core\Di\Di::_()->get('Database\Cassandra\Cql');
+        $this->position = 0;
+    }
+
+    /**
+     * Sets the period to cycle through
+     * @param string $period
+     */
+    public function setPeriod($period = null)
+    {
+        $this->period = $period;
+        $this->getUsers();
+    }
+
+    public function setOffset($offset = '')
+    {
+        $this->offset = $offset;
+        $this->getUsers();
+    }
+    
+
+    /**
+     * Fetch all the users who signed up in a certain period
+     * @return array
+     */
+    protected function getUsers()
+    {
+        $timestamps = array_reverse(Timestamps::span($this->period+1, 'day'));
+
+        $prepared = new Data\Cassandra\Prepared\Custom;
+        $prepared->query("SELECT * from entities_by_time where key='points:snapshot' and column1>?", [
+            (string) $this->offset
+        ]);
+        $prepared->setOpts([
+            'page_size' => $this->limit,
+            'paging_state_token' => $this->token
+        ]);
+
+        $rows = $this->db->request($prepared);
+        if (!$rows) {
+           $this->valid = false;
+           return;
+        }
+
+        $this->token = $rows->pagingStateToken();
+
+        $guids = [];
+        foreach ($rows as $row) {
+            $guids[$row['column1']] = $row['value'];
+        }
+
+        $this->valid = true;
+        $users = Entities::get(['guids' => array_keys($guids) ]);
+        
+        $pushed = 0;
+        foreach ($users as $user) {
+            if ($user->time_created < $timestamps[$this->period]) {
+                $user->points_snapshot = $guids[$user->guid];
+                array_push($this->data, $user);
+                $pushed++;
+            }
+        }
+
+        if ($rows->isLastPage()) { 
+            //$this->valid = false;
+            //return;
+        } 
+
+        if (!$pushed) {
+            error_log("no users past period " . date('d-m-Y', end($users)->time_created));
+            $this->getUsers();
+        }
+    }
+
+    /**
+     * Rewind the array cursor
+     * @return null
+     */
+    public function rewind()
+    {
+        if ($this->cursor >= 0) {
+            $this->getUsers();
+        }
+        $this->next();
+    }
+
+    /**
+     * Get the current cursor's data
+     * @return mixed
+     */
+    public function current()
+    {
+        return $this->data[$this->cursor];
+    }
+
+    /**
+     * Get cursor's key
+     * @return mixed
+     */
+    public function key()
+    {
+        return $this->cursor;
+    }
+
+    /**
+     * Goes to the next cursor
+     * @return null
+     */
+    public function next()
+    {
+        $this->cursor++;
+        if (!isset($this->data[$this->cursor])) {
+            $this->getUsers();
+        }
+    }
+
+    /**
+     * Checks if the cursor is valid
+     * @return bool
+     */
+    public function valid()
+    {
+        return $this->valid && isset($this->data[$this->cursor]);
+    }
+}
---
+++ Core/Email/Campaigns/Templates/points-snapshot.tpl
@@ -0,0 +1,41 @@
+<table cellspacing="8" cellpadding="8" border="0" width="558" align="center">
+  <tbody>
+    <tr>
+      <td style="font-size: 16px;
+        line-height: 22px;
+        letter-spacing: 0.2px;
+        font-family: 'Lato', helvetica;">
+        <b>@<?php echo $vars['user']->username?>,</b>
+      </td>
+    </tr>
+        <tr>
+      <td style="font-size: 16px;
+        line-height: 22px;
+        letter-spacing: 0.2px;
+        font-family: 'Lato', helvetica;">
+        <p>Please take a moment to read our latest crypto announcement regarding your Points. Your current balance is <?php echo number_format($vars['points']);?> Points.</p> 
+      </td>
+    </tr>
+    <tr>
+      <td height="50px" style="text-align:center;">
+        <a href="https://www.minds.com/blog/view/804849279664717824?__e_ct_guid=<?= $vars['guid']?>&campaign=<?= $vars['campaign']?>&validator=<?= $vars['validator'] ?>"
+          style="padding:16px 32px; background-color:#4caf50; color:#FFF; text-decoration:none; font-weight:bold; border-radius:3px;">
+          Read more 
+        </a>
+      </td>
+    </tr>   
+  </tbody>
+</table>
+
+<table cellspacing="8" cellpadding="8" border="0" width="558" align="center">
+  <tbody>
+    <tr>
+      <td style="font-size: 16px;
+        line-height: 22px;
+        letter-spacing: 0.2px;
+        font-family: 'Lato', helvetica;">
+       <a href="https://minds.com/?__e_ct_guid=<?= $vars['guid']?>&campaign=<?= $vars['campaign']; ?>&validator=<?= $vars['validator'] ?>" style="text-decoration:none; color:inherit; font-weight:bold; font-style:italic;">The Minds Team</a>
+      </td>
+    </tr>
+  </tbody>
+</table>
