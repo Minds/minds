62e6ecbbdbec5a9edeae1912bb3211ccb91a957a
Mark Harding <mark@marksmusiconline.co.uk>
2018-02-05 16:52:37 +0000

    (chore): use htmlspecialchars vs strip

--- Helpers/Export.php
+++ Helpers/Export.php
@@ -24,8 +24,9 @@ class Export
                 } else {
                     $return[$k] = (string) $v;
                 }
-                $return[$k] = strip_tags(htmlspecialchars_decode($v, ENT_QUOTES));
-                $return[$k] = html_entity_decode($return[$k]);
+                //$return[$k] = strip_tags(htmlspecialchars_decode($v, ENT_QUOTES));
+                //$return[$k] = html_entity_decode($return[$k]);
+                $return[$k] = htmlspecialchars($return[$k]);
             } elseif (is_bool($v)) {
                 $return[$k] = $v;
             } elseif (is_object($v) || is_array($v)) {
