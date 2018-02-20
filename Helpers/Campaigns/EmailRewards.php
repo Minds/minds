9559fbb149088cbfc475bca6e97437de963f11ce
Mark Harding <mark@kramnorth.com>
2018-02-20 12:48:28 -0500

    (feat): wire email

--- Helpers/Campaigns/EmailRewards.php
+++ Helpers/Campaigns/EmailRewards.php
@@ -38,9 +38,8 @@ class EmailRewards
             $points = 100;
             $label = "Check-in bonus";
             break;
-          case "xmas-17":
-          case "announcement-24-01-18":
-          case "announcement-25-01-18":
+          case "announcement-20-02-18":
+          case "announcement-21-02-18":
             $validator = $_GET['validator'];
             if ($validator == sha1($campaign . $user->guid . Config::_()->get('emails_secret'))) {
                 $points = 500;
