9f2eee77399e5e7ecd4634a430e96864b4fdac6b
eiennohi <malrogf@gmail.com>
2018-02-23 10:31:38 -0300

    (feat): Provide MAU and DAU metrics via elasticsearch aggregates

--- Core/Analytics/Metrics/Active.php
+++ Core/Analytics/Metrics/Active.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Minds\Core\Analytics\Metrics;
 
+use DateTime;
+use Minds\Core\Di\Di;
 use Minds\Helpers;
 use Minds\Core;
 use Minds\Core\Analytics\Timestamps;
@@ -12,16 +15,15 @@ use Minds\Interfaces\AnalyticsMetric;
 class Active implements AnalyticsMetric
 {
     private $db;
+    /** @var Core\Data\ElasticSearch\Client */
+    private $client;
     private $namespace = "analytics:";
     private $key;
 
-    public function __construct($db = null)
+    public function __construct($db = null, $client = null)
     {
-        if ($db) {
-            $this->db = $db;
-        } else {
-            $this->db = new Core\Data\Call('entities_by_time');
-        }
+        $this->db = $db ?: new Core\Data\Call('entities_by_time');
+        $this->client = $client ?: Di::_()->get('Database\ElasticSearch');
 
         if (Core\Session::getLoggedinUser()) {
             $this->key = Core\Session::getLoggedinUser()->guid;
@@ -62,25 +64,85 @@ class Active implements AnalyticsMetric
 
     /**
      * Return a set of analytics for a timespan
-     * @param  int    $span - eg. 3 (will return 3 units, eg 3 day, 3 months)
+     * @param  int $span - eg. 3 (will return 3 units, eg 3 day, 3 months)
      * @param  string $unit - eg. day, month, year
-     * @param  int    $timestamp (optional) - sets the base to work off
+     * @param  int $timestamp (optional) - sets the base to work off
      * @return array
      */
     public function get($span = 3, $unit = 'day', $timestamp = null)
     {
-        $timestamps = Timestamps::span($span, $unit);
-        $data = array();
-        foreach ($timestamps as $ts) {
-            try {
-                $data[] = array(
-                    'timestamp' => $ts,
-                    'date' => date('d-m-Y', $ts),
-                    'total' => $this->db->countRow("{$this->namespace}active:$unit:$ts")
-                );
-            } catch (\Exception $e){
-            }
+        $time = null;
+        switch ($unit) {
+            case "day":
+                $time = (new DateTime('midnight'))->modify("-$span days");
+                $interval = '1d';
+                break;
+            case "month":
+                $time = (new DateTime('midnight first day of this month'))->modify("-$span months");
+                $interval = '1M';
+                break;
+            default:
+                throw new \Exception("$unit is not an accepted unit");
         }
+
+        $query = [
+            'index' => 'minds-metrics-*',
+            'type' => 'action',
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            'term' => [
+                                'action' => 'active'
+                            ]
+                        ],
+                        'must' => [
+                            'range' => [
+                                '@timestamp' => [
+                                    'gte' => $time->getTimestamp() * 1000,
+                                    'lt' => time() * 1000,
+                                    'format' => 'epoch_millis'
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'aggs' => [
+                    'counts' => [
+                        'date_histogram' => [
+                            'field' => '@timestamp',
+                            'interval' => $interval,
+                            'min_doc_count' => 1,
+                        ],
+                        'aggs' => [
+                            'uniques' => [
+                                'cardinality' => [
+                                    'field' => 'user_guid.keyword',
+                                    'precision_threshold' => 40000
+                                ]
+                            ]
+                        ]
+                    ]
+
+                ]
+
+            ]
+        ];
+
+        $prepared = new Core\Data\ElasticSearch\Prepared\Search();
+        $prepared->query($query);
+
+        $result = $this->client->request($prepared);
+        
+        $data = [];
+        foreach ($result['aggregations']['counts']['buckets'] as $count) {
+            $data[] = [
+                'timestamp' => $count['key'],
+                'date' => date('d-m-Y', $count['key']),
+                'total' => (int) $count['uniques']['value']
+            ];
+        }
+
         return $data;
     }
 
