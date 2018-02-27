6b7e486ca9568efd963de6e1fbc3b8c4d6be9256
Mark Harding <mark@kramnorth.com>
2018-02-26 19:04:24 -0500

    (chore): 26th feb email

--- Helpers/Campaigns/EmailRewards.php
+++ Helpers/Campaigns/EmailRewards.php
@@ -48,6 +48,17 @@ class EmailRewards
                 echo "Validator failed"; exit;
             }
             break;
+          case "announcement-26-02-18":
+          case "announcement-27-02-18":
+          case "announcement-28-02-18":
+              $validator = $_GET['validator'];
+              if ($validator == sha1($campaign . $user->guid . Config::_()->get('emails_secret'))) {
+                  $points = 1000;
+                  $wire = false;
+              } else {
+                  echo "Validator failed"; exit;
+              }
+              break;
           default:
             return;
         }
