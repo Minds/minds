ae0e19bebb120b81260ce8ab00eda7cc4b288553
Martin Alejandro Santangelo <martin@minds.com>
2018-03-05 17:11:42 -0300

    (feat) deeplinks apple-app-site-association endpoint added (#369)

---
+++ Controllers/deeplinks.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Minds deeplinks (apple universal links)
+ *
+ * @version 1
+ * @author Martin Santangelo
+ */
+namespace Minds\Controllers;
+
+use Minds\Api\Factory;
+use Minds\Interfaces;
+
+class deeplinks implements Interfaces\Api, Interfaces\ApiIgnorePam
+{
+    private $applinks = [
+        'applinks' => [
+            'apps' => [],
+            'details' => [
+                [
+                    'appID' => "35U3998VRZ.com.minds.mobile",
+                    'paths' => [
+                        "*"
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    /**
+     * Apple universal links /apple-app-site-association
+     * 
+     * @param  array $pages
+     * @return mixed|null
+     */
+    public function get($pages)
+    {
+        ob_end_clean();
+
+        header('Content-type: application/json');
+        header("Access-Control-Allow-Origin: *");
+        echo json_encode($this->applinks);
+    }
+
+    /**
+     * Equivalent to HTTP POST method
+     * @param  array $pages
+     * @return mixed|null
+     */
+    public function post($pages)
+    {
+        return Factory::response([]);
+    }
+
+    /**
+     * Equivalent to HTTP PUT method
+     * @param  array $pages
+     * @return mixed|null
+     */
+    public function put($pages)
+    {
+        return Factory::response([]);
+    }
+
+    /**
+     * Equivalent to HTTP DELETE method
+     * @param  array $pages
+     * @return mixed|null
+     */
+    public function delete($pages)
+    {
+        return Factory::response([]);
+    }
+}
