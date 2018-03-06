8a7ed38d61fe54b915ca007b872b1f7f8d5ac0dd
Marcelo <malrogf@gmail.com>
2018-03-06 13:42:54 -0300

    [Sprint/Urchin] (feat): api/v2/analytics/pageview (#359)
    * (feat): api/v2/analytics/pageview
    
    * (fix): use minds cookie vs analyticsId

---
+++ Controllers/api/v2/analytics/pageview.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Minds\Controllers\api\v2\analytics;
+
+use Minds\Api\Factory;
+use Minds\Core;
+use Minds\Core\Di\Di;
+use Minds\Entities;
+use Minds\Helpers\Counters;
+use Minds\Interfaces;
+
+class pageview implements Interfaces\Api, Interfaces\ApiIgnorePam
+{
+    public function get($pages)
+    {
+        return Factory::response([]);
+    }
+
+    public function post($pages)
+    {
+        if (!isset($_POST['url'])) {
+            return Factory::response([
+                'status' => 'error',
+                'message' => 'You must provide a url'
+            ]);
+        }
+
+        $url = $_POST['url'];
+
+        $event = new Core\Analytics\Metrics\Event();
+        $event->setProduct('platform')
+            ->setAction('pageview')
+            ->setRouteUri($url)
+            ->setUserAgent($_SERVER['HTTP_USER_AGENT'])
+            ->setCookieId($_COOKIE['minds']);
+
+        if (Core\Session::isLoggedIn()) {
+            $event->setUserGuid(Core\Session::getLoggedInUser()->guid);
+        }
+        $event->push();
+
+        return Factory::response([]);
+    }
+
+    public function put($pages)
+    {
+        return Factory::response([]);
+    }
+
+    public function delete($pages)
+    {
+        return Factory::response([]);
+    }
+
+}
