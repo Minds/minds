e16b4a7b4ab9aa50cd206ba2d3cb4db844a1c961
Marcelo <malrogf@gmail.com>
2018-03-07 16:00:24 -0300

    (fix): email provider bindings (#364)

--- Core/Email/EmailProvider.php
+++ Core/Email/EmailProvider.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Minds Email Provider
+ */
 
 namespace Minds\Core\Email;
 
@@ -8,6 +11,13 @@ class EmailProvider extends Provider
 {
     public function register()
     {
+        $this->di->bind('Mailer', function ($di) {
+            return new Mailer(new \PHPMailer());
+        }, ['useFactory'=>true]);
+        $this->di->bind('Email\SpamFilter', function ($di) {
+            return new SpamFilter();
+        }, ['useFactory'=>true]);
+
         $this->di->bind('Email\Manager', function ($di) {
             return new Manager();
         }, ['useFactory' => true]);
