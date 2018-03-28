ed5e6b814056a188593546491e9a7db47e5daa1e
Mark Harding <mark@kramnorth.com>
2018-03-28 15:23:36 -0400

    (fix): use twillio vs amazon vs sms

--- Core/Rewards/Join.php
+++ Core/Rewards/Join.php
@@ -40,7 +40,7 @@ class Join
     )
     {
         $this->twofactor = $twofactor ?: Di::_()->get('Security\TwoFactor');
-        $this->sms = $sms ?: Di::_()->get('SMS\SNS');
+        $this->sms = $sms ?: Di::_()->get('SMS\Twillio');
         $this->libphonenumber = $libphonenumber ?: \libphonenumber\PhoneNumberUtil::getInstance();
         $this->config = $config ?: Di::_()->get('Config');
     }
