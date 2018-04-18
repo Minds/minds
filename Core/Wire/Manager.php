eadf9b3947d384b44fee6f520684c86816fa2f51
Marcelo <malrogf@gmail.com>
2018-04-18 15:17:21 -0300

    (fix): correctly format BigNumber in wire notification (#451)

--- Core/Wire/Manager.php
+++ Core/Wire/Manager.php
@@ -394,9 +394,9 @@ class Manager
     {
         $this->queue->setQueue("WireNotification")
             ->send([
-                "amount" => $wire ? 
-                    (string) BigNumber::_($wire->getAmount())->div(10 ** 18)
-                    : (string) BigNumber::_($this->amount)->div(10 ** 18),
+                "amount" => $wire ?
+                    (float) BigNumber::_($wire->getAmount(), 18)->div(10 ** 18)->toString()
+                    : (float) BigNumber::_($this->amount, 18)->div(10 ** 18)->toString(),
                 "sender" => serialize($wire ? $wire->getSender() : $this->sender),
                 "entity" => serialize($wire ? $wire->getEntity() : $this->entity),
                 "subscribed" => $wire ? $wire->isRecurring() : $this->recurring,
