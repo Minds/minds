376f4fbeb7c5eba0a0c1bc9224ff28898c608cb4
Mark Harding <mark@marksmusiconline.co.uk>
2018-04-25 18:56:38 +0000

    (fix): set the full entity of the user vs just id

--- Controllers/api/v1/payments/subscriptions.php
+++ Controllers/api/v1/payments/subscriptions.php
@@ -89,7 +89,8 @@ class subscriptions implements Interfaces\Api
         if ($subscription->getPaymentMethod() == "money") {
             if (strpos($subscription->getId(), 'sub_', 0) >= -1) {
                 if ($subscription->getEntity()->guid) { //if a wire
-                    $subscription->setMerchant($subscription->getEntity()->getMerchant());
+                    $user = new Entities\User($subscription->getEntity()->guid);
+                    $subscription->setMerchant($user->getMerchant());
                 } elseif (Core\Session::getLoggedInUser()->referrer){
                     $referrer = new Entities\User(Core\Session::getLoggedInUser()->referrer);
                     $subscription->setMerchant($referrer->getMerchant());
@@ -101,7 +102,7 @@ class subscriptions implements Interfaces\Api
                 $braintree->cancelSubscription($subscription);
             }
         }
-
+        
         $success = $manager
             ->setSubscription($subscription)
             ->cancel();
