1680e3d8f55c06c699b5025f0db5aa6fe1f9df92
Mark Harding <mark@marksmusiconline.co.uk>
2018-05-17 15:37:31 +0000

    (fix): use more secure random bytes for two factor

--- Core/Security/TwoFactor.php
+++ Core/Security/TwoFactor.php
@@ -30,14 +30,8 @@ class TwoFactor
      */
     public function createSecret($secretLength = 16)
     {
-        $validChars = $this->_getBase32LookupTable();
-        unset($validChars[32]);
-
-        $secret = '';
-        for ($i = 0; $i < $secretLength; $i++) {
-            $secret .= $validChars[array_rand($validChars)];
-        }
-        return $secret;
+        $bytes = openssl_random_pseudo_bytes(64); 
+        return base64_encode($bytes);
     }
 
     /**
@@ -53,7 +47,7 @@ class TwoFactor
             $timeSlice = floor(time() / 30);
         }
 
-        $secretkey = $this->_base32Decode($secret);
+        $secretkey = base64_decode($secret);
 
         // Pack time into binary string
         $time = chr(0).chr(0).chr(0).chr(0).pack('N*', $timeSlice);
@@ -72,19 +66,6 @@ class TwoFactor
 
         $modulo = pow(10, $this->_codeLength);
         return str_pad($value % $modulo, $this->_codeLength, '0', STR_PAD_LEFT);
-    }
-
-    /**
-     * Get QR-Code URL for image, from google charts
-     *
-     * @param string $name
-     * @param string $secret
-     * @return string
-     */
-    public function getQRCodeGoogleUrl($name, $secret)
-    {
-        $urlencoded = urlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
-        return 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl='.$urlencoded.'';
     }
 
     /**
@@ -121,105 +102,4 @@ class TwoFactor
         return $this;
     }
 
-    /**
-     * Helper class to decode base32
-     *
-     * @param $secret
-     * @return bool|string
-     */
-    protected function _base32Decode($secret)
-    {
-        if (empty($secret)) {
-            return '';
-        }
-
-        $base32chars = $this->_getBase32LookupTable();
-        $base32charsFlipped = array_flip($base32chars);
-
-        $paddingCharCount = substr_count($secret, $base32chars[32]);
-        $allowedValues = array(6, 4, 3, 1, 0);
-        if (!in_array($paddingCharCount, $allowedValues)) {
-            return false;
-        }
-        for ($i = 0; $i < 4; $i++) {
-            if ($paddingCharCount == $allowedValues[$i] &&
-                substr($secret, -($allowedValues[$i])) != str_repeat($base32chars[32], $allowedValues[$i])) {
-                return false;
-            }
-        }
-        $secret = str_replace('=', '', $secret);
-        $secret = str_split($secret);
-        $binaryString = "";
-        for ($i = 0; $i < count($secret); $i = $i+8) {
-            $x = "";
-            if (!in_array($secret[$i], $base32chars)) {
-                return false;
-            }
-            for ($j = 0; $j < 8; $j++) {
-                $x .= str_pad(base_convert(@$base32charsFlipped[@$secret[$i + $j]], 10, 2), 5, '0', STR_PAD_LEFT);
-            }
-            $eightBits = str_split($x, 8);
-            for ($z = 0; $z < count($eightBits); $z++) {
-                $binaryString .= (($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48) ? $y:"";
-            }
-        }
-        return $binaryString;
-    }
-
-    /**
-     * Helper class to encode base32
-     *
-     * @param string $secret
-     * @param bool $padding
-     * @return string
-     */
-    protected function _base32Encode($secret, $padding = true)
-    {
-        if (empty($secret)) {
-            return '';
-        }
-
-        $base32chars = $this->_getBase32LookupTable();
-
-        $secret = str_split($secret);
-        $binaryString = "";
-        for ($i = 0; $i < count($secret); $i++) {
-            $binaryString .= str_pad(base_convert(ord($secret[$i]), 10, 2), 8, '0', STR_PAD_LEFT);
-        }
-        $fiveBitBinaryArray = str_split($binaryString, 5);
-        $base32 = "";
-        $i = 0;
-        while ($i < count($fiveBitBinaryArray)) {
-            $base32 .= $base32chars[base_convert(str_pad($fiveBitBinaryArray[$i], 5, '0'), 2, 10)];
-            $i++;
-        }
-        if ($padding && ($x = strlen($binaryString) % 40) != 0) {
-            if ($x == 8) {
-                $base32 .= str_repeat($base32chars[32], 6);
-            } elseif ($x == 16) {
-                $base32 .= str_repeat($base32chars[32], 4);
-            } elseif ($x == 24) {
-                $base32 .= str_repeat($base32chars[32], 3);
-            } elseif ($x == 32) {
-                $base32 .= $base32chars[32];
-            }
-        }
-        return $base32;
-    }
-
-    /**
-     * Get array with all 32 characters for decoding from/encoding to base32
-     *
-     * @return array
-     */
-    protected function _getBase32LookupTable()
-    {
-        return array(
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', //  7
-            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', // 15
-            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', // 23
-            'Y', 'Z', '2', '3', '4', '5', '6', '7', // 31
-            '='  // padding char
-        );
-    }
 }
