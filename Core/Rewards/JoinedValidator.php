88d4a1768b30daf0e14a63c3007cb397602c84a8
narc88 <narc88@gmail.com>
2018-06-25 13:34:44 -0300

    (feat) on joining rewards create and check join metric if not issue 1tk (#526)

---
+++ Core/Rewards/JoinedValidator.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Minds\Core\Rewards;
+
+use Minds\Core\Data\ElasticSearch;
+use Minds\Core\Di\Di;
+use Minds\Entities\User;
+
+class JoinedValidator
+{
+    /** @var Client $client */
+    protected $client;
+
+    /** @var string $hash */
+    protected $hash;
+
+    public function __construct($client = null)
+    {
+        $this->client = $client ?: Di::_()->get('Database\ElasticSearch');
+    }
+
+    /**
+     * @param string $hash
+     * @return JoinedValidators
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+        return $this;
+    }
+
+    /**
+     * Returns true if no user has referred using the provided hash
+     * @return bool
+     */
+    public function validate()
+    {
+        $must = [
+            [
+                'match' => [
+                    'action.keyword' => 'joined',
+                ],
+            ],
+            [
+                'match' => [
+                    'user_phone_number_hash.keyword' => $this->hash,
+                ],
+            ]
+        ];
+
+        $query = [
+            'index' => 'minds-metrics-*',
+            'type' => 'action',
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'must' => $must,
+                    ]
+                ],
+            ]
+        ];
+
+        $prepared = new ElasticSearch\Prepared\Search();
+        $prepared->query($query);
+
+        try {
+            $result = $this->client->request($prepared);
+        } catch (\Exception $e) {
+            return false;
+        }
+        
+        return count($result['hits']['hits']) === 0;
+    }
+}
--- Spec/Core/Rewards/JoinSpec.php
+++ Spec/Core/Rewards/JoinSpec.php
@@ -6,9 +6,12 @@ use Minds\Core\Config;
 use Minds\Core\Data\ElasticSearch\Client;
 use Minds\Core\Di\Di;
 use Minds\Core\Rewards\ReferralValidator;
+
+use Minds\Core\Rewards\JoinedValidator;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Minds\Core\Blockchain\Wallets\OffChain\Transactions;
 use Minds\Core\SMS\SMSServiceInterface;
 use Minds\Core\Security\TwoFactor;
 use libphonenumber\PhoneNumberUtil;
@@ -129,6 +132,7 @@ class JoinSpec extends ObjectBehavior
         User $user,
         Config $config,
         ReferralValidator $validator,
+        JoinedValidator $joinedValidator,
         Client $esClient
     )
     {
@@ -136,7 +140,7 @@ class JoinSpec extends ObjectBehavior
             return $esClient->getWrappedObject();
         }, ['useFactory' => false]);
 
-        $this->beConstructedWith($twofactor, $sms, $libphonenumber, $config, $validator);
+        $this->beConstructedWith($twofactor, $sms, $libphonenumber, $config, $validator, $joinedValidator);
 
         $libphonenumber->parse("+1 929 387 2643")
             ->shouldBeCalled()
@@ -168,6 +172,80 @@ class JoinSpec extends ObjectBehavior
             ->shouldBeCalled()
             ->willReturn(true);
 
+        $joinedValidator->setHash(Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($joinedValidator);
+        $joinedValidator->validate()
+            ->shouldBeCalled()
+            ->willReturn(false);
+
+        $esClient->request(Argument::any())
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $this->getWrappedObject()
+            ->setUser($user)
+            ->setNumber("1 929 387 2643")
+            ->setCode(123456)
+            ->setSecret('secret');
+        $this->confirm()->shouldReturn(true);
+    }
+
+    function it_should_create_transaction_when_join_confirming(
+        TwoFactor $twofactor,
+        SMSServiceInterface $sms,
+        PhoneNumberUtil $libphonenumber,
+        PhoneNumber $phonenumberMock,
+        User $user,
+        Config $config,
+        ReferralValidator $validator,
+        JoinedValidator $joinedValidator,
+        Client $esClient
+    )
+    {
+        Di::_()->bind('Database\ElasticSearch', function ($di) use ($esClient) {
+            return $esClient->getWrappedObject();
+        }, ['useFactory' => false]);
+
+        $this->beConstructedWith($twofactor, $sms, $libphonenumber, $config, $validator, $joinedValidator);
+
+        $libphonenumber->parse("+1 929 387 2643")
+            ->shouldBeCalled()
+            ->willReturn($phonenumberMock);
+
+        $libphonenumber->format($phonenumberMock, Argument::any())
+            ->shouldBeCalled()
+            ->willReturn('+19293872643');
+
+        $twofactor->verifyCode('secret', 123456, 8)
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $user->setPhoneNumber('+19293872643');
+        $user->setPhoneNumberHash(sha1('+19293872643'));
+
+        $user->get('guid')
+            ->shouldBeCalled()
+            ->willReturn('123');
+
+        $user->get('referrer')
+            ->shouldBeCalled()
+            ->willReturn('1234');
+
+        $validator->setHash(Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($validator);
+        $validator->validate()
+            ->shouldBeCalled()
+            ->willReturn(true);
+
+        $joinedValidator->setHash(Argument::any())
+            ->shouldBeCalled()
+            ->willReturn($joinedValidator);
+        $joinedValidator->validate()
+            ->shouldBeCalled()
+            ->willReturn(false);
+
         $esClient->request(Argument::any())
             ->shouldBeCalled()
             ->willReturn(true);
@@ -190,6 +268,7 @@ class JoinSpec extends ObjectBehavior
         ReferralValidator $validator
     )
     {
+
         $this->beConstructedWith($twofactor, $sms, $libphonenumber, $config, $validator);
 
         $libphonenumber->parse("+1 929 387 2643")
