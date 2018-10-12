e470d0e14611c62f6a049247f44d5f450338ab5f
Mark Harding <mark@kramnorth.com>
2018-10-12 13:53:41 +0100

    (feat): add new secrets

--- containers/php-fpm/pull-secrets.sh
+++ containers/php-fpm/pull-secrets.sh
@@ -7,6 +7,15 @@ mkdir /var/secure;
 aws s3 cp $S3_BUCKET/settings.php /var/www/Minds/engine/settings.php
 aws s3 cp $S3_BUCKET/var/secure/email-public.key /var/secure/email-public.key
 aws s3 cp $S3_BUCKET/var/secure/email-private.key /var/secure/email-private.key
+
+# OAuth
+aws s3 cp $S3_BUCKET/var/secure/oauth-priv.key /var/secure/oauth-priv.key
+aws s3 cp $S3_BUCKET/var/secure/oauth-pub.key /var/secure/oauth-pub.key
+
+# Sessions
+aws s3 cp $S3_BUCKET/var/secure/sessions-priv.key /var/secure/sessions-priv.key
+aws s3 cp $S3_BUCKET/var/secure/sessions-pub.key /var/secure/sessions-pub.key
+
 aws s3 cp $S3_BUCKET/var/secure/google.sheets.key.json /var/secure/google.sheets.key.json
 
 chmod -xr /var/secure/
--- containers/php-runners/pull-secrets.sh
+++ containers/php-runners/pull-secrets.sh
@@ -9,6 +9,14 @@ aws s3 cp $S3_BUCKET/var/secure/email-public.key /var/secure/email-public.key
 aws s3 cp $S3_BUCKET/var/secure/email-private.key /var/secure/email-private.key
 aws s3 cp $S3_BUCKET/apns.pem /var/secure/apns-production.pem
 
+# OAuth
+aws s3 cp $S3_BUCKET/var/secure/oauth-priv.key /var/secure/oauth-priv.key
+aws s3 cp $S3_BUCKET/var/secure/oauth-pub.key /var/secure/oauth-pub.key
+
+# Sessions
+aws s3 cp $S3_BUCKET/var/secure/sessions-priv.key /var/secure/sessions-priv.key
+aws s3 cp $S3_BUCKET/var/secure/sessions-pub.key /var/secure/sessions-pub.key
+
 chmod -xr /var/secure/
 
 echo "PULLED SECRETS";
