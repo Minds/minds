0083b3203701415233ef8f71dd39a0d744227839
Marcelo Rivera <malrogf@gmail.com>
2019-04-02 13:32:21 +0000

    [Sprint/Subscribers] (feat): export new admin_badge attribute for frontend badges

--- Entities/User.php
+++ Entities/User.php
@@ -735,6 +735,7 @@ class User extends \ElggUser
         $export['last_accepted_tos'] = (int) $this->getLastAcceptedTOS();
         $export['opted_in_hashtags'] = (int) $this->getOptedInHashtags();
         $export['canary'] = (bool) $this->isCanary();
+        $export['is_admin'] = $this->attributes['admin'] == 'yes';
 
         if (is_string($export['social_profiles'])) {
             $export['social_profiles'] = json_decode($export['social_profiles']);
