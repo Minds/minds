8515f85ca110edec60278f657b3a10be7451e623
Mark Harding <mark@kramnorth.com>
2019-04-23 15:05:55 +0100

    (fix): force big number to int

--- Core/Boost/Network/ElasticRepository.php
+++ Core/Boost/Network/ElasticRepository.php
@@ -158,7 +158,7 @@ class ElasticRepository
             'doc' => [
                 '@timestamp' => $boost->getCreatedTimestamp(),
                 'bid' => $boost->getBidType() === 'tokens' ?
-                    (int) BigNumber::fromPlain($boost->getBid(), 18) : $boost->getBid(),
+                    (int) BigNumber::fromPlain($boost->getBid(), 18)->toInt() : $boost->getBid(),
                 'bid_type' => $boost->getBidType(),
                 'entity_guid' => $boost->getEntityGuid(),
                 'impressions' => $boost->getImpressions(),
