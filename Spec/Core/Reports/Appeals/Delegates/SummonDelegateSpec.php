dd117ff06ebca6b7bbd26b2165a53a8aaa9de7e7
Mark Harding <mark@kramnorth.com>
2019-05-23 09:02:48 +0100

    (fix): test for summonsing

--- Spec/Core/Reports/Appeals/Delegates/SummonDelegateSpec.php
+++ Spec/Core/Reports/Appeals/Delegates/SummonDelegateSpec.php
@@ -6,6 +6,7 @@ use Minds\Core\Queue\Interfaces\QueueClient;
 use Minds\Core\Queue\Runners\ReportsAppealSummon;
 use Minds\Core\Reports\Appeals\Appeal;
 use Minds\Core\Reports\Appeals\Delegates\SummonDelegate;
+use Minds\Core\Reports\Report;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -27,14 +28,20 @@ class SummonDelegateSpec extends ObjectBehavior
         $this->shouldHaveType(SummonDelegate::class);
     }
 
-    function it_should_queue_on_appeal(Appeal $appeal)
+    function it_should_queue_on_appeal(Report $report)
     {
+        $report = new Report();
+        $report->setReasonCode(2);
+
+        $appeal = new Appeal();
+        $appeal->setReport($report);
+
         $this->queue->setQueue('ReportsAppealSummon')
             ->shouldBeCalled()
             ->willReturn($this->queue);
 
         $this->queue->send([
-            'appeal' => $appeal,
+            'appeal' => serialize($appeal),
             'cohort' => null,
         ])
             ->shouldBeCalled();
