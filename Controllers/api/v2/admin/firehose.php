2f0458e2ac8f34246225e2f74a13afb95ea7605b
Brian Hatchet <brian@minds.com>
2019-06-14 12:20:47 -0600

    Preserving the whitespace

--- Controllers/api/v2/admin/firehose.php
+++ Controllers/api/v2/admin/firehose.php
@@ -141,6 +141,7 @@ class firehose implements Interfaces\Api, Interfaces\ApiAdminPam
         if (isset($_POST['subreason_code'])) {
             $reasonCode = $_POST['subreason'];
         }
+        
         $manager->save($entity, $moderator, $reasonCode, $subReasonCode);
         return Factory::response([]);
     }
