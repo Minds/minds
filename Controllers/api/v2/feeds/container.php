cba74f10bc103b00800d1f9c305371bec2299e27
Mark Harding <mark@marksmusiconline.co.uk>
2019-07-11 14:37:01 +0000

    (fix): reverse pinned posts array

--- Controllers/api/v2/feeds/container.php
+++ Controllers/api/v2/feeds/container.php
@@ -132,7 +132,7 @@ class container implements Interfaces\Api
             'from_timestamp' => $fromTimestamp,
             'query' => $query,
             'single_owner_threshold' => 0,
-            'pinned_guids' => $type === 'activity' ? $container->getPinnedPosts() : null,
+            'pinned_guids' => $type === 'activity' ? array_reverse($container->getPinnedPosts()) : null,
         ];
 
         if (isset($_GET['nsfw'])) {
