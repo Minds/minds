1f5899848bd0dae652e0c16ab54bca8e7968f8dc
Ben Hayward <ben@minds.com>
2022-12-07 12:27:21 +0000

    Add metascraper into `minds start` command

--- local/tasks/start.js
+++ local/tasks/start.js
@@ -2,5 +2,5 @@ const doco = require('../lib/doco');
 
 module.exports = {
   title: 'Starting containers',
-  task: () => doco('up', '-d', 'nginx', 'runners', 'sockets')
+  task: () => doco('up', '-d', 'nginx', 'runners', 'sockets', 'metascraper')
 };
